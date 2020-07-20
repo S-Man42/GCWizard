d4aa9c43f5996ac577293548c0641828f8a1b990
S-Man42 <info@s-man42.de>
2020-07-20 11:01:57 +0200

    Numeral Bases: Overview... view #125

--- lib/widgets/tools/science_and_technology/numeralbases.dart
+++ lib/widgets/tools/science_and_technology/numeralbases.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/science_and_technology/numeral_bases.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
-import 'package:gc_wizard/widgets/common/gcw_numeralbase_spinner.dart';
 import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
+import 'package:gc_wizard/widgets/common/gcw_numeralbase_spinner.dart';
 import 'package:gc_wizard/widgets/common/gcw_text_divider.dart';
+import 'package:gc_wizard/widgets/common/gcw_twooptions_switch.dart';
+import 'package:gc_wizard/widgets/utils/common_widget_utils.dart';
 
 class NumeralBases extends StatefulWidget {
   @override
@@ -17,7 +19,8 @@ class NumeralBasesState extends State<NumeralBases> {
   String _currentInput = '';
   int _currentFromKey = 16;
   int _currentToKey = 10;
-  String _output = '';
+
+  var _currentToMode = GCWSwitchPosition.left;
 
   @override
   void initState() {
@@ -56,29 +59,74 @@ class NumeralBasesState extends State<NumeralBases> {
             });
           },
         ),GCWTextDivider(
-            text: i18n(context, 'numeralbases_to')
+          text: i18n(context, 'numeralbases_to')
         ),
-        GCWNumeralBaseSpinner(
-          value: _currentToKey,
+        GCWTwoOptionsSwitch(
+          leftValue: i18n(context, 'numeralbases_showfrequent'),
+          rightValue: i18n(context, 'numeralbases_chooseoutput'),
+          value: _currentToMode,
           onChanged: (value) {
             setState(() {
-              _currentToKey = value;
+              _currentToMode = value;
             });
           },
         ),
-        GCWDefaultOutput(
-            text: _calculateOutput(context)
-        )
+        _currentToMode == GCWSwitchPosition.right
+          ? GCWNumeralBaseSpinner(
+              value: _currentToKey,
+              onChanged: (value) {
+                setState(() {
+                  _currentToKey = value;
+                });
+              },
+            )
+          : Container(),
+        _buildOutput(context)
       ],
     );
   }
 
-  _calculateOutput(BuildContext context) {
-    if (_currentInput.startsWith('-') && _currentFromKey < 0)
-      _output = i18n(context, 'common_notdefined');
-    else
-      _output = convertBase(_currentInput, _currentFromKey, _currentToKey);
+  _buildOutput(BuildContext context) {
+    if (_currentInput.startsWith('-') && _currentFromKey < 0) {
+      return GCWDefaultOutput(
+        text: i18n(context, 'common_notdefined')
+      );
+    }
 
-    return _output;
+    //TODO: React on exceptions
+    var testValidInput = convertBase(_currentInput, _currentFromKey, 2);
+    if (testValidInput == null || testValidInput.length == 0) {
+      return GCWDefaultOutput(
+          text: i18n(context, 'common_notdefined')
+      );
+    }
+
+    if (_currentToMode == GCWSwitchPosition.right) {
+      return GCWDefaultOutput(
+        text: convertBase(_currentInput, _currentFromKey, _currentToKey)
+      );
+    }
+
+    var outputData = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,60].map((base) {
+      return [base.toString(), convertBase(_currentInput, _currentFromKey, base)];
+    }).toList();
+
+    var rows = columnedMultiLineOutput(outputData, flexValues: [1,3]);
+
+    rows.insert(0,
+      GCWTextDivider(
+        text: i18n(context, 'common_output')
+      )
+    );
+
+    return Column(
+      children: rows
+    );
   }
+
+//  _calculateOutput(BuildContext context) {
+//
+//
+//    return _output;
+//  }
 }
