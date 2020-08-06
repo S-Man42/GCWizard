2497173e944aa073c41fa041a6075e285b6b8dcf
tzimmermann <thomas@familiezimmermann.de>
2020-08-06 21:18:52 +0200

    implement map for 7-segment and 14-segment-display

--- lib/logic/tools/crypto_and_encodings/segment.dart
+++ lib/logic/tools/crypto_and_encodings/segment.dart
@@ -4,38 +4,36 @@ import 'package:gc_wizard/utils/common_utils.dart';
 enum SegmentTyp{Segment7,Segment14,Segment16}
 
 var AZToSegment = {};
-final AZTo14Segment = {
-  //a          b               c        d                e      f              g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9
-  //abcefg1g2 abcdefg1g2hijklm abcdg2il abcdefg1g2hijklm adef abcdefg1g2hijklm abcdil abcdefg1g2hijklm adefg1g2 abcdefg1g2hijklm aefg1g2 abcdefg1g2hijklm acdefg2 abcdefg1g2hijklm bcefg1g2 abcdefg1g2hijklm adil abcdefg1g2hijklm abcde abcdefg1g2hijklm efg1jm abcdefg1g2hijklm def abcdefg1g2hijklm bcefhj abcdefg1g2hijklm bcefhm abcdefg1g2hijklm abcdef abcdefg1g2hijklm abefg1g2 abcdefg1g2hijklm abcdefm abcdefg1g2hijklm abefg1g2m abcdefg1g2hijklm acdg2h abcdefg1g2hijklm ail abcdefg1g2hijklm bcdef abcdefg1g2hijklm efjk abcdefg1g2hijklm bcefkm abcdefg1g2hijklm hjkm abcdefg1g2hijklm hjl abcdefg1g2hijklm adjk abcdefg1g2hijklm abcdefjk abcdefg1g2hijklm bcj abcdefg1g2hijklm abdeg1g2 abcdefg1g2hijklm abcdg2 abcdefg1g2hijklm bcfg1g2 abcdefg1g2hijklm acdfg1g2 abcdefg1g2hijklm acdefg1g2 abcdefg1g2hijklm ajl abcdefg1g2hijklm abcdefg1g2 abcdefg1g2hijklm abcfg1g2
-  //
-  'A' : 'ABCEFG', 'B' : '', 'C' : '', 'D' : '', 'E' : '', 'F' : '', 'G' : '', 'H' : '', 'I' : '', 'J' : '',
-  'K' : '', 'L' : '', 'M' : '',  'N' : '', 'O' : '', 'P' : '', 'Q' : '', 'R' : '', 'S' : '', 'T' : '',
-  'U' : '', 'V' : '', 'W' : '', 'X' : '', 'Y' : '', 'Z' : '',
-  '1' : '', '2' : '', '3' : '', '4' : '', '5' : '', '6' : '', '7' : '', '8' : '', '9' : '', '0' : '',
-};
 final AZTo16Segment = {
-  //a          b               c        d                e      f              g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9
-  //abcefg1g2 abcdefg1g2hijklm abcdg2il abcdefg1g2hijklm adef abcdefg1g2hijklm abcdil abcdefg1g2hijklm adefg1g2 abcdefg1g2hijklm aefg1g2 abcdefg1g2hijklm acdefg2 abcdefg1g2hijklm bcefg1g2 abcdefg1g2hijklm adil abcdefg1g2hijklm abcde abcdefg1g2hijklm efg1jm abcdefg1g2hijklm def abcdefg1g2hijklm bcefhj abcdefg1g2hijklm bcefhm abcdefg1g2hijklm abcdef abcdefg1g2hijklm abefg1g2 abcdefg1g2hijklm abcdefm abcdefg1g2hijklm abefg1g2m abcdefg1g2hijklm acdg2h abcdefg1g2hijklm ail abcdefg1g2hijklm bcdef abcdefg1g2hijklm efjk abcdefg1g2hijklm bcefkm abcdefg1g2hijklm hjkm abcdefg1g2hijklm hjl abcdefg1g2hijklm adjk abcdefg1g2hijklm abcdefjk abcdefg1g2hijklm bcj abcdefg1g2hijklm abdeg1g2 abcdefg1g2hijklm abcdg2 abcdefg1g2hijklm bcfg1g2 abcdefg1g2hijklm acdfg1g2 abcdefg1g2hijklm acdefg1g2 abcdefg1g2hijklm ajl abcdefg1g2hijklm abcdefg1g2 abcdefg1g2hijklm abcfg1g2
   //
-  'A' : 'ABCEFG', 'B' : '', 'C' : '', 'D' : '', 'E' : '', 'F' : '', 'G' : '', 'H' : '', 'I' : '', 'J' : '',
-  'K' : '', 'L' : '', 'M' : '',  'N' : '', 'O' : '', 'P' : '', 'Q' : '', 'R' : '', 'S' : '', 'T' : '',
+  '1' : '', '2' : '', '3' : '', '4' : '', '5' : '',
+  '6' : '', '7' : '', '8' : '', '9' : '', '0' : '',
+  'A' : '', 'B' : '', 'C' : '', 'D' : '', 'E' : '',
+  'F' : '', 'G' : '', 'H' : '', 'I' : '', 'J' : '',
+  'K' : '', 'L' : '', 'M' : '',  'N' : '', 'O' : '',
+  'P' : '', 'Q' : '', 'R' : '',  'S' : '', 'T' : '',
   'U' : '', 'V' : '', 'W' : '', 'X' : '', 'Y' : '', 'Z' : '',
-  '1' : '', '2' : '', '3' : '', '4' : '', '5' : '', '6' : '', '7' : '', '8' : '', '9' : '', '0' : '',
+};
+final AZTo14Segment = {
+  // http://kryptografie.de/kryptografie/chiffre/14-segment.htm
+  '1' : 'bcj', '2' : 'abdeg1g2', '3' : 'abcdg2', '4' : 'bcfg1g2', '5' : 'acdfg1g2',
+  '6' : 'acdefg1g2', '7' : 'ajl', '8' : 'abcdefg1g2', '9' : 'abcfg1g2', '0' : 'abcdefjk',
+  'A' : 'abcefg1g2', 'B' : 'abcdg2il', 'C' : 'adef', 'D' : 'abcdil', 'E' : 'adefg1g2',
+  'F' : 'aefg1g2', 'G' : 'acdefg2', 'H' : 'bcefg1g2', 'I' : 'adil', 'J' : 'abcde',
+  'K' : 'efg1jm', 'L' : 'def', 'M' : 'bcefhj',  'N' : 'bcefhm', 'O' : 'abcdef',
+  'P' : 'abefg1g2', 'Q' : 'abcdefm', 'R' : 'abefg1g2m', 'S' : 'acdg2h', 'T' : 'ail',
+  'U' : 'bcdef', 'V' : 'efjk', 'W' : 'bcefkm', 'X' : 'hjkm', 'Y' : 'hjl', 'Z' : 'adjk',
 };
 final AZTo7Segment = {
-  //a      b     c    d     e     f    g     h     i  j   k     l   m    n     o    p     q     r   s   t
-  //ABCEFG CDEFG ADEF BCDEG ADEFG AEFG ACDEF BCEFG AE ACD ACEFG DEF ACEG ABCEF CDEG ABEFG ABCFG EG ACDF DEFG
-  //
-  // u     v    w    x  y     z    0      1  2      3     4    5     6      7     8     9
-  // BCDEF BEFG BDFG CE BCDFG ABDE ABCDEF BC ABDEG ABCDG BCFG ACDFG ACDEFG ABC ABCDEFG ABCDFG
-  //
-  'A' : 'ABCEFG', 'B' : 'CDEFG', 'C' : 'ADEF', 'D' : 'BCDEG', 'E' : 'ADEFG', 'F' : 'AEFG', 'G' : 'ACDEF', 'H' : 'BCEFG', 'I' : 'AE', 'J' : 'ACD',
-  'K' : 'ACEFG', 'L' : 'DEF', 'M' : 'ACEG',  'N' : 'ABCEF', 'O' : 'CDEG', 'P' : 'ABEFG', 'Q' : 'ABCFG', 'R' : 'EG', 'S' : 'ACDF', 'T' : 'DEFG',
-  'U' : 'BCDEF', 'V' : 'BEFG', 'W' : 'BDFG', 'X' : 'CE', 'Y' : 'BCDFG', 'Z' : 'ABDE',
-  '1' : 'BC', '2' : 'ABDEG', '3' : 'ABCDG', '4' : 'BCFG', '5' : 'ACDFG', '6' : 'ACDEFG', '7' : 'ABC', '8' : 'ABCDEFG', '9' : 'ABCDFG', '0' : 'ABCDEF',
+  // http://kryptografie.de/kryptografie/chiffre/7-segment.htm
+  '1' : 'bc', '2' : 'abdeg', '3' : 'abcdg', '4' : 'bcfg', '5' : 'acdfg',
+  '6' : 'acdefg', '7' : 'abc', '8' : 'abcdefg', '9' : 'abcdfg', '0' : 'abcdef',
+  'A' : 'abcefg', 'B' : 'cdefg', 'C' : 'adef', 'D' : 'bcdeg', 'E' : 'adefg',
+  'F' : 'aefg', 'G' : 'acdef', 'H' : 'bcefg', 'I' : 'ae', 'J' : 'acd',  'K' : 'acefg',
+  'L' : 'def', 'M' : 'aceg',  'N' : 'abcef', 'O' : 'cdeg', 'P' : 'abefg', 'Q' : 'abcfg',
+  'R' : 'ag', 'S' : 'acdf', 'T' : 'defg', 'U' : 'bcdef', 'V' : 'befg', 'W' : 'bdfg',
+  'X' : 'ce', 'Y' : 'bcdfg', 'Z' : 'abde',
 };
-
-// Å has same code as À, so À replaces Å in mapping; Å will not occur in this map
 
 var SegmentToAZ = {};
 var Segment7ToAZ = switchMapKeyValue(AZTo7Segment);
