7a03fe39a0fa67cdc03548ad0c4fd8507d46f9ea
tzimmermann <thomas@familiezimmermann.de>
2020-08-07 11:39:05 +0200

    increase readability of code

--- lib/logic/tools/crypto_and_encodings/bifid.dart
+++ lib/logic/tools/crypto_and_encodings/bifid.dart
@@ -1,3 +1,4 @@
+import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/widgets/common/gcw_twooptions_switch.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/polybios.dart';
 
@@ -11,82 +12,76 @@ class BifidOutput {
 enum BifidAlphabetMode{JToI, CToK, WToVV}
 
 String createBifidAlphabet(int gridDimension, {String firstLetters: '', PolybiosMode mode: PolybiosMode.AZ09, String fillAlphabet: '', BifidAlphabetMode alphabetMode}) {
-  switch (alphabetMode) {
-    case BifidAlphabetMode.JToI:
-      switch (mode) {
-        case PolybiosMode.AZ09:
-          if (gridDimension==5)
+  if (gridDimension ==5){ //
+    switch (mode) {
+      case PolybiosMode.AZ09:
+        switch (alphabetMode) {
+          case BifidAlphabetMode.JToI:
             fillAlphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ';
-          else
-            fillAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-          break;
-        case PolybiosMode.ZA90:
-          if (gridDimension==5)
-            fillAlphabet = 'ZYXWVUTSRQPONMLKIHGFEDCBA';
-          else
-            fillAlphabet = 'ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210';
-          break;
-        default: break;
-      }
-      break;
-    case BifidAlphabetMode.CToK:
-      switch (mode) {
-        case PolybiosMode.AZ09:
-          if (gridDimension == 5)
+            break;
+          case BifidAlphabetMode.CToK:
             fillAlphabet = 'ABDEFGHIJKLMNOPQRSTUVWXYZ';
-          else
-            fillAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-          break;
-        case PolybiosMode.ZA90:
-          if (gridDimension == 5)
-            fillAlphabet = 'ZYXWVUTSRQPONMLKJIHGFEDBA';
-          else
-            fillAlphabet = 'ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210';
-          break;
-        default: break;
-      }
-      break;
-    case BifidAlphabetMode.WToVV:
-      switch (mode) {
-        case PolybiosMode.AZ09:
-          if (gridDimension == 5)
+            break;
+          case BifidAlphabetMode.WToVV:
             fillAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVXYZ';
-          else
-            fillAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-          break;
-        case PolybiosMode.ZA90:
-          if (gridDimension == 5)
+            break;
+          default: break;
+        }
+        break;
+      case PolybiosMode.ZA90:
+        switch (alphabetMode) {
+          case BifidAlphabetMode.JToI:
+            fillAlphabet = 'ZYXWVUTSRQPONMLKIHGFEDCBA';
+            break;
+          case BifidAlphabetMode.CToK:
+            fillAlphabet = 'ZYXWVUTSRQPONMLKJIHGFEDBA';
+            break;
+          case BifidAlphabetMode.WToVV:
             fillAlphabet = 'ZYXVUTSRQPONMLKJIHGFEDCBA';
-          else
-            fillAlphabet = 'ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210';
-          break;
-        default: break;
-      }
-      break;
+            break;
+          default: break;
+        }
+        break;
+      default: break;
+    }
+  } else { // 6x6 square - no modification of alphabet
+    switch (mode) {
+      case PolybiosMode.AZ09:
+        fillAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        break;
+      case PolybiosMode.ZA90:
+        fillAlphabet = 'ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210';
+        break;
+      default:
+        break;
+    }
   }
   mode = PolybiosMode.CUSTOM;
   fillAlphabet = createPolybiosAlphabet(gridDimension, firstLetters: '', mode: mode, fillAlphabet: fillAlphabet);
   return fillAlphabet;
 }
 
+
 BifidOutput encryptBifid (String input, String key, {PolybiosMode mode: PolybiosMode.AZ09, String alphabet, BifidAlphabetMode alphabetMode}) {
   if (input == null || key == null)
     return null; //TODO Exception
+    //return BifidOutput(i18n(context, 'bifid_error_no_encrypt_input'),null);
 
   int dim = key.length;
   if (dim != 5 && dim != 6)
     return null; //TODO Exception
+    //return BifidOutput(i18n(context, 'bifid_error_wrong_griddimension'),null);
 
   alphabet = createBifidAlphabet(dim, mode: mode, fillAlphabet: alphabet, alphabetMode: alphabetMode);
 
   if (alphabet == null)
     return null; //TODO Exception
+    //return BifidOutput(i18n(context, 'bifid_error_no_alphabet'),null);
 
   key = key.toUpperCase();
   input = input.toUpperCase();
 
-  // check Input and correct it according to the chosen Alphabetmodification
-  if (dim == 5) {
+  if (dim == 5) {  // check Input and correct it according to the chosen Alphabetmodification
     switch (alphabetMode) {
       case BifidAlphabetMode.JToI:
         input = input.replaceAll('J', 'I');
@@ -106,6 +101,7 @@ BifidOutput encryptBifid (String input, String key, {PolybiosMode mode: Polybios
 
   if (polybiosOutput == null)
     return null;
+    //return BifidOutput(i18n(context, 'bifid_error_no_output'),null);
 
   var polybiosEncoded = polybiosOutput.output.replaceAll(' ', '');
 
@@ -126,18 +122,22 @@ BifidOutput encryptBifid (String input, String key, {PolybiosMode mode: Polybios
   return BifidOutput(polybiosOutput.output,polybiosOutput.grid);
 }
 
+
 BifidOutput decryptBifid (String input, String key, {PolybiosMode mode: PolybiosMode.AZ09, String alphabet, BifidAlphabetMode alphabetMode}) {
   if (input == null || key == null)
     return null; //TODO Exception
+    //return BifidOutput(i18n(context, 'bifid_error_no_decrypt_input'),null);
 
   int dim = key.length;
   if (dim != 5 && dim != 6)
     return null; //TODO Exception
+    //return BifidOutput(i18n(context, 'bifid_error_wrong_griddimension'),null);
 
   alphabet = createBifidAlphabet(dim, mode: mode, fillAlphabet: alphabet, alphabetMode: alphabetMode);
 
   if (alphabet == null)
     return null; //TODO Exception
+    //return BifidOutput(i18n(context, 'bifid_error_no_alphabet'),null);
 
   key = key.toUpperCase();
   input = input.toUpperCase();
@@ -163,6 +163,7 @@ BifidOutput decryptBifid (String input, String key, {PolybiosMode mode: Polybios
 
   if (polybiosOutput == null)
     return null;
+    //return BifidOutput(i18n(context, 'bifid_error_no_output'),null);
 
   var polybiosEncoded = polybiosOutput.output.replaceAll(' ', '');
 
