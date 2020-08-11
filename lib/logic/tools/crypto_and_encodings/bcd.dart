c0d2066fb51a4737fe8e46c4770ca21eeaeec494
tzimmermann <thomas@familiezimmermann.de>
2020-08-11 21:05:41 +0200

    - checking maps according to wikipedia - replacing whitespace with space to ease tests

--- lib/logic/tools/crypto_and_encodings/bcd.dart
+++ lib/logic/tools/crypto_and_encodings/bcd.dart
@@ -7,14 +7,10 @@
 
 import 'package:gc_wizard/utils/common_utils.dart';
 
-final DigitToBCD = {'0' : "0000", '1' : '0001', '2' : '0010', '3' : '0011', '4' : '0010',
-                    '5' : '0101', '6' : '0110', '7' : '0111', '8' : '1000', '9' : '1001'};
-final DigitToBCDAiken = {'0' : '0000', '1' : '0001', '2' : '0010', '3' : '0011', '4' : '0010',
-                         '5' : '1011', '6' : '1100', '7' : '1101', '8' : '1110', '9' : '1111'};
-final DigitToBCDStibitz = {'0' : '0011', '1' : '0100', '2' : '0101', '3' : '0110', '4' : '0111',
-                           '5' : '1000', '6' : '1001', '7' : '1010', '8' : '1011', '9' : '1100'};
-final DigitToBCDLibawCraig = {'0' : '00000', '1' : '00001', '2' : '0011', '3' : '00111', '4' : '01111',
-                              '5' : '11111', '6' : '11110', '7' : '11100', '8' : '11000', '9' : '10000'};
+final DigitToBCD = {'0' : "0000", '1' : '0001', '2' : '0010', '3' : '0011', '4' : '0100', '5' : '0101', '6' : '0110', '7' : '0111', '8' : '1000', '9' : '1001'};
+final DigitToBCDAiken = {'0' : '0000', '1' : '0001', '2' : '0010', '3' : '0011', '4' : '0110', '5' : '1011', '6' : '1100', '7' : '1101', '8' : '1110', '9' : '1111'};
+final DigitToBCDStibitz = {'0' : '0011', '1' : '0100', '2' : '0101', '3' : '0110', '4' : '0111', '5' : '1000', '6' : '1001', '7' : '1010', '8' : '1011', '9' : '1100'};
+final DigitToBCDLibawCraig = {'0' : '00000', '1' : '00001', '2' : '00011', '3' : '00111', '4' : '01111', '5' : '11111', '6' : '11110', '7' : '11100', '8' : '11000', '9' : '10000'};
 
 var BCDToDigit = switchMapKeyValue(DigitToBCD);
 var BCDAikenToDigit = switchMapKeyValue(DigitToBCDAiken);
@@ -30,7 +26,7 @@ String BCDencodeOriginal(String input){
       .split('')
       .where((character) => DigitToBCD[character] != null)
       .map((character) => DigitToBCD[character])
-      .join(String.fromCharCode(8195)); // using wide space
+      .join(' ');
 }
 
 
@@ -42,7 +38,7 @@ String BCDencodeAiken(String input){
       .split('')
       .where((character) => DigitToBCDAiken[character] != null)
       .map((character) => DigitToBCDAiken[character])
-      .join(String.fromCharCode(8195)); // using wide space
+      .join(' ');
 }
 
 
@@ -54,7 +50,7 @@ String BCDencodeStibitz(String input){
       .split('')
       .where((character) => DigitToBCDStibitz[character] != null)
       .map((character) => DigitToBCDStibitz[character])
-      .join(String.fromCharCode(8195)); // using wide space
+      .join(' '); // using wide space
 }
 
 
@@ -64,9 +60,9 @@ String BCDencodeLibawCraig(String input){
 
   return input
       .split('')
-      .where((character) => BCDLibawCraigToDigit[character] != null)
-      .map((character) => BCDLibawCraigToDigit[character])
-      .join(String.fromCharCode(8195)); // using wide space
+      .where((character) => DigitToBCDLibawCraig[character] != null)
+      .map((character) => DigitToBCDLibawCraig[character])
+      .join(' ');
 }
 
 
