1e4e038d1cc1e0d48efd12a1248d1b9e0bb6631f
tzimmermann <thomas@familiezimmermann.de>
2020-08-13 22:17:16 +0200

    add some more BCD encodings

--- lib/widgets/selector_lists/bcd_selection.dart
+++ lib/widgets/selector_lists/bcd_selection.dart
@@ -3,10 +3,15 @@ import 'package:gc_wizard/widgets/common/gcw_tool.dart';
 import 'package:gc_wizard/widgets/common/gcw_toollist.dart';
 import 'package:gc_wizard/widgets/registry.dart';
 import 'package:gc_wizard/widgets/selector_lists/gcw_selection.dart';
-import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdaiken.dart';
 import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdoriginal.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdaiken.dart';
 import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdstibitz.dart';
 import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdlibawcraig.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdgray.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdglixon.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdpetherick.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdobrien.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcdtompkins.dart';
 import 'package:gc_wizard/widgets/utils/common_widget_utils.dart';
 
 class BCDSelection extends GCWSelection {
@@ -18,8 +23,13 @@ class BCDSelection extends GCWSelection {
       return [
         className(BCDOriginal()),
         className(BCDAiken()),
+        className(BCDGlixon()),
+        className(BCDGray()),
         className(BCDLibawCraig()),
+        className(BCDPetherick()),
+        className(BCDOBrien()),
         className(BCDStibitz()),
+        className(BCDTompkins()),
       ].contains(className(element.tool));
     }).toList();
 
