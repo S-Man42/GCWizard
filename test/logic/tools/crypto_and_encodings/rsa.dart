98953bac2ae9ae75256dcb45e912b01bc4c2ef2a
S-Man42 <info@s-man42.de>
2020-08-13 18:16:48 +0200

    one more test

--- test/logic/tools/crypto_and_encodings/rsa.dart
+++ test/logic/tools/crypto_and_encodings/rsa.dart
@@ -11,6 +11,7 @@ void main() {
       {'input' : null, 'e': BigInt.from(1721), 'p': BigInt.from(307), 'q': BigInt.from(859), 'expectedOutput' : ''},
 
       {'input' : 'WIKIPEDIA IST 42!', 'e': BigInt.from(1721), 'p': BigInt.from(307), 'q': BigInt.from(859), 'expectedOutput' : '35225 253912 163314 253912 251016 133372 139634 253912 114917 191731 253912 218980 211928 191731 33825 241474 192628'},
+      {'input' : '1921', 'e': BigInt.from(17), 'p': BigInt.from(71), 'q': BigInt.from(83), 'expectedOutput' : '1172'},
     ];
 
     _inputsToExpected.forEach((elem) {
@@ -30,6 +31,7 @@ void main() {
       {'input' : null, 'd': BigInt.from(1373), 'p': BigInt.from(307), 'q': BigInt.from(859), 'expectedOutput' : ''},
 
       {'expectedOutput' : 'WIKIPEDIA IST 42!', 'd': BigInt.from(1373), 'p': BigInt.from(307), 'q': BigInt.from(859), 'input' : '35225 253912 163314 253912 251016 133372 139634 253912 114917 191731 253912 218980 211928 191731 33825 241474 192628'},
+      {'input' : '1172', 'd': BigInt.from(1013), 'p': BigInt.from(71), 'q': BigInt.from(83), 'expectedOutput' : '1921'},
     ];
 
     _inputsToExpected.forEach((elem) {
@@ -44,6 +46,7 @@ void main() {
     List<Map<String, dynamic>> _inputsToExpected = [
       {'e': null, 'p': null, 'q': null, 'expectedOutput' : null},
       {'e': BigInt.from(1721), 'p': BigInt.from(307), 'q': BigInt.from(859), 'expectedOutput' : BigInt.from(1373)},
+      {'e': BigInt.from(17), 'p': BigInt.from(71), 'q': BigInt.from(83), 'expectedOutput' : BigInt.from(1013)},
     ];
 
     _inputsToExpected.forEach((elem) {
