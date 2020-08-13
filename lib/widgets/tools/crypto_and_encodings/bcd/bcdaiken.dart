32e1a9d493e21ccf2faead908e9b55181b27fb7b
tzimmermann <thomas@familiezimmermann.de>
2020-08-13 22:16:36 +0200

    rewrite code to avoid dublications

--- lib/widgets/tools/crypto_and_encodings/bcd/bcdaiken.dart
+++ lib/widgets/tools/crypto_and_encodings/bcd/bcdaiken.dart
@@ -1,67 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/bcd.dart';
-import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
-import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
-import 'package:gc_wizard/widgets/common/gcw_twooptions_switch.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcd.dart';
 
-class BCDAiken extends StatefulWidget {
-  @override
-  BCDAikenState createState() => BCDAikenState();
+
+class BCDAiken extends BCD {
+
+  BCDAiken({Key key}) :
+        super(
+        key: key,
+        type: BCDType.AIKEN,
+      );
 }
 
-class BCDAikenState extends State<BCDAiken> {
-  var _inputController;
-
-  String _currentInput = '';
-  GCWSwitchPosition _currentMode = GCWSwitchPosition.left;
-
-  @override
-  void initState() {
-    super.initState();
-    _inputController = TextEditingController(text: _currentInput);
-  }
-
-  @override
-  void dispose() {
-    _inputController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        GCWTextField(
-          controller: _inputController,
-          onChanged: (text) {
-            setState(() {
-              _currentInput = text;
-            });
-          },
-        ),
-        GCWTwoOptionsSwitch(
-          onChanged: (value) {
-            setState(() {
-              _currentMode = value;
-            });
-          },
-        ),
-        _buildOutput(context)
-      ],
-    );
-  }
-
-  Widget _buildOutput(BuildContext context) {
-    var output = '';
-
-    if (_currentMode == GCWSwitchPosition.left) {
-      output = BCDencodeAiken(_currentInput);
-    } else {
-      output = BCDdecodeAiken(_currentInput);
-    }
-
-    return GCWDefaultOutput(
-        text: output
-    );
-  }
-}
--- lib/widgets/tools/crypto_and_encodings/bcd/bcdlibawcraig.dart
+++ lib/widgets/tools/crypto_and_encodings/bcd/bcdlibawcraig.dart
@@ -1,67 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/bcd.dart';
-import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
-import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
-import 'package:gc_wizard/widgets/common/gcw_twooptions_switch.dart';
+import 'package:gc_wizard/widgets/tools/crypto_and_encodings/bcd/bcd.dart';
 
-class BCDLibawCraig extends StatefulWidget {
-  @override
-  BCDLibawCraigState createState() => BCDLibawCraigState();
+
+class BCDLibawCraig extends BCD {
+
+  BCDLibawCraig({Key key}) :
+        super(
+        key: key,
+        type: BCDType.LIBAWCRAIG,
+      );
 }
 
-class BCDLibawCraigState extends State<BCDLibawCraig> {
-  var _inputController;
-
-  String _currentInput = '';
-  GCWSwitchPosition _currentMode = GCWSwitchPosition.left;
-
-  @override
-  void initState() {
-    super.initState();
-    _inputController = TextEditingController(text: _currentInput);
-  }
-
-  @override
-  void dispose() {
-    _inputController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        GCWTextField(
-          controller: _inputController,
-          onChanged: (text) {
-            setState(() {
-              _currentInput = text;
-            });
-          },
-        ),
-        GCWTwoOptionsSwitch(
-          onChanged: (value) {
-            setState(() {
-              _currentMode = value;
-            });
-          },
-        ),
-        _buildOutput(context)
-      ],
-    );
-  }
-
-  Widget _buildOutput(BuildContext context) {
-    var output = '';
-
-    if (_currentMode == GCWSwitchPosition.left) {
-      output = BCDencodeLibawCraig(_currentInput);
-    } else {
-      output = BCDdecodeLibawCraig(_currentInput);
-    }
-
-    return GCWDefaultOutput(
-        text: output
-    );
-  }
-}
