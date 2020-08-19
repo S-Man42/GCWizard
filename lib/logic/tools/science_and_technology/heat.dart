b8dd741eb87f8d571f03f8b78882092bd68d7b52
tzimmermann <thomas@familiezimmermann.de>
2020-08-19 19:00:53 +0200

    rewrite algorithm

--- lib/logic/tools/science_and_technology/heat.dart
+++ lib/logic/tools/science_and_technology/heat.dart
@@ -1,87 +1,51 @@
+// https://de.wikipedia.org/wiki/Hitzeindex
 import 'dart:math';
-
-class HeatOutput {
-  final String state;
-  final String output;
-
-  HeatOutput(this.state, this.output);
-}
 
 enum HeatTemperatureMode{Celsius, Fahrenheit}
 
 var heatParameterCelsius = {
-  'c1' : 8.784695 * (-1),
-  'c2' : 1.61139411,
-  'c3' : 2.338549,
-  'c4' : 0.14611605 * (-1),
-  'c5' : 1.2308094 * pow(10,-2) * (-1),
-  'c6' : 1.6424828 * pow(10,-2) * (-1),
-  'c7' : 2.211732 * pow(10,-3),
-  'c8' : 7.2546 * pow(10,-4),
-  'c9' : 3.582 * pow(10,-6) * (-1)
+  1 : 8.784695 * (-1),
+  2 : 1.61139411,
+  3 : 2.338549,
+  4 : 0.14611605 * (-1),
+  5 : 1.2308094 * pow(10,-2) * (-1),
+  6 : 1.6424828 * pow(10,-2) * (-1),
+  7 : 2.211732 * pow(10,-3),
+  8 : 7.2546 * pow(10,-4),
+  9 : 3.582 * pow(10,-6) * (-1)
 };
 
 var heatParameterFahrenheit = {
-  'c1' : 42.379 * (-1),
-  'c2' : 2.04901523,
-  'c3' : 10.1433127,
-  'c4' : 0.22475541 * (-1),
-  'c5' : 6.83783 * pow(10,-3) * (-1),
-  'c6' : 5.481717 * pow(10,-2) * (-1),
-  'c7' : 1.22874 * pow(10,-3),
-  'c8' : 8.5282 * pow(10,-4),
-  'c9' : 1.99 * pow(10,-6) * (-1),
+  1 : 42.379 * (-1),
+  2 : 2.04901523,
+  3 : 10.1433127,
+  4 : 0.22475541 * (-1),
+  5 : 6.83783 * pow(10,-3) * (-1),
+  6 : 5.481717 * pow(10,-2) * (-1),
+  7 : 1.22874 * pow(10,-3),
+  8 : 8.5282 * pow(10,-4),
+  9 : 1.99 * pow(10,-6) * (-1),
 };
 
-var heatParameter;
+var c;
 
-HeatOutput calculateHeat (String StrTemperature, String StrHumidity, HeatTemperatureMode TemperatureMode) {
-  //https://de.wikipedia.org/wiki/Hitzeindex
+String calculateHeat (double temperature, double humidity, HeatTemperatureMode temperatureMode) {
 
-  double Temperature = 0;
-  double Humidity = 0;
-  double Heat = 0;
+  double heat = 0;
 
-  String degree = '';
+  if (temperatureMode == HeatTemperatureMode.Celsius)
+    c = heatParameterCelsius;
+  else
+    c = heatParameterFahrenheit;
 
-  if (StrTemperature == null || StrTemperature.length == 0) {
-    return HeatOutput('ERROR', 'heat_error_noInput_Temperature');
-  }
-
-  Temperature = double.tryParse(StrTemperature);
-  if (Temperature == null ) {
-    return HeatOutput('ERROR', 'heat_error_wrongFormat_Double_Temperature');
-  }
-
-  if (StrHumidity == null || StrHumidity.length == 0) {
-    return HeatOutput('ERROR', 'heat_error_noInput_Humidity');
-  }
-
-  Humidity = double.tryParse(StrHumidity);
-  if (Humidity == null ) {
-    return HeatOutput('ERROR', 'heat_error_wrongFormat_Double_Humidity');
-  }
-
-  switch (TemperatureMode) {
-    case HeatTemperatureMode.Celsius:
-      heatParameter = heatParameterCelsius;
-      degree = ' °C';
-      break;
-    case HeatTemperatureMode.Fahrenheit:
-      heatParameter = heatParameterFahrenheit;
-      degree = ' °F';
-      break;
-    default:break;
-  }
-
-  Heat =  heatParameter['c1'] +
-          heatParameter['c2'] * Temperature +
-          heatParameter['c3'] * Humidity +
-          heatParameter['c4'] * Temperature * Humidity +
-          heatParameter['c5'] * Temperature * Temperature +
-          heatParameter['c6'] * Humidity * Humidity +
-          heatParameter['c7'] * Temperature * Temperature * Humidity +
-          heatParameter['c8'] * Temperature * Humidity * Humidity +
-          heatParameter['c9'] * Temperature * Temperature *Humidity * Humidity;
-  return HeatOutput('OK', Heat.toStringAsFixed(5) + degree);
+  heat =  c[1] +
+          c[2] * temperature +
+          c[3] * humidity +
+          c[4] * temperature * humidity +
+          c[5] * temperature * temperature +
+          c[6] * humidity * humidity +
+          c[7] * temperature * temperature * humidity +
+          c[8] * temperature * humidity * humidity +
+          c[9] * temperature * temperature *humidity * humidity;
+  return heat.toStringAsFixed(3);
 }
