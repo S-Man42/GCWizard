58cbac32b6297fb5d1a8b09faf9b84905dc79cc0
tzimmermann <thomas@familiezimmermann.de>
2020-08-19 20:49:55 +0200

    rewrite UI

--- lib/logic/tools/science_and_technology/projectiles.dart
+++ lib/logic/tools/science_and_technology/projectiles.dart
@@ -9,63 +9,76 @@ class ProjectilesOutput {
   ProjectilesOutput(this.state, this.output, this.formula);
 }
 
-ProjectilesOutput calculateEnergy (String GMass, String GSpeed) {
-  double ProjectilesEnergy = 0;
-  double ProjectilesMass = 0;
-  double ProjectilesSpeed = 0;
+//ProjectilesOutput calculateEnergy (String GMass, String GSpeed) {
+double calculateEnergy (double mass, double speed) {
+//  double projectilesEnergy = 0;
+//  double projectilesMass = 0;
+//  double projectilesSpeed = 0;
 
-  if (GMass == null || GMass.length == 0) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Mass', '');
-  }
+//  if (GMass == null || GMass.length == 0) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Mass', '');
+//    return 0;
+//  }
 
-  ProjectilesMass = double.tryParse(GMass);
-  if (ProjectilesMass == null ) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Mass', '');
-  }
+//  projectilesMass = double.tryParse(GMass);
+//  if (projectilesMass == null ) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Mass', '');
+//    return 0;
+//  }
 
-  if (GSpeed == null || GSpeed.length == 0) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Speed', '');
-  }
+//  if (GSpeed == null || GSpeed.length == 0) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Speed', '');
+//    return 0;
+//  }
 
-  ProjectilesSpeed = double.tryParse(GSpeed);
-  if (ProjectilesSpeed == null ) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Speed', '');
-  }
+//  ProjectilesSpeed = double.tryParse(GSpeed);
+//  if (ProjectilesSpeed == null ) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Speed', '');
+//    return 0;
+//  }
 
-  ProjectilesEnergy = ProjectilesMass / 2 * ProjectilesSpeed * ProjectilesSpeed;
-  return ProjectilesOutput('OK', ProjectilesEnergy.toStringAsFixed(5), 'Projectiles_energy');
+//  ProjectilesEnergy = ProjectilesMass / 2 * ProjectilesSpeed * ProjectilesSpeed;
+//  return ProjectilesOutput('OK', ProjectilesEnergy.toStringAsFixed(5), 'Projectiles_energy');
+    return mass / 2 * speed * speed;
 }
 
 
-ProjectilesOutput calculateMass (String GEnergy, String GSpeed) {
-  double ProjectilesMass = 0;
-  double ProjectilesEnergy = 0;
-  double ProjectilesSpeed = 0;
+//ProjectilesOutput calculateMass (String GEnergy, String GSpeed) {
+double calculateMass (double energy, double speed) {
+//  double ProjectilesMass = 0;
+//  double ProjectilesEnergy = 0;
+//  double ProjectilesSpeed = 0;
 
-  if (GEnergy == null || GEnergy.length == 0) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Energy', '');
-  }
+//  if (GEnergy == null || GEnergy.length == 0) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Energy', '');
+//  }
 
-  ProjectilesEnergy = double.tryParse(GEnergy);
-  if (ProjectilesEnergy == null ) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Energy', '');
-  }
+//  ProjectilesEnergy = double.tryParse(GEnergy);
+//  if (ProjectilesEnergy == null ) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Energy', '');
+//  }
 
-  if (GSpeed == null || GSpeed.length == 0) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Speed', '');
-  }
+//  if (GSpeed == null || GSpeed.length == 0) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_noInput_Speed', '');
+//  }
 
-  ProjectilesSpeed = double.tryParse(GSpeed);
-  if (ProjectilesSpeed == null ) {
-    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Speed', '');
-  }
+//  ProjectilesSpeed = double.tryParse(GSpeed);
+//  if (ProjectilesSpeed == null ) {
+//    return ProjectilesOutput('ERROR', 'Projectiles_error_wrongFormat_Double_Speed', '');
+//  }
 
-  ProjectilesMass = 2 * ProjectilesEnergy / ProjectilesSpeed / ProjectilesSpeed;
-  return ProjectilesOutput('OK', ProjectilesMass.toStringAsFixed(5), 'Projectiles_mass');
+//  ProjectilesMass = 2 * ProjectilesEnergy / ProjectilesSpeed / ProjectilesSpeed;
+//  return ProjectilesOutput('OK', ProjectilesMass.toStringAsFixed(5), 'Projectiles_mass');
+  if ( speed == 0)
+    return 0;
+  else
+    return 2 * energy / speed / speed;
 }
 
 
-ProjectilesOutput calculateSpeed (String GEnergy, String GMass) {
+//ProjectilesOutput calculateSpeed (String GEnergy, String GMass) {
+double calculateSpeed (double energy, double mass) {
+/*
   double calculateSpeed = 0;
   double ProjectilesEnergy = 0;
   double ProjectilesMass = 0;
@@ -90,4 +103,9 @@ ProjectilesOutput calculateSpeed (String GEnergy, String GMass) {
 
   calculateSpeed = sqrt(2 * ProjectilesEnergy / ProjectilesMass);
   return ProjectilesOutput('OK', calculateSpeed.toStringAsFixed(5), 'Projectiles_speed');
+*/
+  if (mass == 0)
+    return 0;
+  else
+    return sqrt(2 * energy / mass);
 }
--- lib/widgets/tools/science_and_technology/projectiles.dart
+++ lib/widgets/tools/science_and_technology/projectiles.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/science_and_technology/projectiles.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/polybios.dart';
+import 'package:gc_wizard/utils/units/velocity.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_button.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_dropdownbutton.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_output_text.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_toast.dart';
 import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
+import 'package:gc_wizard/widgets/common/gcw_double_spinner.dart';
 import 'package:gc_wizard/widgets/common/gcw_encrypt_buttonbar.dart';
 import 'package:gc_wizard/widgets/common/gcw_output.dart';
 import 'package:gc_wizard/widgets/common/gcw_text_divider.dart';
@@ -19,6 +21,105 @@ class Projectiles extends StatefulWidget {
 }
 
 class ProjectilesState extends State<Projectiles> {
+  double _currentEnergy = 0.0;
+  double _currentMass = 0.0;
+  double _currentSpeed = 0.0;
+  GCWSwitchPosition _currentSpeedUnit = GCWSwitchPosition.left;
+  var _isMetric = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        GCWDoubleSpinner(
+            title: i18n(context, 'projectiles_energy'),
+            min: 0.0,
+            value: _currentEnergy,
+            onChanged: (value) {
+              setState(() {
+                _currentEnergy = value;
+              });
+            }
+        ),
+        GCWDoubleSpinner(
+            title: i18n(context, 'projectiles_mass'),
+            min: 0.0,
+            value: _currentMass,
+            onChanged: (value) {
+              setState(() {
+                _currentMass = value;
+              });
+            }
+        ),
+        GCWDoubleSpinner(
+            title: i18n(context, 'projectiles_speed'),
+            value: _currentSpeed,
+            min: 0.0,
+            onChanged: (value) {
+              setState(() {
+                _currentSpeed = value;
+              });
+            }
+        ),
+            _buildOutput(context)
+      ],
+    );
+  }
+
+  Widget _buildOutput(BuildContext context) {
+    String calculate = '';
+    String result = '';
+
+    if (_currentEnergy == 0) {
+      calculate = 'projectiles_energy';
+      result = calculateEnergy(_currentMass, _currentSpeed).toStringAsFixed(3);
+    } else
+      if (_currentMass == 0) {
+        calculate = 'projectiles_mass';
+        result = calculateMass(_currentEnergy, _currentSpeed).toStringAsFixed(3);
+      } else
+        if (_currentSpeed == 0) {
+          calculate = 'projectiles_speed';
+          result = calculateSpeed(_currentEnergy, _currentMass).toStringAsFixed(3);
+        }
+
+    return GCWOutput(
+      child: Column(
+        children: <Widget>[
+          GCWTextDivider(
+              text: i18n(context, calculate)
+          ),
+
+          GCWOutputText(
+              text: result
+          ),
+        ],
+      ),
+    );
+
+/*
+
+    var windchill;
+    var temperature;
+
+    if (_isMetric) {
+      windchill = _currentSpeedUnit == GCWSwitchPosition.left
+          ? calcWindchillMetric(_currentTemperature, _currentWindSpeed)
+          : calcWindchillMetricMS(_currentTemperature, _currentWindSpeed);
+
+      temperature = TEMPERATURE_CELSIUS;
+    } else {
+      windchill = calcWindchillImperial(_currentTemperature, _currentWindSpeed);
+      temperature = TEMPERATURE_FAHRENHEIT;
+    }
+
+    return '${NumberFormat('#.###').format(windchill)} ${temperature.symbol}';
+  }
+*/
+  }
+
+
+/*
   var _inputControllerEnergy;
   var _inputControllerMass;
   var _inputControllerSpeed;
@@ -132,4 +233,5 @@ class ProjectilesState extends State<Projectiles> {
       );
     }
   }
+*/
 }
