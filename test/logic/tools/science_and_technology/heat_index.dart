72dcbd6ed53f2321faef8fc3eccc4a7259ffbf51
S-Man42 <info@s-man42.de>
2020-08-21 00:16:00 +0200

    fix heatindex tests

--- test/logic/tools/science_and_technology/heat_index.dart
+++ test/logic/tools/science_and_technology/heat_index.dart
@@ -1,24 +1,20 @@
 import "package:flutter_test/flutter_test.dart";
 import 'package:gc_wizard/logic/tools/science_and_technology/heat_index.dart';
+import 'package:gc_wizard/logic/units/temperature.dart';
 
 void main() {
   group("heat.calculate:", () {
     List<Map<String, dynamic>> _inputsToExpected = [
-      {'temperature' : 56.0, 'humidity' : 30.0, 'isMetric' : true, 'expectedOutput' : '87.287'},
-      {'temperature' : 56.0, 'humidity' : 30.0, 'isMetric' : false, 'expectedOutput' : '81.263'},
-      {'temperature' : 0.0, 'humidity' : 0.0, 'isMetric' : true, 'expectedOutput' : '-8.785'},
-      {'temperature' : 0.0, 'humidity' : 0.0, 'isMetric' : false, 'expectedOutput' : '-42.379'},
+      {'temperature' : 56.0, 'humidity' : 30.0, 'temperatureUnit' : TEMPERATURE_CELSIUS, 'expectedOutput' : '87.287'},
+      {'temperature' : 56.0, 'humidity' : 30.0, 'temperatureUnit' : TEMPERATURE_FAHRENHEIT, 'expectedOutput' : '81.263'},
+      {'temperature' : 0.0, 'humidity' : 0.0, 'temperatureUnit' : TEMPERATURE_CELSIUS, 'expectedOutput' : '-8.785'},
+      {'temperature' : 0.0, 'humidity' : 0.0, 'temperatureUnit' : TEMPERATURE_FAHRENHEIT, 'expectedOutput' : '-42.379'},
     ];
 
     _inputsToExpected.forEach((elem) {
-      test('temperature: ${elem['temperature']}, humidity: ${elem['humidity']}, isMetric: ${elem['isMetric']}', () {
-        var _actual;
-        if (elem['isMetric'])
-          _actual = calculateHeatIndex(elem['temperature'], elem['humidity'], HeatTemperatureMode.CELSIUS);
-        else
-          _actual = calculateHeatIndex(elem['temperature'], elem['humidity'], HeatTemperatureMode.FAHRENHEIT);
-
-        expect(_actual, elem['expectedOutput']);
+      test('temperature: ${elem['temperature']}, humidity: ${elem['humidity']}, temperatureUnit: ${elem['temperatureUnit'].symbol}', () {
+        var _actual = calculateHeatIndex(elem['temperature'], elem['humidity'], elem['temperatureUnit']);
+        expect(_actual.toStringAsFixed(3), elem['expectedOutput']);
       });
     });
   });
