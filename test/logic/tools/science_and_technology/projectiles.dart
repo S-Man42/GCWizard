07f351e2ef71b63f7e7a998053158b781630ef54
tzimmermann <thomas@familiezimmermann.de>
2020-08-23 01:11:32 +0200

    saving work in progress

--- test/logic/tools/science_and_technology/projectiles.dart
+++ test/logic/tools/science_and_technology/projectiles.dart
@@ -1,5 +1,9 @@
 import "package:flutter_test/flutter_test.dart";
 import 'package:gc_wizard/logic/tools/science_and_technology/projectiles.dart';
+import 'package:gc_wizard/logic/units/energy.dart';
+import 'package:gc_wizard/logic/units/mass.dart';
+import 'package:gc_wizard/logic/units/unit.dart';
+import 'package:gc_wizard/logic/units/velocity.dart';
 
 void main() {
   group("Projectiles.calculate:", () {
@@ -11,15 +15,21 @@ void main() {
       test('energy: ${elem['energy']}, mass: ${elem['mass']}, speed: ${elem['speed']}', () {
         var _actual;
         if (elem['mode'] == CalculateProjectilesMode.ENERGY) {
-          _actual = calculateEnergy(elem['mass'], elem['speed']).toStringAsFixed(3);
+          _actual = convert(calculateSpeed(convert(elem['input1'] , elem['inputUnit1'], MASS_GRAM) / 1000,
+                                           convert(elem['input2'] , elem['inputUnit2'], VELOCITY_MS)),
+                            ENERGY_JOULE, elem['outputUnit'])
+                            .toStringAsFixed(3) + ' ' + elem['outputUnit'].symbol;
         }
-        else if (elem['mode'] == CalculateProjectilesMode.ENERGY) {
-          _actual = calculateMass(elem['energy'], elem['speed']).toStringAsFixed(3);
+        else if (elem['mode'] == CalculateProjectilesMode.MASS) {
+          _actual = convert(calculateSpeed(convert(elem['input1'] , elem['inputUnit1'], ENERGY_JOULE),
+                                           convert(elem['input2'] , elem['inputUnit2'], VELOCITY_MS)),
+                            MASS_GRAM, elem['outputUnit'])
+                            .toStringAsFixed(3) + ' ' + elem['outputUnit'].symbol;
         } else {
-          _actual = calculateSpeed(
-              convert(elem['input1'] , elem[inputUnit1], ENERGY_JOULE),
-              convert(elem['input2'] , elem['inputUnit2'], _currentOutputVelocityUnit) / 1000
-          ).toStringAsFixed(3) + ' ' + _currentOutputVelocityUnit.symbol;
+          _actual = convert(calculateSpeed(convert(elem['input1'] , elem['inputUnit1'], ENERGY_JOULE),
+                                           convert(elem['input2'] , elem['inputUnit2'], MASS_GRAM) / 1000),
+                            VELOCITY_MS, elem['outputUnit'])
+                            .toStringAsFixed(3) + ' ' + elem['outputUnit'].symbol;
       }
         expect(_actual, elem['expectedOutput']);
       });
