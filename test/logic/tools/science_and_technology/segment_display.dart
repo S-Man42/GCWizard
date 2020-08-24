bcefb64a192de2c61264591ae89a5486b6683fa7
S-Man42 <info@s-man42.de>
2020-08-24 17:02:43 +0200

    Fix NULL Pointer Issue in Segment Display #168

--- test/logic/tools/science_and_technology/segment_display.dart
+++ test/logic/tools/science_and_technology/segment_display.dart
@@ -42,6 +42,13 @@ void main() {
       {'input' : ' .', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['dp']]},
       {'input' : '  .', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [[],['dp']]},
       {'input' : '1 1', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['b', 'c'], [], ['b', 'c']]},
+
+      {'input' : 'ö', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': []},
+      {'input' : 'öa', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['a1', 'a2', 'b', 'c', 'e', 'f', 'g']]},
+      {'input' : 'ö.', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['dp']]},
+      {'input' : 'aö', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['a1', 'a2', 'b', 'c', 'e', 'f', 'g']]},
+      {'input' : '.ö', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['dp']]},
+      {'input' : 'aö', 'segmentType' : SegmentDisplayType.SEVEN, 'expectedOutput': [['a1', 'a2', 'b', 'c', 'e', 'f', 'g']]},
     ];
 
     _inputsToExpected.forEach((elem) {
