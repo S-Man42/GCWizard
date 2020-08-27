accd091552f0ad7d3ab456b4e3a38662993baa5b
tzimmermann <thomas@familiezimmermann.de>
2020-08-27 22:21:25 +0200

    refactoring the logic to be more flexible by using a map with a list of frequencies as value/key

--- lib/logic/tools/science_and_technology/dtmf.dart
+++ lib/logic/tools/science_and_technology/dtmf.dart
@@ -18,8 +18,9 @@ final DigitToDTMF = {'0' : [941, 1336], '1' : [697, 1209], '2' : [697, 1336], '3
                      '5' : [770, 1336], '6' : [770, 1477], '7' : [852, 1209], '8' : [852, 1336], '9' : [852, 1477],
                      'A' : [697, 1633], 'B' : [770, 1633], 'C' : [852, 1633], 'D' : [941, 1633],
                      '*' : [941, 1209], '#' : [941, 1477]};
-
 final DTMFToDigit = switchMapKeyValue(DigitToDTMF);
+
+final DTMFFrequencies = {'697' : 697, '770' : 770, '852' : 852, '941' : 941, '1209' : 1209, '1336' : 1336, '1477' : 1477, '1633' : 1633};
 
 
 String encodeDTMF(String plain) {
@@ -29,27 +30,47 @@ String encodeDTMF(String plain) {
       .where((digit) => DigitToDTMF[digit] != null)
       .map((digit) => DigitToDTMF[digit])
       .join('   ');
-
 }
 
 
 String decodeDTMF(String chiffre) {
-  List chiffreList = chiffre.split(' ');
-//  List dtmfCode = new List<String>();
-  List dtmfCode = new List();
-  List tones = new List();
+  List _tones = new List<int>();
+  List _dtmfCodeClean = new List();
+  var _character;
+  String _result = '';
 
-  for(var i = 0; i < chiffreList.length - 1; i = i + 2) {
-//    dtmfCode.add(chiffreList[i] + ' ' + chiffreList[i + 1]);
-    tones = [int.tryParse(chiffreList[i]), int.tryParse(chiffreList[i + 1])];
-    tones.sort();
-    dtmfCode.add(tones);
+  List _dtmfCode = new List<List>();
+
+  // split input into numbers
+  List _chiffreList = chiffre.split(RegExp(r'[^0123456789]'));
+
+  // remove all numbers which are not correct frequencies
+  for (var i = 0; i < _chiffreList.length - 1; i++) {
+    if (DTMFFrequencies[_chiffreList[i]] != null) {
+      print (DTMFFrequencies[_chiffreList[i]]);
+      _dtmfCodeClean.add(DTMFFrequencies[_chiffreList[i]]);
+    }
   }
 
-  return dtmfCode
-    .map((tone) {
-      var character = DTMFToDigit[tone];
-      return character != null ? character : '';
-    })
-    .join(' ');
+  // build new list with pairs of frequencies
+  for(var i = 0; i < _dtmfCodeClean.length - 1; i = i + 2) {
+    if (i + 1 < _dtmfCodeClean.length - 1) {
+      _tones = [_dtmfCodeClean[i], _dtmfCodeClean[i + 1]];
+      _tones.sort();
+      _dtmfCode.add(_tones);
+    }
+  }
+
+  //decode list
+  for (var i = 0; i < _dtmfCode.length - 1; i++) {
+    _tones = _dtmfCode[i];
+    _character = DTMFToDigit[_tones];
+    if (_character != null) {
+      _result = _result + _character;
+    } else {
+      _result = _result + '?';
+    }
+  }
+  return _result;
+
 }
