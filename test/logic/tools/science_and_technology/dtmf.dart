5707566c8d885a4a164d8a62e4e478117d1d688b
tzimmermann <thomas@familiezimmermann.de>
2020-08-28 20:25:58 +0200

    corect typing errors to get the tests run through

--- test/logic/tools/science_and_technology/dtmf.dart
+++ test/logic/tools/science_and_technology/dtmf.dart
@@ -1,15 +1,14 @@
 import "package:flutter_test/flutter_test.dart";
-import 'package:gc_wizard/logic/tools/crypto_and_encodings/dtmf.dart';
 import 'package:gc_wizard/logic/tools/science_and_technology/dtmf.dart';
 
 void main() {
   group("DTMF.encodeDTMF:", () {
     List<Map<String, dynamic>> _inputsToExpected = [
       {'input' : '', 'expectedOutput' : ''},
-      {'input' : 'A', 'expectedOutput' : '[697, 1633]]'},
+      {'input' : 'A', 'expectedOutput' : '[697, 1633]'},
       {'input' : 's', 'expectedOutput' : ''},
-      {'input' : '27', 'expectedOutput' : '[697, 1336] [852, 1209]]'},
-      {'input' : '2h6', 'expectedOutput' : '[697, 1336] [770, 1477]'},
+      {'input' : '27', 'expectedOutput' : '[697, 1336]   [852, 1209]'},
+      {'input' : '2h6', 'expectedOutput' : '[697, 1336]   [770, 1477]'},
     ];
 
     _inputsToExpected.forEach((elem) {
