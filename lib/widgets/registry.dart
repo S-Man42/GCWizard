5dd2e54aea9c4beec01b647427f2b9468513e7b6
S-Man42 <info@s-man42.de>
2020-09-06 23:00:46 +0200

    Rename NATO Flags

--- lib/widgets/registry.dart
+++ lib/widgets/registry.dart
@@ -1498,13 +1498,19 @@ class Registry {
         tool: SymbolTable(symbolKey: 'flags'),
         i18nPrefix: 'symboltables_flags',
         iconPath: SYMBOLTABLES_ASSETPATH + 'flags/71.png',
-        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'flags flaggen wimpel fahnen '
+        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'flags flaggen wimpel fahnen flaggenalphabet flagalphabet '
       ),
       GCWToolWidget(
         tool: SymbolTable(symbolKey: 'flags_german_kriegsmarine'),
         i18nPrefix: 'symboltables_flags_german_kriegsmarine',
         iconPath: SYMBOLTABLES_ASSETPATH + 'flags_german_kriegsmarine/70.png',
-        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'flags flaggen wimpel fahnen deutsche kriegsmarine german warnavy '
+        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'flags flaggen wimpel fahnen deutsche kriegsmarine german warnavy flaggenalphabet flagalphabet '
+      ),
+      GCWToolWidget(
+        tool: SymbolTable(symbolKey: 'flags_nato'),
+        i18nPrefix: 'symboltables_flags_nato',
+        iconPath: SYMBOLTABLES_ASSETPATH + 'flags_nato/54.png',
+        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'flags flaggen wimpel fahnen nato army armee flaggenalphabet flagalphabet '
       ),
       GCWToolWidget(
         tool: SymbolTable(symbolKey: 'fonic'),
@@ -1727,12 +1733,6 @@ class Registry {
         i18nPrefix: 'symboltables_murraybaudot',
         iconPath: SYMBOLTABLES_ASSETPATH + 'murraybaudot/74.png',
         searchStrings: SEARCHSTRING_SYMBOLTABLES + SEARCHSTRING_CCITT2
-      ),
-      GCWToolWidget(
-        tool: SymbolTable(symbolKey: 'nato'),
-        i18nPrefix: 'symboltables_nato',
-        iconPath: SYMBOLTABLES_ASSETPATH + 'nato/54.png',
-        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'flags flaggen wimpel fahnen nato army armee '
       ),
       GCWToolWidget(
         tool: SymbolTable(symbolKey: 'notes'),
