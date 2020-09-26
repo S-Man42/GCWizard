0c7cae05cc27678bcbe73208d6b0091df05a4f0a
S-Man42 <info@s-man42.de>
2020-09-26 16:17:31 +0200

    change order of calls... first permission, afterwards service...

--- lib/widgets/tools/coords/base/gcw_mapview.dart
+++ lib/widgets/tools/coords/base/gcw_mapview.dart
@@ -165,20 +165,19 @@ class GCWMapViewState extends State<GCWMapView> {
   }
 
   _checkLocationPermission() async {
-    var _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
     var _permissionGranted = await location.hasPermission();
-
     if (_permissionGranted == PermissionStatus.denied) {
       _permissionGranted = await location.requestPermission();
       if (_permissionGranted != PermissionStatus.granted) {
         _currentLocationPermissionGranted = false;
+        return;
+      }
+    }
+
+    var _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
         return;
       }
     }
