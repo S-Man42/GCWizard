1bd25512cd92a4307eed2a1625882d5277ceb477
MLorenz42 <S-Man42@users.noreply.github.com>
2020-10-13 18:46:55 +0200

    Update registry.dart

--- lib/widgets/registry.dart
+++ lib/widgets/registry.dart
@@ -1651,7 +1651,7 @@ class Registry {
         tool: SymbolTable(symbolKey: 'eurythmy'),
         i18nPrefix: 'symboltables_eurythmy',
         iconPath: SYMBOLTABLES_ASSETPATH + 'eurythmy/65.png',
-        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'eurythmy eurythmie walddorf motion art'
+        searchStrings: SEARCHSTRING_SYMBOLTABLES + 'eurythmy eurythmie waldorf motion art marievonsivers rudolfsteiner movementart motionart bewegungskunst'
       ),
       GCWToolWidget(
         tool: SymbolTable(symbolKey: 'fakoo'),
