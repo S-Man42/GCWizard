c3069e50cbe4726e4daa4cf52ccf38216ffeaa94
S-Man42 <info@s-man42.de>
2020-10-14 20:55:02 +0200

    Restructured widgets Added tests

---
+++ lib/widgets/tools/crypto_and_encodings/numeral_words/numeral_words_lists.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:gc_wizard/i18n/app_localizations.dart';
+import 'package:gc_wizard/logic/tools/crypto_and_encodings/numeral_words.dart';
+import 'package:gc_wizard/widgets/common/base/gcw_dropdownbutton.dart';
+import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
+import 'package:gc_wizard/widgets/utils/common_widget_utils.dart';
+
+class NumeralWordsLists extends StatefulWidget {
+  @override
+  NumeralWordsListsState createState() => NumeralWordsListsState();
+}
+
+class NumeralWordsListsState extends State<NumeralWordsLists> {
+  TextEditingController _decodeController;
+
+  var _currentDecodeInput = '';
+  var _currentLanguage = NumeralWordsLanguage.ENG;
+
+  @override
+  void initState() {
+    super.initState();
+    _decodeController = TextEditingController(text: _currentDecodeInput);
+  }
+
+  @override
+  void dispose() {
+    _decodeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      children: <Widget>[
+        GCWDropDownButton(
+          value: _currentLanguage,
+          onChanged: (value) {
+            setState(() {
+              _currentLanguage = value;
+            });
+          },
+          items: NUMERALWORDS_LANGUAGES.entries.map((mode) {
+            return GCWDropDownMenuItem(
+              value: mode.key,
+              child: i18n(context, mode.value),
+            );
+          }).toList(),
+        ),
+        _buildOutput(context)
+      ],
+    );
+  }
+
+  Widget _buildOutput(BuildContext context) {
+    return GCWDefaultOutput(
+      child: Column(
+        children: columnedMultiLineOutput(
+          numeralWordsMap(_currentLanguage).entries.map((entry) {
+            if (int.tryParse(entry.value) != null)
+              return [entry.key, entry.value];
+          }).toList()
+        ),
+      ),
+    );
+  }
+}
