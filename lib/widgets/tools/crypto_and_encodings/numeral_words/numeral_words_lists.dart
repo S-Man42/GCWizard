4e3eb5d09497a383bd0baf70d62a44fe7407c1b9
tzimmermann <thomas@familiezimmermann.de>
2020-10-18 13:42:58 +0200

    refactor Output: there will be only an overview of one selected language. A list with all numeral words iterated over all languages seems not to be feasible

--- lib/widgets/tools/crypto_and_encodings/numeral_words/numeral_words_lists.dart
+++ lib/widgets/tools/crypto_and_encodings/numeral_words/numeral_words_lists.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/numeral_words.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_dropdownbutton.dart';
+import 'package:gc_wizard/widgets/common/base/gcw_output_text.dart';
 import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
 import 'package:gc_wizard/widgets/utils/common_widget_utils.dart';
 
@@ -53,15 +54,22 @@ class NumeralWordsListsState extends State<NumeralWordsLists> {
   }
 
   Widget _buildOutput(BuildContext context) {
-    return GCWDefaultOutput(
-      child: Column(
-        children: columnedMultiLineOutput(
-          numeralWordsMap(_currentLanguage).entries.map((entry) {
-            if (int.tryParse(entry.value) != null)
-              return [entry.key, entry.value];
-          }).toList()
+    if (_currentLanguage == NumeralWordsLanguage.ALL)
+      return GCWDefaultOutput(
+        child: GCWOutputText(
+          text: i18n(context, 'numeralwords_language_all_not_feasible')
+        )
+      );
+    else
+      return GCWDefaultOutput(
+        child: Column(
+          children: columnedMultiLineOutput(
+            NumWords[_currentLanguage].entries.map((entry) {
+              if (int.tryParse(entry.value) != null)
+                return [entry.key, entry.value];
+            }).toList()
+          ),
         ),
-      ),
-    );
+      );
   }
 }
