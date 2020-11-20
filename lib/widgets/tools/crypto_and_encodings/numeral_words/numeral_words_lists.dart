7ea103b87a30cded2185dba908c837cdb9489ec2
tzimmermann <thomas@familiezimmermann.de>
2020-11-20 19:01:07 +0100

    refactor: remove unused imports

--- lib/widgets/tools/crypto_and_encodings/numeral_words/numeral_words_lists.dart
+++ lib/widgets/tools/crypto_and_encodings/numeral_words/numeral_words_lists.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/numeral_words.dart';
-import 'package:gc_wizard/theme/theme_colors.dart';
-import 'package:gc_wizard/utils/common_utils.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_dropdownbutton.dart';
-import 'package:gc_wizard/widgets/common/base/gcw_output_text.dart';
-import 'package:gc_wizard/widgets/common/base/gcw_text.dart';
 import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
-import 'package:gc_wizard/widgets/common/gcw_output.dart';
 import 'package:gc_wizard/widgets/utils/common_widget_utils.dart';
 
 class NumeralWordsLists extends StatefulWidget {
