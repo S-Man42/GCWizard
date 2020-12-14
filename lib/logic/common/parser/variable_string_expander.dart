2551552869082847469cf331d677aee8ebb6845b
S-Man42 <info@s-man42.de>
2020-12-14 12:46:03 +0100

    safe stand, not compilable

--- lib/logic/common/parser/variable_string_expander.dart
+++ lib/logic/common/parser/variable_string_expander.dart
@@ -2,65 +2,145 @@ import 'dart:collection';
 
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/substitution.dart';
 
-Set<int> _expandVariableGroup(String group) {
-  var output = SplayTreeSet<int>();
+main() {
+  // var x = <String, String>{};
+  // var a;
+  // for (int i = 10000; i <= 30000; i++) {
+  //   if (i % 1000 == 0)
+  //     print(i);
+  //   for (int j = 55000; j <= 70000; j++) {
+  //      a = Map<String, String>.from(x);
+  //   }
+  // }
+  //
+  // print(a);
 
-  if (group == null)
-    return output;
+  var x = VariableStringExpander('AB', {'A': '10000-30000', 'B': '55000-70000'}, () => true);
 
-  group = group.replaceAll(RegExp(r'[^\d,\-#]'), '');
+  x.expand();
+}
 
-  if (group.length == 0)
-    return output;
+class VariableStringExpander{
+  String text;
+  Map<String, String> substitutions;
+  Function isResultIf;
+  Function breakIf;
 
-  var ranges = group.split(',');
-  ranges.forEach((range) {
-    var rangeParts = range.split('#');
-    var rangeBounds = rangeParts[0].split('-');
-    if(rangeBounds.length == 1) {
-      output.add(int.tryParse(rangeBounds[0]));
+  VariableStringExpander(this.text, this.substitutions, this.isResultIf, {this.breakIf});
+
+  Map<String, List<int>> expandedSubstitutions = {};
+  List<String> expSubstKeys;
+
+  List<int> _expandVariableGroup(String group) {
+    var output = SplayTreeSet<int>();
+
+    if (group == null)
+      return [];
+
+    group = group.replaceAll(RegExp(r'[^\d,\-#]'), '');
+
+    if (group.length == 0)
+      return [];
+
+    var ranges = group.split(',');
+    ranges.forEach((range) {
+      var rangeParts = range.split('#');
+      var rangeBounds = rangeParts[0].split('-');
+      if(rangeBounds.length == 1) {
+        output.add(int.tryParse(rangeBounds[0]));
+        return;
+      }
+
+      var start = int.tryParse(rangeBounds[0]);
+      var end = int.tryParse(rangeBounds[1]);
+
+      if (start == end) {
+        output.add(start);
+        return;
+      }
+
+      var increment = 1;
+      if (rangeParts.length > 1)
+        increment = int.tryParse(rangeParts[1]);
+
+      if (start < end) {
+        for (int i = start; i <= end; i += increment)
+          output.add(i);
+      } else {
+        for (int i = start; i >= end; i -= increment)
+          output.add(i);
+      }
+    });
+
+    return output.toList();
+  }
+  List<Map<String, String>> results = [];
+
+  void _generateCartesianVariables(int keyIndex) {
+    // var keys = expandedSubstitutions.keys.toList();
+    if (keyIndex == expSubstKeys.length) {
+      // results.add(current);
       return;
     }
 
-    var start = int.tryParse(rangeBounds[0]);
-    var end = int.tryParse(rangeBounds[1]);
+    var key = expSubstKeys[keyIndex];
+    var list = expandedSubstitutions[key];
+    for (int i = 0; i < list.length; i++) {
+      if (keyIndex == 0 && i % 1000 == 0)
+        print(i);
 
-    if (start == end) {
-      output.add(start);
-      return;
+      // var newList = Map<String, String>.from(current);
+      // newList.putIfAbsent(key, () => list[i].toString());
+      _generateCartesianVariables(keyIndex + 1);
     }
-
-    var increment = 1;
-    if (rangeParts.length > 1)
-      increment = int.tryParse(rangeParts[1]);
-
-    if (start < end) {
-      for (int i = start; i <= end; i += increment)
-        output.add(i);
-    } else {
-      for (int i = start; i >= end; i -= increment)
-        output.add(i);
-    }
-  });
-
-  return output;
-}
-
-void _generateCartesianVariables(Map<String, List<int>> lists, List<Map<String, String>> result, int keyIndex, Map<String, String> current) {
-  var keys = lists.keys.toList();
-  if (keyIndex == keys.length) {
-    result.add(current);
-    return;
   }
 
-  var key = keys[keyIndex];
-  var list = lists[key];
-  for (int i = 0; i < list.length; i++) {
-    var newList = Map<String, String>.from(current);
-    newList.putIfAbsent(key, () => list[i].toString());
-    _generateCartesianVariables(lists, result, keyIndex + 1, newList);
+  List<Map<String, dynamic>> expand() {
+    var expandedList = SplayTreeSet<String>();
+
+    if (text == null || text.length == 0)
+      return [];
+
+    if (substitutions == null || substitutions.length == 0) {
+      return [{'text': text, 'variables': {}}];
+    }
+
+    text = sanitizeForFormula(text);
+
+    substitutions.entries.forEach((substitution) => expandedSubstitutions.putIfAbsent(
+        substitution.key,
+            () => _expandVariableGroup(substitution.value).toList()
+    ));
+
+    expSubstKeys = expandedSubstitutions.keys.toList();
+
+    print('A');
+    _generateCartesianVariables(0);
+
+    List<Map<String, dynamic>> output = [];
+
+    // RegExp regExp = new RegExp(r'\[.+?\]');
+    // var matches = regExp.allMatches(text.trim());
+    //
+    // variableCombinations.forEach((combination) {
+    //   var substituted = text;
+    //
+    //   matches.forEach((match) {
+    //     var content = substitution(match.group(0), combination, caseSensitive: false);
+    //     substituted = substituted.replaceAll(match.group(0), content);
+    //   });
+    //
+    //   if (expandedList.add(substituted)) {
+    //     output.add({'text': substituted, 'variables': combination});
+    //   }
+    // });
+
+    return output;
   }
 }
+
+
+
 
 sanitizeForFormula(String formula) {
   RegExp regExp = new RegExp(r'\[.+?\]');
@@ -68,47 +148,4 @@ sanitizeForFormula(String formula) {
     return formula;
 
   return '[$formula]';
-}
-
-List<Map<String, dynamic>> expandText(String text, Map<String, String> substitutions) {
-  var expandedList = SplayTreeSet<String>();
-
-  if (text == null || text.length == 0)
-    return [];
-
-  if (substitutions == null || substitutions.length == 0) {
-    expandedList.add(text);
-    return [];
-  }
-
-  text = sanitizeForFormula(text);
-
-  Map<String, List<int>> expandedSubstitutions = {};
-  substitutions.entries.forEach((substitution) => expandedSubstitutions.putIfAbsent(
-      substitution.key,
-          () => _expandVariableGroup(substitution.value).toList()
-  ));
-
-  List<Map<String, String>> variableCombinations = [];
-  _generateCartesianVariables(expandedSubstitutions, variableCombinations, 0, {});
-
-  List<Map<String, dynamic>> output = [];
-
-  RegExp regExp = new RegExp(r'\[.+?\]');
-  var matches = regExp.allMatches(text.trim());
-
-  variableCombinations.forEach((combination) {
-    var substituted = text;
-
-    matches.forEach((match) {
-      var content = substitution(match.group(0), combination, caseSensitive: false);
-      substituted = substituted.replaceAll(match.group(0), content);
-    });
-
-    if (expandedList.add(substituted)) {
-      output.add({'text': substituted, 'variables': combination});
-    }
-  });
-
-  return output;
 }
