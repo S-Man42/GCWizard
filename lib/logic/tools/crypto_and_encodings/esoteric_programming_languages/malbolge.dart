857a06ecdb9a12f8fbc05f59de923ec3fdc6c3a6
tzimmermann <thomas@familiezimmermann.de>
2021-01-07 19:21:02 +0100

    work in progress

--- lib/logic/tools/crypto_and_encodings/esoteric_programming_languages/malbolge.dart
+++ lib/logic/tools/crypto_and_encodings/esoteric_programming_languages/malbolge.dart
@@ -21,8 +21,8 @@ class DebugOutput{
   DebugOutput(this.pc, this.command, this.stack, this.output);
 }
 
-final normalTranslate  = '+b(29e*j1VMEKLyC})8&m#~W>qxdRp0wkrUo[D7,XTcA"lI.v%{gJh4G\\-=O@5`_3i<?Z\';FNQuY]szf\$!BS/|t:Pn6^Ha';
-final encryptionTranslate  = '5z]&gqtyfr\$(we4{WP)H-Zn,[%\\3dL+Q;>U!pJS72FhOA1CB6v^=I_0/8|jsb9m<.TVac`uY*MK\'X~xDl}REokN:#?G"i@';
+final xlat1  = '+b(29e*j1VMEKLyC})8&m#~W>qxdRp0wkrUo[D7,XTcA"lI.v%{gJh4G\\-=O@5`_3i<?Z\';FNQuY]szf\$!BS/|t:Pn6^Ha';
+final xlat2  = '5z]&gqtyfr\$(we4{WP)H-Zn,[%\\3dL+Q;>U!pJS72FhOA1CB6v^=I_0/8|jsb9m<.TVac`uY*MK\'X~xDl}REokN:#?G"i@';
 final validInstructions  = {'j', 'i', '*', 'p', '<', '/', 'v', 'o'};
 final opCodeList = {
   'j' : 'mov d, [d]',
@@ -35,7 +35,7 @@ final opCodeList = {
   'o' : 'nop' };
 
 malbolgeOutput generateMalbolge(String inputString){
-  String endString = '';
+  String endString = 'j';
   String tempString = '';
   List masterList = new List();
   List tempList = new List();
@@ -50,6 +50,7 @@ malbolgeOutput generateMalbolge(String inputString){
 
   for (int x = 0; x < inputString.length; x++){                                 // Master loop, looks for letters
     tempString = '';
+
     found = false;                                                              // Whether or not the current letter of the desired string has been found
     if (masterList.length > 0) masterList.removeRange(0, masterList.length - 1);// masterList holds all possible combination of the above opCodes for some length.
     if (tempList.length > 0) tempList.removeRange(0, masterList.length - 1);    // tempList is used as a temporary holder while all possible combinations of opCodes of length + 1 are generated
@@ -119,44 +120,47 @@ malbolgeOutput generateMalbolge(String inputString){
 malbolgeOutput interpretMalbolge(String program, String STDIN){
   List<int> memory = new List<int>(59049);
 
+  if (program.length < 2)
+    return malbolgeOutput([
+      'malbolge_error_invalid_program',
+      'malbolge_error_program_to_short'], [], [], []);
+
   if (_checkNormalize(program))
     program = _reverseNormalize(program);
 
   // load program
-  int codeUnit = 0;
+  int charCode = 0;
   String malbolge = program.replaceAll(RegExp(r'\s'), '');
   int j = 0;
-  for (int i = 0; i < malbolge.length; i++) {
-    codeUnit = malbolge.codeUnitAt(i);
-     if (codeUnit < 127 && codeUnit > 32) {
-       if (!validInstructions .contains(normalTranslate [(codeUnit - 33 + i) % 94]))
+  int i = 0;
+  while (i < malbolge.length){
+    charCode = malbolge.codeUnitAt(i);
+     if (charCode < 127 && charCode > 32) {
+       if (!validInstructions .contains(xlat1 [(charCode - 33 + i) % 94]))
         return malbolgeOutput([
           'malbolge_error_invalid_program',
           'malbolge_error_invalid_character'], [], [], []);
      }
-    if (i > 59048) {
+    if (i == 59049) {
       return malbolgeOutput([
         'malbolge_error_invalid_program',
         'malbolge_error_program_to_big'], [], [], []);
     }
-     memory[i] = codeUnit;
-     j = i;
+     memory[i] = charCode;
+     i++;
   }
-  // fill memory with crazy(i-1, i-2)
-  if (j < 2)
-    return malbolgeOutput([
-      'malbolge_error_invalid_program',
-      'malbolge_error_program_to_short'], [], [], []);
-  for (int i = j + 1; i < 59049; i++) {
-    memory[i] = _crazy(memory[i - 1], memory[i - 2]);
+
+  // fill memory with op(i-1, i-2)
+  while (i < 59049){
+    memory[i] = _op(memory[i - 1], memory[i - 2]);
+    i++;
   }
 
   // execute programm
   int a = 0;
   int c = 0;
   int d = 0;
-  int opCode = 0;
-  String op = '';
+  String opcode = '';
   bool halt = false;
   int input = 0;
   List<String> output = new List<String>();
@@ -166,13 +170,13 @@ malbolgeOutput interpretMalbolge(String program, String STDIN){
   String STDOUT = '';
 
   while (!halt){
-    opCode = memory[c];
-    if (_invalid(opCode)) {
+    if (memory[c] < 33 || memory[c] > 126) {
       output.addAll([STDOUT,
         '',
         'malbolge_error_runtime',
         'malbolge_error_invalid_opcode',
-        'opCode: '+opCode.toString()+' '+String.fromCharCode(opCode),
+        'opCode: '+memory[c].toString()+' '+String.fromCharCode(memory[c]),
+        'malbolge_error_infinite_loop',
         '',
         'STACK TRACE ----------',
         'c = '+c.toString(),
@@ -180,13 +184,28 @@ malbolgeOutput interpretMalbolge(String program, String STDIN){
         'd = '+d.toString(),]);
       return malbolgeOutput(output, assembler, memnonic, debug);
     }
-    op = normalTranslate [(memory[c] - 33 + c) % 94];
-    assembler.add(_format(c) + '   ' + op);
-    memnonic.add(opCodeList[op]);
+    opcode = xlat1 [(memory[c] - 33 + c) % 94];
+    assembler.add(_format(c) + '   ' + opcode);
+    memnonic.add(opCodeList[opcode]);
 
-    switch (op) {
+    switch (opcode) {
+      case 'j':  //    40     mov d, [d]
+        d = memory[d];
+        break;
+
       case 'i':   //     4     c = [d]
         c = memory[d];
+        break;
+
+      case '*':  //    39     a = [d] = rotr [d]
+        memory[d] =  memory[d] ~/ 3 + (memory[d] % 3) * 19683;
+        //memory[d] =  _rotr(memory[d]);
+        a = memory[d];
+        break;
+
+      case 'p':  //    62     a = [d] = crazy(a, [d])
+        memory[d] = _op(a, memory[d]);
+        a = memory[d];
         break;
 
       case '<':   //   23     out a % 256
@@ -207,20 +226,6 @@ malbolgeOutput interpretMalbolge(String program, String STDIN){
         }
         break;
 
-      case '*':  //    39     a = [d] = rotr [d]
-        memory[d] = _rotate(memory[d]);
-        a = memory[d];
-        break;
-
-      case 'j':  //    40     mov d, [d]
-        d = memory[d];
-        break;
-
-      case 'p':  //    62     a = [d] = crazy(a, [d])
-        memory[d] = _crazy(a, memory[d]);
-        a = memory[d];
-        break;
-
       case 'o':  //    68     NOP
         break;
 
@@ -230,8 +235,22 @@ malbolgeOutput interpretMalbolge(String program, String STDIN){
       default:
     };
 
-    if (!_invalid(memory[c]))
-      memory[c] = encryptionTranslate .codeUnitAt(memory[c] - 33);
+    if (memory[c] < 33 || memory[c] > 126) {
+      output.addAll([STDOUT,
+        '',
+        'malbolge_error_runtime',
+        'malbolge_error_invalid_opcode',
+        'opCode: '+memory[c].toString()+' '+String.fromCharCode(memory[c]),
+        (opcode == 'i') ? 'malbolge_error_illegal_jump' : 'malbolge_error_illegal_write',
+        '',
+        'STACK TRACE ----------',
+        'c = '+c.toString(),
+        'a = '+a.toString(),
+        'd = '+d.toString(),]);
+      return malbolgeOutput(output, assembler, memnonic, debug);
+    };
+
+    memory[c] = xlat2 .codeUnitAt(memory[c] - 33);
 
     if (c == 59048)
       c = 0;
@@ -246,41 +265,33 @@ malbolgeOutput interpretMalbolge(String program, String STDIN){
   return malbolgeOutput(output, assembler, memnonic, debug);
 }
 
-final p9 = {0 : 1, 1 : 9, 2 : 81, 3 : 729, 4 : 6561};
+final p9 = [1, 9, 81, 729, 6561];
 
-final o = {
-  0 : { 0:4, 1:3, 2:3, 3:1, 4:0, 5:0, 6:1, 7:0, 8:0 },
-  1 : { 0:4, 1:3, 2:5, 3:1, 4:0, 5:2, 6:1, 7:0, 8:2 },
-  2 : { 0:5, 1:5, 2:4, 3:2, 4:2, 5:1, 6:2, 7:2, 8:1 },
-  3 : { 0:4, 1:3, 2:3, 3:1, 4:0, 5:0, 6:7, 7:6, 8:6 },
-  4 : { 0:4, 1:3, 2:5, 3:1, 4:0, 5:2, 6:7, 7:6, 8:8 },
-  5 : { 0:5, 1:5, 2:4, 3:2, 4:2, 5:1, 6:8, 7:8, 8:7 },
-  6 : { 0:7, 1:6, 2:6, 3:7, 4:6, 5:6, 6:4, 7:3, 8:3 },
-  7 : { 0:7, 1:6, 2:8, 3:7, 4:6, 5:8, 6:4, 7:3, 8:5 },
-  8 : { 0:8, 1:8, 2:7, 3:8, 4:8, 5:7, 6:5, 7:5, 8:4 },
-};
+final o = [
+  [4, 3, 3, 1, 0, 0, 1, 0, 0 ],
+  [4, 3, 5, 1, 0, 2, 1, 0, 2 ],
+  [5, 5, 4, 2, 2, 1, 2, 2, 1 ],
+  [4, 3, 3, 1, 0, 0, 7, 6, 6 ],
+  [4, 3, 5, 1, 0, 2, 7, 6, 8 ],
+  [5, 5, 4, 2, 2, 1, 8, 8, 7 ],
+  [7, 6, 6, 7, 6, 6, 4, 3, 3 ],
+  [7, 6, 8, 7, 6, 8, 4, 3, 5 ],
+  [8, 8, 7, 8, 8, 7, 5, 5, 4 ],
+];
 
-int _crazy(int x, y){
+int _op(int x, y){
   int i = 0;
   for (int j = 0; j < 5; j++){
     i = i + o[(y ~/ p9[j]) % 9][(x ~/ p9[j]) % 9] * p9[j];
   }
   return i;
-  // String trits1 = _intToTrits(x);
-  // String trits2 = _intToTrits(y);
-  // String output = '';
-  // for (int i = 0; i < trits1.length; i++)
-  //   output = output + crazyMap[trits2[i] + trits1[i]];
-  //return _tritsToInt(output);
 }
 
-int _rotate(int n){
+int _rotr(int n){
  return (n ~/ 3 + (n % 3) * 19683);
 }
 
-bool _invalid(int opCode) {
-  return !(32 < opCode && opCode < 127);
-}
+
 
 String _format(int n) {
   String output = n.toString();
@@ -303,7 +314,7 @@ String _normalize(String instructionList){                                      
   String returnString = '';
   String tempChar = '';
   for (int x = 0; x < instructionList.length; x++) {
-    tempChar = normalTranslate[((instructionList.codeUnitAt(x) + x - 33) % 94)];
+    tempChar = xlat1[((instructionList.codeUnitAt(x) + x - 33) % 94)];
     if (validInstructions.contains(tempChar))
       returnString = returnString + tempChar;
   }
@@ -315,7 +326,7 @@ String _reverseNormalize(String instructionList){                               
   String tempChar = '';
   for (int x = 0; x < instructionList.length; x++) {
     if (validInstructions.contains(instructionList[x]))  {                        // Checks to see if the instructions provided are valid
-      tempChar = String.fromCharCode(((_index(normalTranslate, instructionList[x]) - x) % 94) + 33);
+      tempChar = String.fromCharCode(((_index(xlat1, instructionList[x]) - x) % 94) + 33);
       returnString = returnString + tempChar;
     } else
       return "Invalid program string";
@@ -355,7 +366,7 @@ String _interpret(String instructionList){
   //}
 
   while(running){
-    currentInstruction = normalTranslate[(malbolgeTape[c] - 33 + c) % 94];
+    currentInstruction = xlat1[(malbolgeTape[c] - 33 + c) % 94];
 
     // executes instruction
     switch (currentInstruction) { // check only the instructions the generator uses
@@ -366,7 +377,7 @@ String _interpret(String instructionList){
         if(d >= maxInstructions)
           running =false;
         else {
-          a = _rotate(malbolgeTape[d]);
+          a = _rotr(malbolgeTape[d]);
           malbolgeTape[d] = a;
         }
         break;
@@ -374,10 +385,13 @@ String _interpret(String instructionList){
         if(d >= maxInstructions)
           running = false;
         else {
-          a = _crazy(a, malbolgeTape[d]);
+          a = _op(a, malbolgeTape[d]);
           malbolgeTape[d] = a;
         }
       break;
+      case 'j': // d = mov d, [d]
+        d = malbolgeTape[d];
+        break;
       case 'v': // halt
         running = false;
         break;
@@ -387,7 +401,7 @@ String _interpret(String instructionList){
 
     // Encrypts the last-executed instructions on the tape - If you aren't using jumps, this isn't technically necessary
     if (33 <= malbolgeTape[c] && malbolgeTape[c] <= 126)
-      malbolgeTape[c] = encryptionTranslate.codeUnitAt(malbolgeTape[c] - 33);
+      malbolgeTape[c] = xlat2.codeUnitAt(malbolgeTape[c] - 33);
 
     // Auto-increment C and D
     c++;
