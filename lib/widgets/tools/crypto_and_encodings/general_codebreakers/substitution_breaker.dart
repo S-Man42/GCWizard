c5d27d65cb7939ca64cb1040c6c8fc8b0ae61cc6
Mike-3 <Mike.Binnemann@web.de>
2021-01-17 21:40:36 +0100

    substitution breaker sumbit button

--- lib/widgets/tools/crypto_and_encodings/general_codebreakers/substitution_breaker.dart
+++ lib/widgets/tools/crypto_and_encodings/general_codebreakers/substitution_breaker.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/general_codebreakers/substitution_breaker/quadgrams/quadgrams.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/general_codebreakers/substitution_breaker/substitution_breaker.dart';
+import 'package:gc_wizard/widgets/common/base/gcw_button.dart';
 import 'package:gc_wizard/widgets/common/gcw_submit_button.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_dropdownbutton.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_output_text.dart';
