47b60fb3f78ad9aae0592358027106295d403329
Mike-3 <Mike.Binnemann@web.de>
2021-01-25 17:19:30 +0100

    backup

--- lib/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/multi_decoder.dart
+++ lib/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/multi_decoder.dart
@@ -9,7 +9,6 @@ import 'package:gc_wizard/widgets/common/base/gcw_iconbutton.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
 import 'package:gc_wizard/widgets/common/gcw_output.dart';
 import 'package:gc_wizard/widgets/common/gcw_async_executer.dart';
-import 'package:gc_wizard/widgets/common/gcw_async_executer.dart';
 import 'package:gc_wizard/widgets/common/gcw_text_divider.dart';
 import 'package:gc_wizard/widgets/common/gcw_tool.dart';
 import 'package:gc_wizard/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/gcw_multi_decoder_tool.dart';
@@ -18,12 +17,17 @@ import 'package:gc_wizard/widgets/tools/crypto_and_encodings/general_codebreaker
 import 'package:gc_wizard/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/tools/md_tools.dart';
 import 'package:gc_wizard/widgets/utils/no_animation_material_page_route.dart';
 
+//import 'package:gc_wizard/logic/tools/crypto_and_encodings/general_codebreakers/substitution_breaker/substitution_breaker.dart';
+import 'package:gc_wizard/logic/tools/crypto_and_encodings/general_codebreakers/vigenere_breaker/vigenere_breaker.dart';
+
 class MultiDecoderJobData {
   final List<GCWMultiDecoderTool> mdtTools;
+  final BuildContext context;
   final String input;
 
   MultiDecoderJobData({
     this.mdtTools = null,
+    this.context = null,
     this.input = ''
   });
 }
@@ -143,7 +147,7 @@ class MultiDecoderState extends State<MultiDecoder> {
     );
   }
 
-  _toolTitle(GCWMultiDecoderTool tool) {
+  _toolTitle(BuildContext context, GCWMultiDecoderTool tool) {
     var optionValues = tool.options.values.map((value) {
       var result = value;
 
@@ -164,7 +168,7 @@ class MultiDecoderState extends State<MultiDecoder> {
   _initOutput() {
     _currentOutput = Column(
       children: _mdtTools.map((tool) {
-        return GCWTextDivider(text: _toolTitle(tool));
+        return GCWTextDivider(text: _toolTitle(context, tool));
       }).toList()
     );
   }
@@ -173,33 +177,35 @@ class MultiDecoderState extends State<MultiDecoder> {
     return GCWAsyncExecuterParameters(
         MultiDecoderJobData(
             mdtTools : _mdtTools,
+            context: context,
             input: _currentInput
         )
     );
   }
 
-  void _calculateOutputAsync(dynamic jobData)  {
+  void _calculateOutputAsync(dynamic jobData) async {
     var output = _calculateOutput(
         jobData.mdtTools,
+        jobData.context,
         jobData.parameters.input
     );
 
     jobData.sendAsyncPort.send(output);
   }
 
-  _calculateOutput(List<GCWMultiDecoderTool> mdtTools, String input) {
+  _calculateOutput(List<GCWMultiDecoderTool> mdtTools, BuildContext context, String input) {
     var results = mdtTools.map((tool) {
       var result;
 
       try {
-        result = tool.onDecode(input);
+        result = 'ww'; //tool.onDecode(input);
       } catch(e){}
 
       if (result == null || result.toString().length == 0)
         return Container();
 
       return GCWOutput(
-        title: _toolTitle(tool),
+        title: 'xx', //_toolTitle(context, tool),
         child: result,
       );
     }).toList();
@@ -217,3 +223,54 @@ class MultiDecoderState extends State<MultiDecoder> {
     });
   }
 }
+
+void _calculateOutputAsync1(dynamic jobData) async {
+  var output = _calculateOutput(
+      jobData.mdtTools,
+      jobData.context,
+      jobData.parameters.input
+  );
+
+  jobData.sendAsyncPort.send(output);
+}
+
+_calculateOutput(List<GCWMultiDecoderTool> mdtTools, BuildContext context, String input) {
+  var results = mdtTools.map((tool) {
+    var result;
+
+    try {
+      result = tool.onDecode(input);
+    } catch(e){}
+
+    if (result == null || result.toString().length == 0)
+      return Container();
+
+    return GCWOutput(
+      title: _toolTitle(context, tool),
+      child: result,
+    );
+  }).toList();
+
+  return Column(
+      children: results
+  );
+}
+
+_toolTitle(BuildContext context, GCWMultiDecoderTool tool) {
+  var optionValues = tool.options.values.map((value) {
+    var result = value;
+
+    if (tool.internalToolName == MDT_INTERNALNAMES_COORDINATEFORMATS) {
+      result = getCoordinateFormatByKey(value).name;
+    }
+
+    return i18n(context, result.toString()) ?? result;
+  }).join(', ');
+
+  var result = tool.name;
+  if (optionValues != null && optionValues.length > 0)
+    result += ' ($optionValues)';
+
+  return result;
+}
+
