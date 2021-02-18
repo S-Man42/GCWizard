3a4b3b27fa4bfb26c7ea011a12da0e34347590a6
LudoO <ludovic.valente@gmail.com>
2021-02-19 00:12:24 +0100

    Add french (main ui ok) + settings locale changer (issue)

--- lib/i18n/app_language.dart
+++ lib/i18n/app_language.dart
@@ -6,30 +6,41 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 // from: https://medium.com/flutter-community/flutter-internationalization-the-easy-way-using-provider-and-json-c47caa4212b2
 class AppLanguage extends ChangeNotifier {
-  Locale _appLocale = Locale('en');
+ // static const Locale _defaultAppLocale = Locale('en');
+  static const Locale _defaultAppLocale = Locale('fr');
+  Locale _appLocale = _defaultAppLocale;
 
-  Locale get appLocal => _appLocale ?? Locale("en");
-  fetchLocale() async {
+  Locale get appLocal => _appLocale ?? _defaultAppLocale;
+  Future<Locale> fetchLocale() async {
     var prefs = await SharedPreferences.getInstance();
     if (prefs.getString('language_code') == null) {
-      _appLocale = Locale('en');
+      _appLocale = _defaultAppLocale;
+      //_appLocale = Locale('en', 'US');
       return null;
     }
     _appLocale = Locale(prefs.getString('language_code'));
-    return null;
+    //_appLocale = Locale(prefs.getString('language_code'), prefs.getString('countryCode'));
+    return _appLocale;
   }
-
+  // Future<Locale> fetchLocaleAndReturn() async {
+  //   fetchLocale();
+  //   return appLocal;
+  // }
 
   void changeLanguage(Locale type) async {
     var prefs = await SharedPreferences.getInstance();
-    if (_appLocale == type) {
-      return;
-    }
+    //if (_appLocale == type) {
+    // if (_appLocale.languageCode == type.languageCode) {
+    //   return;
+    // }
 
-    _appLocale = type;
+    //_appLocale = type;
+    _appLocale = Locale(type.languageCode);
     await prefs.setString('language_code', _appLocale.languageCode);
     await prefs.setString('countryCode', _appLocale.countryCode);
 
     notifyListeners();
   }
+
+
 }
