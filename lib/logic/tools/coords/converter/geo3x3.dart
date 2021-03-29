c6b12ea71851e4558ae0a552e2e32c0b031c6a14
S-Man42 <info@s-man42.de>
2021-03-29 15:54:46 +0200

    Fix Geo3x3

--- lib/logic/tools/coords/converter/geo3x3.dart
+++ lib/logic/tools/coords/converter/geo3x3.dart
@@ -2,7 +2,7 @@ import 'package:latlong/latlong.dart';
 import 'package:gc_wizard/logic/tools/coords/converter/taisukef/geo3x3/geo3x3.dart';
 
 LatLng geo3x3ToLatLon(String code) {
-  var latLon = Geo3x3.decode(code);
+  var latLon = Geo3x3.decode(code.toUpperCase());
   return LatLng(latLon[0], latLon[1]);
 }
 
