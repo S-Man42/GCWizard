0abfeb92a3fc4527aaba7a8b590f47f82b3764d3
LudoO <ludovic.valente@gmail.com>
2021-04-12 10:11:46 +0200

    Search in registry optimized

--- lib/widgets/registry.dart
+++ lib/widgets/registry.dart
@@ -275,6 +275,8 @@ import 'package:gc_wizard/widgets/tools/symbol_tables/gcw_symbol_table_tool.dart
 class Registry {
   static List<GCWTool> toolList;
 
+  static bool indexed = false;
+
   static initialize(BuildContext context) {
     toolList = [
       //MainSelection
@@ -6244,5 +6246,9 @@ class Registry {
 
       return toolWidget;
     }).toList();
+
+    //Sort once for search
+    toolList.sort((a, b) => a.toolName.toLowerCase().compareTo(b.toolName.toLowerCase()));
+
   }
 }
