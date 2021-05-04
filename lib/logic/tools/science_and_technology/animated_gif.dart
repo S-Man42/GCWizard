5fb87574505bed2e9c317db2820308b41fb52d48
Mike-3 <Mike.Binnemann@web.de>
2021-05-04 18:27:46 +0200

    create output zip-file

--- lib/logic/tools/science_and_technology/animated_gif.dart
+++ lib/logic/tools/science_and_technology/animated_gif.dart
@@ -1,28 +1,94 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:math';
 import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'package:archive/archive.dart';
+import 'package:archive/archive_io.dart';
 //import 'package:flutter/cupertino.dart';
 import 'package:image/image.dart' as img;
 
-Future <Map<String, dynamic>> analyseGif(Uint8List bytes) async {
+
+Future <Map<String, dynamic>> analyseGifAsync(dynamic jobData) async {
+  if (jobData == null) {
+    jobData.sendAsyncPort.send(null);
+    return null;
+  }
+
+  var output = await analyseGif(jobData.parameters, sendAsyncPort: jobData.sendAsyncPort);
+
+  if (jobData.sendAsyncPort != null) jobData.sendAsyncPort.send(output);
+
+  return output;
+}
+
+Future <Map<String, dynamic>> analyseGif(Uint8List bytes, {SendPort sendAsyncPort}) async {
+  var progress = 0;
   final decoder = img.findDecoderForData(bytes);
+
   if (decoder == null)
     return null;
 
   var out = Map<String, dynamic>();
   var animation = decoder.decodeAnimation(bytes);
+  int progressStep = max(animation.length ~/ 100, 1); // 100 steps
 
   var imageList = <Uint8List>[];
   var durations = <int>[];
   animation?.frames.forEach((image) {
     imageList.add(img.encodeGif(image));
     durations.add(image.duration);
+    progress++;
+    if (sendAsyncPort != null && (progress % progressStep == 0)) {
+      sendAsyncPort.send({'progress': progress / animation.length});
+    }
   });
   out.addAll({"animation": animation});
 
   // out.addAll({"image": bytes});
   out.addAll({"images": imageList});
   out.addAll({"durations": durations});
+  //out.addAll({"decoder": durations});
 
 
   return out;
 }
+
+Future<Uint8List> createZipFile(String fileName, List<Uint8List> imageList) async {
+  try {
+    String tmpDir = (await getTemporaryDirectory()).path;
+    var counter = 0;
+    var zipPath = '$tmpDir/gcwizardtmp.zip';
+
+    var encoder = ZipFileEncoder();
+    encoder.create(zipPath);
+
+    for (Uint8List imageBytes in imageList) {
+    //await imageList.forEach( async (imageBytes)  {
+      counter++;
+      var fileNameZip = fileName + '_'+ counter.toString();
+      var tmpPath = '$tmpDir/' + fileNameZip;
+      if (File(tmpPath).existsSync())
+        File(tmpPath).delete();
+
+      File temp = new File(tmpPath);
+      temp = await temp.create();
+      temp = await temp.writeAsBytes(imageBytes);
+
+      encoder.addFile(temp, fileNameZip);
+      temp.delete();
+    };
+    
+    encoder.close();
+
+    var bytes = File(encoder.zip_path).readAsBytesSync();
+    await File(encoder.zip_path).delete();
+
+    return bytes;
+  } on Exception {
+    return null;
+  }
+}
+
+
 
--- lib/widgets/tools/science_and_technology/animated_gif.dart
+++ lib/widgets/tools/science_and_technology/animated_gif.dart
@@ -5,12 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_output_text.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_text.dart';
 import 'package:gc_wizard/widgets/common/gcw_output.dart';
+import 'package:gc_wizard/widgets/common/gcw_submit_button.dart';
 import 'package:image/image.dart' as imageData;
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/science_and_technology/animated_gif.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_button.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_iconbutton.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
+import 'package:gc_wizard/widgets/common/gcw_async_executer.dart';
 import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
 import 'package:gc_wizard/widgets/common/gcw_exported_file_dialog.dart';
 import 'package:gc_wizard/widgets/common/gcw_symbol_container.dart';
@@ -19,6 +21,7 @@ import 'package:gc_wizard/widgets/utils/file_utils.dart';
 import 'package:gc_wizard/widgets/utils/file_picker.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:image/image.dart' as img;
+import 'package:intl/intl.dart';
 
 
 class AnimatedGif extends StatefulWidget {
@@ -32,7 +35,7 @@ class AnimatedGif extends StatefulWidget {
 }
 
 class AnimatedGifState extends State<AnimatedGif> {
-  Map<String, dynamic>  _outData;
+  Map<String, dynamic> _outData;
   PlatformFile _platformFile;
   GCWSwitchPosition _currentMode = GCWSwitchPosition.right;
 
@@ -40,7 +43,8 @@ class AnimatedGifState extends State<AnimatedGif> {
   Widget build(BuildContext context) {
     if (widget.platformFile != null) {
       _platformFile = widget.platformFile;
-      _analystePlatformFile(_platformFile);
+      _analysePlatformFileAsync();
+      //_analystePlatformFile(_platformFile);
     }
 
     return Column(
@@ -53,7 +57,8 @@ class AnimatedGifState extends State<AnimatedGif> {
                 if (files != null && files.length > 0) {
                   getFileData(files.first).then((bytes) {
                     _platformFile = new PlatformFile(path: files.first.path, name: files.first.name, bytes: bytes);
-                    _analystePlatformFile(_platformFile);
+                    //_analystePlatformFile(_platformFile);
+                    _analysePlatformFileAsync();
                   });
                 };
               });
@@ -64,16 +69,36 @@ class AnimatedGifState extends State<AnimatedGif> {
           text: _outData == null ? "" : _platformFile.name,
         ),
         GCWDefaultOutput(child: _buildOutput(),
-          trailing: GCWIconButton(
-            iconData: Icons.save,
-            size: IconButtonSize.SMALL,
-            iconColor: _outData == null ? Colors.grey : null,
-            onPressed: () {
-              //_outData == null ? null : _exportFiles(context, _outData);
-            },
-          )
+          trailing: Row (
+              children: <Widget>[
+                GCWIconButton(
+                  iconData: Icons.play_arrow,
+                  size: IconButtonSize.SMALL,
+                  iconColor: _outData == null ? Colors.grey : null,
+                  onPressed: () {
+                    //_outData == null ? null : _exportFiles(context, _outData);
+                  },
+                ),
+                GCWIconButton(
+                  iconData: Icons.stop,
+                  size: IconButtonSize.SMALL,
+                  iconColor: _outData == null ? Colors.grey : null,
+                  onPressed: () {
+                    //_outData == null ? null : _exportFiles(context, _outData);
+                  },
+                ),
+                GCWIconButton(
+                  iconData: Icons.save,
+                  size: IconButtonSize.SMALL,
+                  iconColor: _outData == null ? Colors.grey : null,
+                  onPressed: () {
+                    _outData == null ? null : _exportFiles(context,_platformFile.name, _outData["images"]);
+                  },
+                )
+              ]
         )
-      ],
+        )
+      ]
     );
   }
 
@@ -85,22 +110,28 @@ class AnimatedGifState extends State<AnimatedGif> {
     var durations = "";
     if (_outData["durations"] != null && _outData["durations"]?.length> 1)
       durations = _outData["durations"].map((value) => value.toString() + 'ms').join(", ");
+Image i;
+    double height;
+    if (_outData["animation"]?.width <= MediaQuery.of(context).size.width )
+      height = _outData["animation"]?.height.toDouble();
+    else
+      height = MediaQuery.of(context).size.width * _outData["animation"]?.height / _outData["animation"]?.width;
 
-    return Column(
+      return Column(
       children: <Widget>[
          Container(
-          child : Swiper(
-              itemCount:  _outData["images"]?.length,
+          child : //Image.memory(_platformFile.bytes),
+          Swiper(
+              itemCount:  _outData["animation"]?.numFrames, //_outData["images"]?.length,
               itemBuilder: (BuildContext context, int index) {
-                var i = Image.memory(_outData["images"][index]);
-                print(i.width.toString() +"x" +i.height.toString());
+                i = Image.memory(_outData["images"][index]);// Image.memory(img.encodeGif(_outData["animation"].frames[index]));
                 return i;
               },
               control: SwiperControl(),
-              pagination: SwiperPagination(),
+              pagination: FractionPaginationBuilder(),
             ),
 
-            height: _outData["animation"]?.height.toDouble(),
+            height: height,
         ),
         durations != null
           ? GCWOutput(
@@ -119,11 +150,47 @@ class AnimatedGifState extends State<AnimatedGif> {
       });
     });
   }
-  _exportFiles(BuildContext context, List<imageData.Image> gifs) async {
-    // var fileType = getFileType(_outData);
-    // var value = await saveByteDataToFile(
-    //     data.buffer.asByteData(), DateFormat('yyyyMMdd_HHmmss').format(DateTime.now()) + fileType, subDirectory: "hexstring_export");
-    //
-    // if (value != null) showExportedFileDialog(context, value['path'], fileType: fileType);
+
+  _analysePlatformFileAsync() async {
+      await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return Center(
+            child: Container(
+              child: GCWAsyncExecuter(
+                isolatedFunction: analyseGifAsync,
+                parameter: _buildJobData(),
+                onReady: (data) => _showOutput(data),
+                isOverlay: true,
+              ),
+              height: 220,
+              width: 150,
+            ),
+          );
+        },
+      );
+  }
+
+  Future<GCWAsyncExecuterParameters> _buildJobData() async {
+    return GCWAsyncExecuterParameters(_platformFile.bytes);
+  }
+
+  _showOutput(Map<String, dynamic> output) {
+    _outData = output;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {});
+    });
+  }
+
+  _exportFiles(BuildContext context, String fileName, List<Uint8List> data) async {
+    createZipFile(fileName, data).then((bytes) async {
+      var fileType = '.zip';
+      var value = await saveByteDataToFile(
+          bytes.buffer.asByteData(), DateFormat('yyyyMMdd_HHmmss').format(DateTime.now()) + fileType); //, subDirectory: "hexstring_export"
+
+      if (value != null) showExportedFileDialog(context, value['path'], fileType: fileType);
+    });
   }
 }
