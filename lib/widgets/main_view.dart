c5b1f1e66341d2ffe004ca34531f599797b3a2a7
LudoO <ludovic.valente@gmail.com>
2021-05-05 22:53:11 +0200

    remove clearIcon option in text field
    remove commented code
    
    (cherry picked from commit e47ecb9389e8ad21c640066065d3eb4aa548e884)

--- lib/widgets/main_view.dart
+++ lib/widgets/main_view.dart
@@ -187,10 +187,10 @@ class _MainViewState extends State<MainView> {
 
     _searchController.addListener(() {
       setState(() {
-        if (_searchController.text.isEmpty){
+        if (_searchController.text.isEmpty) {
           _searchText = '';
-        }else if (_searchText!= _searchController.text) {
-          _searchText =  _searchController.text;
+        } else if (_searchText != _searchController.text) {
+          _searchText = _searchController.text;
         }
       });
     });
@@ -377,11 +377,6 @@ class _MainViewState extends State<MainView> {
       ].contains(className(element.tool));
     }).toList();
 
-    // already sorted in registry
-    // _toolList.sort((a, b) {
-    //   return a.toolName.toLowerCase().compareTo(b.toolName.toLowerCase());
-    // });
-
     final List<GCWTool> _categoryList = Registry.toolList.where((element) {
       return [
         className(CoordsSelection()),
@@ -452,7 +447,6 @@ class _MainViewState extends State<MainView> {
     return _isSearching
         ? GCWTextField(
             autofocus: true,
-            clearIcon: false,
             controller: _searchController,
             icon: Icon(Icons.search, color: themeColors().mainFont()),
             hintText: i18n(context, 'common_search_hint'))
@@ -474,11 +468,6 @@ class _MainViewState extends State<MainView> {
     List<String> _queryTexts = splitWords(removeAccents(_searchText.toLowerCase()));
 
     var list = Registry.indexedTools.where((tool) {
-   // var list = Registry.toolList.where((tool) {
-   //    if (tool.indexedStrings==null) {
-   //      print("no indexedStrings");
-   //      return false;
-   //    }
       //Search result as AND result of separated words
       for (final q in _queryTexts) {
         if (!tool.indexedStrings.contains(q)) {
@@ -489,10 +478,8 @@ class _MainViewState extends State<MainView> {
     }).toList();
 
     print('found ${list.length} tools in ${stopwatch.elapsed}');
-
     stopwatch.stop();
 
     return list;
   }
-
 }
