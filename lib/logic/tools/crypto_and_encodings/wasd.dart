484f6d76a99eaec8252f57274614047a32ea6010
tzimmermann <thomas@familiezimmermann.de>
2021-08-09 19:41:12 +0200

    tool wasd encoding/decoding

---
+++ lib/logic/tools/crypto_and_encodings/wasd.dart
@@ -0,0 +1,32 @@
+// https://www.geocaching.com/geocache/GC8PJEQ
+//
+// ASSDWW.DSAWSSDW.WWDSSA.WWA.WDSASDW
+//
+// SDWSS.AWDSSA.SAWDWAS.ASSDWA.DWWASS
+
+final Map<String, List<String>> WASD_ENCODE = {
+  '0' : [''],
+  '1' : [''],
+  '2' : [''],
+  '3' : [''],
+  '4' : [''],
+  '5' : [''],
+  '6' : [''],
+  '7' : [''],
+  '8' : [''],
+  '9' : [''],
+};
+
+final Map<String, String> WASD_DECODE = {
+  'ASSDWW' : '0',
+  'DSAWSSDW' : '8',
+  'WWDSSA' : '0',
+  'WWA' : '7',
+  'WDSASDW' : '8',
+  'SDWSS' : '4',
+  'AWDSSA' : '9',
+  'SAWDWAS' : '8',
+  'ASSDWA' : '6',
+  'DWWASS' : '0',
+  '' : '',
+};
