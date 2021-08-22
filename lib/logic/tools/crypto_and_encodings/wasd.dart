0015617418ddad6851c32615791a45448d0cb0ae
tzimmermann <thomas@familiezimmermann.de>
2021-08-22 09:55:49 +0200

    refactor logic

--- lib/logic/tools/crypto_and_encodings/wasd.dart
+++ lib/logic/tools/crypto_and_encodings/wasd.dart
@@ -135,80 +135,38 @@ String decodeWASD(String input, WASD_TYPE controls, List<String> controlSet){
   return resultDecode.join('');
 }
 
-Offset analyseOffset(String encodedText) {
-  int peak = 0;
-  int valley = 0;
-  int position = 0;
-  int xOffset = 0;
-  int yOffset = 0;
-  int rightBorder = 0;
-  _CURSOR_POSITION cursor = _CURSOR_POSITION.RIGHT;
-  for (int i = 0; i < encodedText.length; i++) {
-    switch (encodedText[i]){
-      case 'W':
-        if (cursor == _CURSOR_POSITION.LEFT)
-          rightBorder = 1;
-        position++;
-        if (position > peak)
-          peak = position;
-        break;
-      case 'S':
-        if (cursor == _CURSOR_POSITION.LEFT)
-          rightBorder = 1;
-        position--;
-        if (position < valley)
-          valley = position;
-        break;
-      case 'A':
-        xOffset = xOffset + _SEGMENT_LENGTH + 1;
-        cursor = _CURSOR_POSITION.LEFT;
-        break;
-    }
-  }
-  if (peak == 2) {
-    yOffset = 0;
-  }
-  else if (valley == -2) {
-    yOffset = -_SEGMENT_LENGTH - _SEGMENT_LENGTH - 1;
-  } else {
-    yOffset = -_SEGMENT_LENGTH - 1;
-  }
-print('xoffset '+xOffset.toString());
-  return Offset(xOffset, yOffset, rightBorder);
-}
-
 String decodeWASDGraphic(String input, WASD_TYPE controls, List<String> controlSet){
   if (input == '' || input == null)
     return '';
 
   int x = 0;
   int y = 0;
-  int maxX = 0;
-  int maxY = 0;
-  int minX = 0;
-  int minY = 0;
-  _CURSOR_POSITION cursor = _CURSOR_POSITION.RIGHT;
-  int leftBorder = 0;
-  int rightBorder = 0;
+  int maxLetterX = 0;
+  int maxLetterY = 0;
+  int minLetterX = 0;
+  int minLetterY = 0;
+  int maxSentenceX = 0;
+  int maxSentenceY = 0;
+  int minSentenceX = 0;
+  int minSentenceY = 0;
 
-  Map<String, String> world = new Map();
+  Map<String, String> sentence = new Map();
 
   var direction = _WASD_DIRECTION.UP;
   
-  List<String> wordList = _normalizeDecodingInput(input, controls, controlSet).split(' ');
-  String word = '';
+  _normalizeDecodingInput(input, controls, controlSet).split(' ').forEach((word) {
 
-  for (int i = 0; i < wordList.length; i++) {
-    word = wordList[i];
-    y = analyseOffset(word).yOffset;
+    // draw picture per letter
+    // transform/normalize picture
+    // align picture in world
+
+    y = 0;
+    x = 0;
+    Map<String, String> letter = new Map();
 
     word.split('').forEach((element) {
       switch (element){
         case 'W':  // forward, up
-        if (cursor == _CURSOR_POSITION.LEFT)
-          leftBorder = 1;
-        if (cursor == _CURSOR_POSITION.RIGHT)
-          rightBorder = 1;
           switch (direction) {
             case _WASD_DIRECTION.UP:
               y--;
@@ -223,23 +181,19 @@ String decodeWASDGraphic(String input, WASD_TYPE controls, List<String> controlS
               x++;
               break;
           }
-          if (y < minY) minY = y;
-          if (x < minX) minX = x;
-          if (y > maxY) maxY = y;
-          if (x > maxX) maxX = x;
+          if (y < minLetterY) minLetterY = y;
+          if (x < minLetterX) minLetterX = x;
+          if (y > maxLetterY) maxLetterY = y;
+          if (x > maxLetterX) maxLetterX = x;
           for (int i = 0; i < _SEGMENT_LENGTH; i++) {
-            y--; world[x.toString() + '|' + (y).toString()] = '1';
-            if (y < minY)
-              minY = y;
+            y--; letter[x.toString() + '|' + (y).toString()] = '1';
+            if (y < minLetterY)
+              minLetterY = y;
           }
           direction = _WASD_DIRECTION.UP;
           break;
 
         case 'S':  // back, down
-          if (cursor == _CURSOR_POSITION.LEFT)
-            leftBorder = 1;
-          if (cursor == _CURSOR_POSITION.RIGHT)
-            rightBorder = 1;
           switch (direction) {
             case _WASD_DIRECTION.UP:
               y--;
@@ -254,20 +208,19 @@ String decodeWASDGraphic(String input, WASD_TYPE controls, List<String> controlS
               x++;
               break;
           }
-          if (y < minY) minY = y;
-          if (x < minX) minX = x;
-          if (y > maxY) maxY = y;
-          if (x > maxX) maxX = x;
+          if (y < minLetterY) minLetterY = y;
+          if (x < minLetterX) minLetterX = x;
+          if (y > maxLetterY) maxLetterY = y;
+          if (x > maxLetterX) maxLetterX = x;
           for (int i = 0; i < _SEGMENT_LENGTH; i++) {
-            y++; world[x.toString() + '|' + (y).toString()] = '1';
-            if (y > maxY)
-              maxY = y;
+            y++; letter[x.toString() + '|' + (y).toString()] = '1';
+            if (y > maxLetterY)
+              maxLetterY = y;
           }
           direction = _WASD_DIRECTION.DOWN;
           break;
 
         case 'A':  // left
-          cursor = _CURSOR_POSITION.LEFT;
           switch (direction) {
             case _WASD_DIRECTION.UP:
               y--;
@@ -282,20 +235,19 @@ String decodeWASDGraphic(String input, WASD_TYPE controls, List<String> controlS
               x++;
               break;
           }
-          if (y < minY) minY = y;
-          if (x < minX) minX = x;
-          if (y > maxY) maxY = y;
-          if (x > maxX) maxX = x;
+          if (y < minLetterY) minLetterY = y;
+          if (x < minLetterX) minLetterX = x;
+          if (y > maxLetterY) maxLetterY = y;
+          if (x > maxLetterX) maxLetterX = x;
           for (int i = 0; i < _SEGMENT_LENGTH; i++) {
-            x--; world[x.toString() + '|' + (y).toString()] = '1';
-            if (x < minX)
-              minX = x;
+            x--; letter[x.toString() + '|' + (y).toString()] = '1';
+            if (x < minLetterX)
+              minLetterX = x;
           }
           direction = _WASD_DIRECTION.LEFT;
           break;
 
         case 'D':  // right
-          cursor = _CURSOR_POSITION.RIGHT;
           switch (direction) {
             case _WASD_DIRECTION.UP:
               y--;
@@ -310,40 +262,43 @@ String decodeWASDGraphic(String input, WASD_TYPE controls, List<String> controlS
               x++;
               break;
           }
-          if (y < minY) minY = y;
-          if (x < minX) minX = x;
-          if (y > maxY) maxY = y;
-          if (x > maxX) maxX = x;
+          if (y < minLetterY) minLetterY = y;
+          if (x < minLetterX) minLetterX = x;
+          if (y > maxLetterY) maxLetterY = y;
+          if (x > maxLetterX) maxLetterX = x;
           for (int i = 0; i < _SEGMENT_LENGTH; i++) {
-            x++; world[x.toString() + '|' + (y).toString()] = '1';
-            if (x > maxX)
-              maxX = x;
+            x++; letter[x.toString() + '|' + (y).toString()] = '1';
+            if (x > maxLetterX)
+              maxLetterX = x;
           }
           direction = _WASD_DIRECTION.RIGHT;
           break;
       }
-    });
-    print('maxx '+maxX.toString());
-    if (i < wordList.length - 1)
-      if (cursor == _CURSOR_POSITION.LEFT)
-        x = maxX + _SEGMENT_LENGTH + 2 + rightBorder + analyseOffset(wordList[i + 1]).xOffset + analyseOffset(word).leftBorder;
-      else
-        x = maxX + 2 + rightBorder + analyseOffset(wordList[i + 1]).xOffset + analyseOffset(wordList[i + 1]).leftBorder;
+    }); // for Each letter
+    print('x: '+minLetterX.toString()+' '+maxLetterX.toString());
+    print('y: '+minLetterY.toString()+' '+maxLetterY.toString());
+    print(letter);
+    // transform/normalize letter
+    maxSentenceX = maxSentenceX + maxLetterX + 2;
+    if (maxLetterY > maxSentenceY)
+      maxSentenceY = maxLetterY;
 
-  };
+    // add letter to sentence
 
-  var binaryWorld = List.generate(maxX - minX + 1, (y) => List(maxY - minY + 1), growable: false);
-  world.forEach((key, value) {
-    x = - minX + int.parse(key.split('|')[0]);
-    y = - minY + int.parse(key.split('|')[1]);
+  }); // forEach word
+
+  var binaryWorld = List.generate(maxSentenceX - minSentenceX + 1, (y) => List(maxSentenceY - minSentenceY + 1), growable: false);
+  sentence.forEach((key, value) {
+    x = - minSentenceX + int.parse(key.split('|')[0]);
+    y = - minSentenceY + int.parse(key.split('|')[1]);
     binaryWorld[x][y] = value;
   });
   String outputLine = '##';
   List<String> output = new List();
-  output.add(outputLine.padRight(maxX + 1, '#'));
-  for (y = 0; y < maxY - minY + 1; y++) {
+  output.add(outputLine.padRight(maxSentenceX + 1, '#'));
+  for (y = 0; y < maxSentenceY - minSentenceY + 1; y++) {
     outputLine = '##';
-    for (x = 0; x < maxX - minX + 1; x++) {
+    for (x = 0; x < maxSentenceX - minSentenceX + 1; x++) {
       if (binaryWorld[x][y] == null)
         outputLine = outputLine + '#';
       else
