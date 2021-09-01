518e312ffec603944d8bac214c508fed3b7d42aa
Mike-3 <Mike.Binnemann@web.de>
2021-09-01 18:18:06 +0200

    off color changed

--- lib/widgets/tools/science_and_technology/segment_display/base/n_segment_display.dart
+++ lib/widgets/tools/science_and_technology/segment_display/base/n_segment_display.dart
@@ -89,7 +89,7 @@ class NSegmentDisplayState extends State<NSegmentDisplay> {
     Canvas canvas = Canvas(recorder);
     final size = context.size;
     final painter = SegmentDisplayPainter(context, widget.type, _segments, (key, value) {},
-        customPaint: widget.customPaint, segment_color_on: Colors.black, segment_color_off: Colors.black12);
+        customPaint: widget.customPaint, segment_color_on: Colors.black, segment_color_off: Colors.white);
 
     canvas.save();
     painter.paint(canvas, size);
