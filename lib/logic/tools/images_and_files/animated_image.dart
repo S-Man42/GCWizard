c6ad38d94fe910f44ace33ff379852355d192eaf
Mike-3 <Mike.Binnemann@web.de>
2021-09-15 16:25:59 +0200

    lib barcode_scanner

--- lib/logic/tools/images_and_files/animated_image.dart
+++ lib/logic/tools/images_and_files/animated_image.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
 import 'dart:isolate';
 import 'dart:math';
 import 'dart:typed_data';
 import 'package:gc_wizard/widgets/utils/file_utils.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:archive/archive_io.dart';
 import 'package:image/image.dart' as Image;
 
 Future<Map<String, dynamic>> analyseImageAsync(dynamic jobData) async {
--- lib/logic/tools/images_and_files/qr_code.dart
+++ lib/logic/tools/images_and_files/qr_code.dart
@@ -1,15 +1,60 @@
 import 'dart:async';
 import 'dart:typed_data';
+import 'dart:ui';
+import 'package:barcode_scanner/barcode_scanning_data.dart';
 import 'package:gc_wizard/theme/fixed_colors.dart';
+import 'package:gc_wizard/widgets/utils/file_utils.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 import 'dart:ui' as ui;
+//import 'package:google_ml_kit/google_ml_kit.dart' as google;
+import 'package:image/image.dart' as Image;
+import 'package:barcode_scanner/scanbot_barcode_sdk.dart' as sacnner1;
+import 'package:barcode_scanner/scanbot_sdk_models.dart'  as sacnner2;
+//import io.scanbot.sdk.ScanbotSDKInitializer
+
+
 
 /// Parse to code string with uint8list
 Future<String> scanBytes(Uint8List bytes) async {
   if (bytes == null) return null;
   try {
-    return scanner.scanBytes(bytes);
-  } catch (e) {}
+    //var ocr =google.BarcodeScanner();
+
+    // // Below example uses metadata values based on an RGBA-encoded 1080x1080 image
+    // final planeMetadata = google.InputImagePlaneMetadata(
+    //   width: 420,
+    //   height: 420,
+    //   bytesPerRow: 420 * 4,
+    // );
+    //
+    // final imageMetadata = google.InputImageData(
+    //   size: Size(420, 420),
+    //   planeData: List.from([planeMetadata]),
+    //   inputImageFormat : google.InputImageFormat.BGRA8888,
+    // );
+    //
+    // var inputImage = google.InputImage.fromBytes(bytes: bytes,
+    //     inputImageData: imageMetadata);
+    var file = await createTmpFile('png',bytes);
+    // var inputImage = google.InputImage.fromFilePath(file.path);
+    //
+    // var codes = await ocr.processImage(inputImage);
+    // if (codes != null)
+    //   return codes.map((code ) {
+    //     return code.value.toString();
+    //     }).join("\n");
+    //scanner2.
+    //ScanbotSDKInitializer().initialize(this)
+    var config = sacnner2.ScanbotSdkConfig(          licenseKey: "",          loggingEnabled: true      );
+    await sacnner1.ScanbotBarcodeSdk.initScanbotSdk(config);
+    var codes = await sacnner1.ScanbotBarcodeSdk.detectFromImageFile(file.uri, BarcodeFormat.values.toList());
+    if (codes != null)
+      return codes.barcodeItems.map((code ) {
+        return code.text;
+        }).join("\n");
+  } catch (e) {
+    ;
+  }
   return null;
 }
 
