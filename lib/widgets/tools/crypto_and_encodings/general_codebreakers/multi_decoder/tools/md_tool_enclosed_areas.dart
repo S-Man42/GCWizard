8b716b319db8e0ffc65bd7e13d28a03bec01dabb
S-Man42 <info@s-man42.de>
2021-12-06 19:59:41 +0100

    fixes #776 Switch Bug

--- lib/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/tools/md_tool_enclosed_areas.dart
+++ lib/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/tools/md_tool_enclosed_areas.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/enclosed_areas.dart';
+import 'package:gc_wizard/widgets/common/base/gcw_dropdownbutton.dart';
+import 'package:gc_wizard/widgets/common/gcw_stateful_dropdownbutton.dart';
 import 'package:gc_wizard/widgets/common/gcw_twooptions_switch.dart';
 import 'package:gc_wizard/widgets/tools/crypto_and_encodings/general_codebreakers/multi_decoder/gcw_multi_decoder_tool.dart';
 
@@ -8,6 +10,8 @@ import '../gcw_multi_decoder_tool_configuration.dart';
 
 const MDT_INTERNALNAMES_ENCLOSEDAREAS = 'multidecoder_tool_enclosedareas_title';
 const MDT_ENCLOSEDAREAS_OPTION_MODE = 'multidecoder_tool_bacon_option_mode';
+const MDT_ENCLOSEDAREAS_OPTION_WITH4 = 'enclosedareas_with4';
+const MDT_ENCLOSEDAREAS_OPTION_WITHOUT4 = 'enclosedareas_without4';
 
 class MultiDecoderToolEnclosedAreas extends GCWMultiDecoderTool {
   MultiDecoderToolEnclosedAreas({Key key, int id, String name, Map<String, dynamic> options, BuildContext context})
@@ -17,21 +21,21 @@ class MultiDecoderToolEnclosedAreas extends GCWMultiDecoderTool {
             name: name,
             internalToolName: MDT_INTERNALNAMES_ENCLOSEDAREAS,
             onDecode: (String input, String key) {
-              return decodeEnclosedAreas(input, options[MDT_ENCLOSEDAREAS_OPTION_MODE] == 'enclosedareas_with4');
+              return decodeEnclosedAreas(input, options[MDT_ENCLOSEDAREAS_OPTION_MODE] == MDT_ENCLOSEDAREAS_OPTION_WITH4);
             },
             options: options,
             configurationWidget: GCWMultiDecoderToolConfiguration(widgets: {
-              MDT_ENCLOSEDAREAS_OPTION_MODE: GCWTwoOptionsSwitch(
-                value: options[MDT_ENCLOSEDAREAS_OPTION_MODE] == 'enclosedareas_with4'
-                    ? GCWSwitchPosition.left
-                    : GCWSwitchPosition.right,
-                notitle: true,
-                leftValue: i18n(context, 'enclosedareas_with4'),
-                rightValue: i18n(context, 'enclosedareas_without4'),
-                onChanged: (value) {
-                  options[MDT_ENCLOSEDAREAS_OPTION_MODE] =
-                      value == GCWSwitchPosition.left ? 'enclosedareas_with4' : 'enclosedareas_without4';
+              MDT_ENCLOSEDAREAS_OPTION_MODE: GCWStatefulDropDownButton(
+                value: options[MDT_ENCLOSEDAREAS_OPTION_MODE],
+                onChanged: (newValue) {
+                  options[MDT_ENCLOSEDAREAS_OPTION_MODE] = newValue;
                 },
+                items: [MDT_ENCLOSEDAREAS_OPTION_WITH4, MDT_ENCLOSEDAREAS_OPTION_WITHOUT4].map((mode) {
+                  return GCWDropDownMenuItem(
+                    value: mode,
+                    child: i18n(context, mode),
+                  );
+                }).toList(),
               )
             }));
 }
