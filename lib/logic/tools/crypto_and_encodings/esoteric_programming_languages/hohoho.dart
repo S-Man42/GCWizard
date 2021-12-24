c3886a10a75d816816b4c841102dfeef3be70b00
tzimmermann <thomas@familiezimmermann.de>
2021-12-24 12:54:47 +0100

    build the skeleton

---
+++ lib/logic/tools/crypto_and_encodings/esoteric_programming_languages/hohoho.dart
@@ -0,0 +1,30 @@
+// https://arxiv.org/pdf/1712.06259.pdf
+// https://www.geocaching.com/geocache/GC9GFQM_hohoho
+//
+// Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Hohohohohoho! Ho! Hoho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Hoho! Ho! Hohoho! Ho! Ho! Hoho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Hoho! Hohohohohohohohoho! Hoho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Hoho! Hohohohohohohohohohohohohohohohohohohohoho! Hohohohohoho! Hohohohohoho! Hoho! Ho! Ho! Hoho! Hoho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Hoho! Hoho! Ho! Ho! Hoho! Hohohohohoho! Hoho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Ho! Hoho! Hohoho! Ho! Ho!
+// HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo Hohoho hohoho HoHoho HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo hoHoHo hohoHo HoHoho HoHoHo HoHoHo HoHoHo hoHoho hohoho hohoho hoHoho HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo hoHoho hohoho hohoho hohoho hohoho hohoho hohoho hoHoho hohoho hoHoho hohoho hoHoho HoHoHo hoHoho HoHoHo HoHoHo HoHoHo HoHoHo HoHoHo hoHoho HoHoHo hoHoho hohoho hoHoho HoHoHo HoHoHo HoHoHo HoHoHo hoHoho hoHoHo
+//
+//
+
+class HohohoOutput {
+  String output = '';
+  String error = '';
+  List<String> debug;
+
+  HohohoOutput(this.output, this.error, this.debug);
+}
+
+
+HohohoOutput interpretHohoho(String plainText, {String STDIN}){
+  // convert to hohoho
+  // convert to Brainfck
+  // interpret
+  // return result
+}
+
+String generateHohoho(String OutputText){
+  // generate Brainfck
+  // transfer to hohoho
+  // normalize
+  // return result
+}
