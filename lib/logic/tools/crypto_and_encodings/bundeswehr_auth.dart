af85b19cabc8a09505d7d3d711bb47d11702d465
tzimmermann <thomas@familiezimmermann.de>
2022-04-06 19:52:52 +0200

    work in progress

--- lib/logic/tools/crypto_and_encodings/bundeswehr_auth.dart
+++ lib/logic/tools/crypto_and_encodings/bundeswehr_auth.dart
@@ -1,12 +1,82 @@
 
+import 'dart:math';
+
 import 'package:flutter/services.dart';
 
-String buildAuthBundeswehr(){
-  List<String> result = [];
 
-  return result.join(' ');
+class TableTitle{
+  final List<String> row;
+  final List<String> column;
+
+  TableTitle(this.row, this.column);
 }
 
-String checkAuthBundeswehr(){
+String buildAuthBundeswehr(String currentCallSign, String currentLetterAuth, String currentLetterCallSign, List<String> rowTitle, List<String> colTitle, List<String> numeralCode, Map<String, Map<String, String>> authTable){
+  if (currentCallSign.split('').contains(currentLetterCallSign)) {
+
+    if (['A', 'D', 'E', 'G', 'H', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U',].contains(currentLetterCallSign)) {
+      List<String> result = [];
+      List<String> tupel1 = [];
+      for (int i = 0; i < numeralCode.length; i++) {
+        if (numeralCode[i] == currentLetterCallSign){
+           String t1 = colTitle[i % 13];
+           String t2 = rowTitle[i ~/ 13];
+           tupel1.add(t1 + t2);
+           tupel1.add(t2 + t1);
+        }
+      }
+
+      String authCode = authTable[currentLetterCallSign][currentLetterAuth];
+      List<String> tupel2 = [];
+      for (int i = 0; i < numeralCode.length; i++) {
+        if (numeralCode[i] == authCode.split('')[0]){
+          String t1 = colTitle[i % 13];
+          String t2 = rowTitle[i ~/ 13];
+          tupel2.add(t1 + t2);
+          tupel2.add(t2 + t1);
+        }
+      }
+
+      List<String> tupel3 = [];
+      for (int i = 0; i < numeralCode.length; i++) {
+        if (numeralCode[i] == authCode.split('')[1]){
+          String t1 = colTitle[i % 13];
+          String t2 = rowTitle[i ~/ 13];
+          tupel3.add(t1 + t2);
+          tupel3.add(t2 + t1);
+        }
+      }
+
+      int limit = max(tupel1.length, tupel2.length);
+      if (tupel3.length > limit)
+        limit = tupel3.length;
+      String line = '';
+      for (int i = 0; i < limit; i++){
+        line = '';
+        if (i < tupel1.length)
+          line = line + tupel1[i] + '  ';
+        else
+          line = line + '  ' + '  ';
+
+        if (i < tupel2.length)
+          line = line + tupel2[i] + '  ';
+        else
+          line = line + '  ' + '  ';
+
+        if (i < tupel3.length)
+          line = line + tupel3[i] + '  ';
+        else
+          line = line + '  ' + '  ';
+
+        result.add(line);
+      }
+      return result.join('\n');
+    }
+    return 'ERROR: LETTER NOT PART AUF AUTHENTICATION CODE';
+  }
+   return 'ERROR: INVALID LETTER FROM CALLSIGN';
+}
+
+String checkAuthBundeswehr(String currentCallSign, String currentAuth){
   return 'bundeswehr_auth_correct';
 }
--- lib/widgets/tools/crypto_and_encodings/bundeswehr_auth.dart
+++ lib/widgets/tools/crypto_and_encodings/bundeswehr_auth.dart
@@ -1,11 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gc_wizard/i18n/app_localizations.dart';
 import 'package:gc_wizard/logic/tools/crypto_and_encodings/bundeswehr_auth.dart';
-import 'package:gc_wizard/logic/tools/crypto_and_encodings/polybios.dart';
-import 'package:gc_wizard/widgets/common/gcw_alphabetdropdown.dart';
+import 'package:gc_wizard/widgets/common/base/gcw_output_text.dart';
 import 'package:gc_wizard/widgets/common/base/gcw_textfield.dart';
 import 'package:gc_wizard/widgets/common/gcw_default_output.dart';
-import 'package:gc_wizard/widgets/common/gcw_text_divider.dart';
+import 'package:gc_wizard/widgets/common/gcw_expandable.dart';
 import 'package:gc_wizard/widgets/common/gcw_twooptions_switch.dart';
 
 class BundeswehrAuth extends StatefulWidget {
@@ -16,11 +18,22 @@ class BundeswehrAuth extends StatefulWidget {
 class BundeswehrAuthState extends State<BundeswehrAuth> {
   var _inputAuthController;
   var _callSignController;
-  var _letterController;
+  var _letterControllerAuth;
+  var _letterControllerCallSign;
 
   String _currentAuthInput = '';
   String _currentCallSign = '';
-  String _currentLetter = '';
+  String _currentLetterAuth = '';
+  String _currentLetterCallSign = '';
+
+  List<String> _colTitle = [];
+  List<String> _rowTitle = [];
+
+  String _authTableString = '';
+  Map<String, Map<String, String>> _authTable = {};
+
+  String _numeralCodeString = '';
+  List<String> _numeralCode = [];
 
   var _currentMode = GCWSwitchPosition.right;
 
@@ -29,23 +42,33 @@ class BundeswehrAuthState extends State<BundeswehrAuth> {
     super.initState();
     _inputAuthController = TextEditingController(text: _currentAuthInput);
     _callSignController = TextEditingController(text: _currentCallSign);
-    _letterController = TextEditingController(text: _currentLetter);
+    _letterControllerAuth = TextEditingController(text: _currentLetterAuth);
+    _letterControllerCallSign = TextEditingController(text: _currentLetterCallSign);
+
+    _colTitle = _buildTitles().column;
+    _rowTitle = _buildTitles().row;
+
+    _buildAuthTable();
+    _buildNumeralCode();
+
   }
 
   @override
   void dispose() {
     _inputAuthController.dispose();
     _callSignController.dispose();
-    _letterController.dispose();
+    _letterControllerAuth.dispose();
+    _letterControllerCallSign.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: <Widget>[
         GCWTwoOptionsSwitch(
+          rightValue: i18n(context, 'bundeswehr_auth_mode_auth_check'),
+          leftValue: i18n(context, 'bundeswehr_auth_mode_auth'),
           value: _currentMode,
           onChanged: (value) {
             setState(() {
@@ -53,8 +76,18 @@ class BundeswehrAuthState extends State<BundeswehrAuth> {
             });
           },
         ),
-        _currentMode == GCWSwitchPosition.left
-        ? Column(
+        GCWTextField(
+          controller: _callSignController,
+          hintText: i18n(context, 'bundeswehr_auth_call_sign'),
+          inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[A-Z]')),],
+          onChanged: (text) {
+            setState(() {
+              _currentCallSign = text;
+            });
+          },
+        ),
+        _currentMode == GCWSwitchPosition.right
+        ? Column( // decode - checkAuth
           children: <Widget>[
             GCWTextField(
               controller: _inputAuthController,
@@ -64,15 +97,59 @@ class BundeswehrAuthState extends State<BundeswehrAuth> {
                 });
               },
             ),
-
           ],
         )
-        : Column(
+        : Column( // encode - auth
           children: <Widget>[
-
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: GCWTextField(
+                      controller: _letterControllerAuth,
+                      hintText: i18n(context, 'bundeswehr_auth_letter_auth'),
+                      inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[VWXYZ]')),],
+                      onChanged: (text) {
+                        setState(() {
+                          _currentLetterAuth = text;
+                        });
+                      },
+                    ),
+                ),
+                Expanded(
+                    child: GCWTextField(
+                      controller: _letterControllerCallSign,
+                      hintText: i18n(context, 'bundeswehr_auth_letter_callsign'),
+                      inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[A-Z]')),],
+                      onChanged: (text) {
+                        setState(() {
+                          _currentLetterCallSign = text;
+                        });
+                      },
+                    ),
+                )
+              ]
+            ),
           ],
         ),
-        GCWDefaultOutput(child: _calculateOutput() //_currentOutput == null ? '' : _currentOutput
+        GCWExpandableTextDivider(
+          text: i18n(context, 'bundeswehr_auth_numeral_code'),
+          child:  GCWOutputText(
+                    text: _numeralCodeString,
+                    isMonotype: true,
+                 )
+        ),
+        GCWExpandableTextDivider(
+          text: i18n(context, 'bundeswehr_auth_authentification_code'),
+          child:  GCWOutputText(
+                    text: _authTableString,
+                    isMonotype: true,
+                  )
+          ),
+        GCWDefaultOutput(
+          child: GCWOutputText(
+            text: _calculateOutput(),
+            isMonotype: true,
+          ),
         )
       ],
     );
@@ -81,12 +158,88 @@ class BundeswehrAuthState extends State<BundeswehrAuth> {
   _calculateOutput() {
     var output = '';
 
-    if (_currentMode == GCWSwitchPosition.left) {
-        output = buildAuthBundeswehr();
+    if (_currentMode == GCWSwitchPosition.right) {
+      output = i18n(context, checkAuthBundeswehr(_currentCallSign, _currentAuthInput));
     } else {
-        output = i18n(context, checkAuthBundeswehr());
+      output = buildAuthBundeswehr(_currentCallSign, _currentLetterAuth, _currentLetterCallSign, _rowTitle, _colTitle, _numeralCode, _authTable);
     }
 
     return output ?? '';
   }
+
+  String _buildAuthTable(){
+    List<int> authCode = [];
+    var random = new Random();
+    int rnd = 0;
+    while (authCode.length != 65) {
+      rnd = random.nextInt(100);
+      if (authCode.contains(rnd)) {
+
+      } else {
+        authCode.add(rnd);
+      }
+    }
+
+    int i = 0;
+    ['A', 'D', 'E', 'G', 'H', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U',].forEach((row) {
+      Map<String, String> authTableRow = {'V': '0', 'W': '0', 'X': '0', 'Y': '0', 'Z':'0'};
+      ['V', 'W', 'X', 'Y', 'Z', ].forEach((col) {
+        authTableRow[col] = authCode[i].toString().padLeft(2, '0');
+        i++;
+      });
+      _authTable[row] = authTableRow;
+    });
+
+    i = 0;
+    _authTableString = '     V   W   X   Y   Z \n-----------------------\n';
+    ['A', 'D', 'E', 'G', 'H', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U',].forEach((element) {
+      _authTableString = _authTableString + ' ' + element + '   ' + authCode[i].toString().padLeft(2, '0')
+          + '  ' + authCode[i + 1].toString().padLeft(2, '0')
+          + '  ' + authCode[i + 2].toString().padLeft(2, '0')
+          + '  ' + authCode[i + 3].toString().padLeft(2, '0')
+          + '  ' + authCode[i + 4].toString().padLeft(2, '0') + '\n';
+      i = i + 5;
+    });
+  }
+
+  String _buildNumeralCode(){
+    _numeralCode.addAll('0123456789ABC'.split(''));
+    _numeralCode.addAll('D0123456789EF'.split(''));
+    _numeralCode.addAll('GH0123456789I'.split(''));
+    _numeralCode.addAll('JKL0123456789'.split(''));
+    _numeralCode.addAll('9MNO012345678'.split(''));
+    _numeralCode.addAll('89PQR01234567'.split(''));
+    _numeralCode.addAll('789STU0123456'.split(''));
+    _numeralCode.addAll('6789VWX012345'.split(''));
+    _numeralCode.addAll('56789YZA01234'.split(''));
+    _numeralCode.addAll('456789DEG0123'.split(''));
+    _numeralCode.addAll('3456789HIL012'.split(''));
+    _numeralCode.addAll('23456789NOR01'.split(''));
+    _numeralCode.addAll('123456789STU0'.split(''));
+
+    int i = 0;
+    _numeralCodeString = '   ' + _colTitle.join(' ') +'\n----------------------------\n ';
+    _rowTitle.forEach((row) {
+      _numeralCodeString = _numeralCodeString + row + ' ';
+      for (int j = 0; j < 13; j++) {
+        _numeralCodeString = _numeralCodeString + _numeralCode[i * 13 + j] + ' ';
+      }
+      i++;
+      _numeralCodeString = _numeralCodeString + '\n ';
+    });
+  }
+
+  TableTitle _buildTitles(){
+    List<String> alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+    var random = new Random();
+    int rnd = 0;
+    String description = '';
+    while (alphabet.length > 0) {
+      rnd = random.nextInt(alphabet.length);
+      description = description + alphabet[rnd];
+      alphabet.removeAt(rnd);
+    }
+    return TableTitle(description.substring(0, 13).split(''), description.substring(13).split(''));
+  }
+
 }
