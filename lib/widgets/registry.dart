041cc1d789ed5f567e0060f8618f658f20f194d9
Mike-3 <Mike.Binnemann@web.de>
2022-05-09 17:08:56 +0200

    alien_mushrooms, my_little_pony

--- lib/widgets/registry.dart
+++ lib/widgets/registry.dart
@@ -2733,6 +2733,9 @@ initializeRegistry(BuildContext context) {
     GCWSymbolTableTool(symbolKey: 'alchemy_alphabet', symbolSearchStrings: [
       'symbol_alchemy_alphabet',
     ]),
+    GCWSymbolTableTool(symbolKey: 'alien_mushrooms', symbolSearchStrings: [
+      'symbol_alien_mushrooms',
+    ]),
     GCWSymbolTableTool(symbolKey: 'angerthas_cirth', symbolSearchStrings: [
       'symbol_lordoftherings',
       'symbol_runes',
@@ -3213,6 +3216,9 @@ initializeRegistry(BuildContext context) {
         symbolKey: 'murraybaudot', symbolSearchStrings: ['ccitt', 'symbol_murraybaudot', 'teletypewriter']),
     GCWSymbolTableTool(symbolKey: 'musica', symbolSearchStrings: [
       'symbol_musica',
+    ]),
+    GCWSymbolTableTool(symbolKey: 'my_little_pony', symbolSearchStrings: [
+      'symbol_my_little_pony',
     ]),
     GCWSymbolTableTool(symbolKey: 'nazcaan', symbolSearchStrings: [
       'symbol_nazcaan',
