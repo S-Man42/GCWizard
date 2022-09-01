5f296b3159fc8ef06db24231ff91a1075d7eae91
Mike-3 <Mike.Binnemann@web.de>
2022-09-01 07:05:30 +0200

    date format

--- lib/widgets/tools/science_and_technology/date_and_time/day_of_the_year.dart
+++ lib/widgets/tools/science_and_technology/date_and_time/day_of_the_year.dart
@@ -5,6 +5,7 @@ import 'package:gc_wizard/widgets/common/gcw_datetime_picker.dart';
 import 'package:gc_wizard/widgets/common/gcw_text_divider.dart';
 import 'package:gc_wizard/widgets/utils/common_widget_utils.dart';
 import 'package:gc_wizard/logic/tools/science_and_technology/date_and_time/day_of_the_year.dart';
+import 'package:intl/intl.dart';
 
 class DayOfTheYear extends StatefulWidget {
   @override
@@ -25,10 +26,11 @@ class DayOfTheYearState extends State<DayOfTheYear> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        GCWTextDivider(text: i18n(context, 'dates_weekday_date')),
+        GCWTextDivider(text: i18n(context, 'dates_day_of_the_year_date')),
         GCWDateTimePicker(
           config: {DateTimePickerConfig.DAY_OF_THE_YEAR},
           datetime: _currentDate,
+          maxDays: 366,
           onChanged: (value) {
             setState(() {
               _currentDate = value['datetime'];
@@ -44,8 +46,10 @@ class DayOfTheYearState extends State<DayOfTheYear> {
     var outputData = calculateDayInfos(_currentDate.year, _currentDate.difference(new DateTime(_currentDate.year)).inDays + 1);
     if (outputData == null) return Container();
 
+    DateFormat formatter = DateFormat('yyyy-MM-dd');;
+
     var rows = columnedMultiLineOutput(context, [
-      [i18n(context, 'dates_weekday_date'), outputData.date.toString()],
+      [i18n(context, 'dates_weekday_date'), formatter.format(outputData.date)],
       [i18n(context, 'dates_weekday'), i18n(context, WEEKDAY[outputData.weekday])],
       [i18n(context, 'dates_weeknumber'), outputData.weeknumber]
     ]);
