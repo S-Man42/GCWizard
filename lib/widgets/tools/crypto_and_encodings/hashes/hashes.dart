4f8d01b383bc6c82300dc01cd1f0bf3c59008dcf
Mike-3 <Mike.Binnemann@web.de>
2022-10-06 18:26:44 +0200

    Update hashes.dart

--- lib/widgets/tools/crypto_and_encodings/hashes/hashes.dart
+++ lib/widgets/tools/crypto_and_encodings/hashes/hashes.dart
@@ -8,7 +8,7 @@ class _DefaultHash extends StatefulWidget {
   final Function hashFunction;
   final bool keyRequired;
 
-  const _DefaultHash({Key key, this.hashFunction, this.keyRequired}) : super(key: key);
+  const _DefaultHash({Key key, this.hashFunction, this.keyRequired = false}) : super(key: key);
 
   @override
   _DefaultHashState createState() => _DefaultHashState();
@@ -29,7 +29,7 @@ class _DefaultHashState extends State<_DefaultHash> {
             });
           },
         ),
-        widget.keyRequired == true
+        widget.keyRequired
           ? GCWTextField(
             hintText: i18n(context, 'common_key'),
             onChanged: (value) {
