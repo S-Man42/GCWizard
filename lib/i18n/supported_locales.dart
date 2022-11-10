ac352910de54e87961b44ef6d68c6c9648d912cf
S-Man42 <info@s-man42.de>
2022-11-10 12:19:58 +0100

    final final test release?

--- lib/i18n/supported_locales.dart
+++ lib/i18n/supported_locales.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
 final Map<Locale, Map<String, dynamic>> SUPPORTED_LOCALES = {
-  Locale('cz'): {'name_native': '🇨🇿 Čeština', 'percent_translated': 11},
+  Locale('cz'): {'name_native': '🇨🇿 Čeština', 'percent_translated': 10},
   Locale('da'): {'name_native': '🇩🇰 Dansk', 'percent_translated': 2},
   Locale('de'): {'name_native': '🇩🇪 Deutsch', 'percent_translated': 100},
   Locale('el'): {'name_native': '🇬🇷 Ελληνικά', 'percent_translated': 5},
   Locale('en'): {'name_native': '🇬🇧🇺🇸 English', 'percent_translated': 100},
-  Locale('es'): {'name_native': '🇪🇸 Español', 'percent_translated': 6},
-  Locale('fi'): {'name_native': '🇫🇮 Suomi', 'percent_translated': 54},
-  Locale('fr'): {'name_native': '🇫🇷 Français', 'percent_translated': 87},
-  Locale('it'): {'name_native': '🇮🇹 Italiano', 'percent_translated': 9},
+  Locale('es'): {'name_native': '🇪🇸 Español', 'percent_translated': 8},
+  Locale('fi'): {'name_native': '🇫🇮 Suomi', 'percent_translated': 51},
+  Locale('fr'): {'name_native': '🇫🇷 Français', 'percent_translated': 84},
+  Locale('it'): {'name_native': '🇮🇹 Italiano', 'percent_translated': 8},
   Locale('ko'): {'name_native': '🇰🇷 한국어', 'percent_translated': 83},
   Locale('nl'): {'name_native': '🇳🇱 Nederlands', 'percent_translated': 100},
   Locale('pl'): {'name_native': '🇵🇱 Polski', 'percent_translated': 53},
-  Locale('pt'): {'name_native': '🇵🇹 Português', 'percent_translated': 19},
-  Locale('ru'): {'name_native': '🇷🇺 Ру́сский', 'percent_translated': 6},
-  Locale('sk'): {'name_native': '🇸🇰 Slovenský', 'percent_translated': 92},
-  Locale('sv'): {'name_native': '🇸🇪 Svenska', 'percent_translated': 93},
-  Locale('tr'): {'name_native': '🇹🇷 Türkçe', 'percent_translated': 11},
+  Locale('pt'): {'name_native': '🇵🇹 Português', 'percent_translated': 18},
+  Locale('ru'): {'name_native': '🇷🇺 Ру́сский', 'percent_translated': 5},
+  Locale('sk'): {'name_native': '🇸🇰 Slovenský', 'percent_translated': 100},
+  Locale('sv'): {'name_native': '🇸🇪 Svenska', 'percent_translated': 100},
+  Locale('tr'): {'name_native': '🇹🇷 Türkçe', 'percent_translated': 10},
 };
 
 const Locale DEFAULT_LOCALE = Locale('en');
