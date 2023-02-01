dd9440f3410a22c608b268e6e5bd2c3a5690a4da
Mike-3 <Mike.Binnemann@web.de>
2023-02-01 13:11:19 +0100

    cleaning

--- test/tools/images_and_files/hidden_data/logic/hidden_data.dart
+++ test/tools/images_and_files/hidden_data/logic/hidden_data.dart
@@ -46,22 +46,21 @@ void main() {
   group("hidden_data.hiddenData:", () {
     List<Map<String, dynamic>> _inputsToExpected = [
 
-      {'input' : 'hidden.zip', 'expectedOutput' :
-          'hidden.zip, ZIP, 164 bytes, \n'
+      {'input' : 'hidden1.zip', 'expectedOutput' :
+          'hidden1.zip, ZIP, 164 bytes, \n'
           '    hidden.TXT, TXT, 6 bytes, \n'
           ''},
-      {'input' : 'hidden1.jpg', 'expectedOutput' : 'hidden1.jpg, JPEG, 1489 bytes, '},
-      {'input' : 'hidden2.jpg', 'expectedOutput' : 'hidden2.jpg, JPEG, 87428 bytes, '},
-      {'input' : 'hidden3.jpg', 'expectedOutput' : 'hidden3.jpg, JPEG, 1489 bytes, '},
-      {'input' : 'hidden4.jpg', 'expectedOutput' :
-          'hidden4.jpg, JPEG, 4193 bytes, \n'
+      {'input' : 'hidden2.rar.zip', 'expectedOutput' :
+          'hidden2.rar.zip, ZIP, 49358 bytes, \n'
+          '    test1.rar, RAR, 49225 bytes, \n'
+          ''},
+      {'input' : 'hidden3.jpg', 'expectedOutput' :
+          'hidden3.jpg, JPEG, 4193 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, ZIP, 313 bytes, \n'
           '        QuickResponse.bin, TXT, 0 bytes, \n'
           ''},
-      {'input' : 'hidden5.jpg', 'expectedOutput' : 'hidden5.jpg, JPEG, 31459 bytes, '},
-      {'input' : 'hidden6.jpg', 'expectedOutput' : 'hidden6.jpg, JPEG, 41805 bytes, '},
-      {'input' : 'hidden7.jpg', 'expectedOutput' :
-          'hidden7.jpg, JPEG, 23557 bytes, \n'
+      {'input' : 'hidden4.jpg', 'expectedOutput' :
+          'hidden4.jpg, JPEG, 23557 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, ZIP, 11729 bytes, \n'
           '        lat.mp3, MP3, 31857 bytes, \n'
           '            <<!!!HIDDEN_FILE!!!>>_2, JPEG, 14942 bytes, \n'
@@ -72,57 +71,56 @@ void main() {
           '    <<!!!HIDDEN_FILE!!!>>_6, JPEG, 1670 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_7, JPEG, 1670 bytes, \n'
           ''},
-      {'input' : 'hidden8.jpg', 'expectedOutput' :
-          'hidden8.jpg, JPEG, 53827 bytes, \n'
+      {'input' : 'hidden5.jpg', 'expectedOutput' :
+          'hidden5.jpg, JPEG, 53827 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, ZIP, 2264 bytes, \n'
           '        Lorem Ipsum.txt, TXT, 4501 bytes, \n'
           ''},
-      {'input' : 'hidden9.jpg', 'expectedOutput' :
-          'hidden9.jpg, JPEG, 237081 bytes, \n'
+      {'input' : 'hidden6.jpg', 'expectedOutput' :
+          'hidden6.jpg, JPEG, 237081 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, ZIP, 178 bytes, \n'
           '        secret.txt, TXT, 10 bytes, \n'
           ''},
-      {'input' : 'hidden10.jpg', 'expectedOutput' :
-          'hidden10.jpg, JPEG, 239449 bytes, \n'
+      {'input' : 'hidden7.jpg', 'expectedOutput' :
+          'hidden7.jpg, JPEG, 239449 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, PNG, 2532 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_2, TXT, 14 bytes, \n'
           ''},
-      {'input' : 'hidden11.png', 'expectedOutput' :
-          'hidden11.png, PNG, 239599 bytes, \n'
+      {'input' : 'hidden8.png', 'expectedOutput' :
+          'hidden8.png, PNG, 239599 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, JPEG, 236903 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_2, ZIP, 164 bytes, \n'
           '        secret.txt, TXT, 10 bytes, \n'
           ''},
-      {'input' : 'hidden12.png', 'expectedOutput' :
-          'hidden12.png, PNG, 726398 bytes, \n'
+      {'input' : 'hidden9.png', 'expectedOutput' :
+          'hidden9.png, PNG, 726398 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, ZIP, 1130 bytes, \n'
           '        Rtsel.txt, TXT, 5525 bytes, \n'
           ''},
-      {'input' : 'hidden13.jpg', 'expectedOutput' :
-          'hidden13.jpg, JPEG, 53827 bytes, \n'
+      {'input' : 'hidden10.jpg', 'expectedOutput' :
+          'hidden10.jpg, JPEG, 53827 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, ZIP, 2264 bytes, \n'
           '        Lorem Ipsum.txt, TXT, 4501 bytes, \n'
           ''},
-      {'input' : 'hidden14.tar.bz2', 'expectedOutput' :
-          'hidden14.tar.bz2, BZIP2, 107211 bytes, \n'
-          '    hidden14.tar, TAR, 118784 bytes, \n'
+      {'input' : 'hidden11.tar.bz2', 'expectedOutput' :
+          'hidden11.tar.bz2, BZIP2, 107211 bytes, \n'
+          '    hidden11.tar, TAR, 118784 bytes, \n'
           '        homerl.jpg, JPEG, 71128 bytes, \n'
           '        Temp.zip, ZIP, 45060 bytes, \n'
           '            donald.jpg, JPEG, 17708 bytes, \n'
           '            garfield.jpg, JPEG, 27696 bytes, \n'
           ''},
-      {'input' : 'hidden15.jpg', 'expectedOutput' :
-          'hidden15.jpg, JPEG, 243577 bytes, \n'
+      {'input' : 'hidden12.jpg', 'expectedOutput' :
+          'hidden12.jpg, JPEG, 243577 bytes, \n'
           '    <<!!!HIDDEN_FILE!!!>>_1, _7z, 104345 bytes, \n'
           ''},
 
-      {'input' : 'nano.png', 'expectedOutput' : 'hidden1.jpg, JPEG, 1489 bytes, '},
-      {'input' : 'QuickResponse.jpg', 'expectedOutput' : 'hidden1.jpg, JPEG, 1489 bytes, '},
-
-      {'input' : 'sample.tar.gz', 'expectedOutput' : 'hidden1.jpg, JPEG, 1489 bytes, '},
-      {'input' : 'test1.rar.zip', 'expectedOutput' : 'hidden1.jpg, JPEG, 1489 bytes, '},
-      {'input' : 'teufel.png', 'expectedOutput' : 'hidden1.jpg, JPEG, 1489 bytes, '},
-
+      {'input' : 'hidden13.jpg', 'expectedOutput' :
+          'hidden13.jpg, JPEG, 5526 bytes, \n'
+          '    <<!!!HIDDEN_FILE!!!>>_1, JPEG, 4616 bytes, \n'
+          '    <<!!!HIDDEN_FILE!!!>>_2, JPEG, 3706 bytes, \n'
+          '    <<!!!HIDDEN_FILE!!!>>_3, JPEG, 2795 bytes, \n'
+          ''},
     ];
 
     _inputsToExpected.forEach((elem) {
