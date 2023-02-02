39630a6bb3304942a168779c0fdcc5a46e622db3
Mike-3 <Mike.Binnemann@web.de>
2023-02-02 16:38:57 +0100

    Update dtmf_test.dart

--- test/tools/science_and_technology/dtmf/logic/dtmf_test.dart
+++ test/tools/science_and_technology/dtmf/logic/dtmf_test.dart
@@ -7,8 +7,8 @@ void main() {
       {'input' : '', 'expectedOutput' : ''},
       {'input' : 'A', 'expectedOutput' : '[697, 1633]'},
       {'input' : 's', 'expectedOutput' : ''},
-      {'input' : '27', 'expectedOutput' : '[697, 1336]   [852, 1209]'},
-      {'input' : '2h6', 'expectedOutput' : '[697, 1336]   [770, 1477]'},
+      {'input' : '27', 'expectedOutput' : '[697, 1336] [852, 1209]'},
+      {'input' : '2h6', 'expectedOutput' : '[697, 1336] [770, 1477]'},
     ];
 
     _inputsToExpected.forEach((elem) {
