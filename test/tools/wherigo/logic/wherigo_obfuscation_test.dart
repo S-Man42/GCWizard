1ae8ba33538dc52e05741e389f09c1e6f4dc5bcc
tzimmermann <thomas@familiezimmermann.de>
2023-02-04 17:26:37 +0100

    check and correct test wherigo obsucation WWB

--- test/tools/wherigo/logic/wherigo_obfuscation_test.dart
+++ test/tools/wherigo/logic/wherigo_obfuscation_test.dart
@@ -33,7 +33,7 @@ void main() {
       {'input' : 'Bruder Malusvisus, der beste Angler des Klosters Bebenhausen.', 'expectedOutput' : 'K15pr5 cjv548w7@1, pr5 rn24q O2wuo2 qs7 Tvz46s68 Lpnr1wq32pzK'},
       {'input' : 'Schrein zu Ehren des Hl. Hubertus.', 'expectedOutput' : 'bms3rw2 84 Qu5t3 np4 V0N R5nr58@1H'},
       {'input' : 'Entschuldigung! Bitte kommen Sie dem Versuchsgelände nicht zu nahe.', 'expectedOutput' : 'Nx44pv91msr60u! Ks45r z4vwpz gxu npy jt714nt5ut1äypr 2ylr4 -8 3jrpJ'},
-      {'input' : 'He, Sie! Hier ist überall Sperrbereich. Und angefasst werden darf hier erst recht nichts. Legen Sie alle Gegenstände wieder an ihren Platz und verlassen Sie das Gelände.', 'expectedOutput' : 'Qo, fwt! Rtq4 x82 üos6quv e2s67ko2qvqwN eyp o2wnpl457 -n1oq0 sq0p tvs6 n135 5tsq3 zvqw91H<\\001\\002>\\n\\003\\003\\003\\003Xrut3 ctq o01n Rqts282äzqs -rooq4 p3 ss3r1 fuk4. 82t 5p3yo78nx evs tj2 SrzäwnpJ'},
+      {'input' : 'He, Sie! Hier ist überall Sperrbereich. Und angefasst werden darf hier erst recht nichts. Legen Sie alle Gegenstände wieder an ihren Platz und verlassen Sie das Gelände.', 'expectedOutput' : 'Qo, fwt! Rtq4 x82 üos6quv e2s67ko2qvqwN eyp o2wnpl457 -n1oq0 sq0p tvs6 n135 5tsq3 zvqw91H Xrut3 ctq o01n Rqts282äzqs -rooq4 p3 ss3r1 fuk4. 82t 5p3yo78nx evs tj2 SrzäwnpJ'},
     ];
 
     _inputsToExpected.forEach((elem) {
