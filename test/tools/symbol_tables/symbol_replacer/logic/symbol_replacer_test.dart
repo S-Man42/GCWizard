ef5809e77c6df24d513dd69ae49643793f81c29e
Mike-3 <Mike.Binnemann@web.de>
2023-03-16 21:18:53 +0100

    replacer test

--- test/tools/symbol_tables/symbol_replacer/logic/symbol_replacer_test.dart
+++ test/tools/symbol_tables/symbol_replacer/logic/symbol_replacer_test.dart
@@ -2,12 +2,7 @@ import 'dart:io' as io;
 import 'dart:typed_data';
 
 import "package:flutter_test/flutter_test.dart";
-import 'package:gc_wizard/application/registry.dart';
-import 'package:gc_wizard/tools/symbol_tables/_common/logic/symbol_table_data.dart';
-import 'package:gc_wizard/tools/symbol_tables/_common/widget/gcw_symbol_table_tool.dart';
-import 'package:gc_wizard/tools/symbol_tables/_common/widget/symbol_table.dart';
 import 'package:gc_wizard/tools/symbol_tables/symbol_replacer/logic/symbol_replacer.dart';
-import 'package:gc_wizard/tools/symbol_tables/symbol_replacer/widget/symbol_replacer_symboldata.dart';
 import 'package:path/path.dart' as path;
 
 var testDirPath = 'test/tools/symbol_tables/symbol_replacer/resources/';
@@ -20,15 +15,16 @@ Uint8List _getFileData(String name) {
 void main() {
 
   group("symbol_replacer.replaceSymbols:", () {
-    List<Map<String, Object>> _inputsToExpected = [
-      {'input' : 'dancing_man.png', 'expectedOutputSymbolsCount' : 12, 'expectedOutputText' : '1234'},
+    List<Map<String, Object?>> _inputsToExpected = [
+      {'input' : 'dancing_man1.png', 'expectedOutput' : 4},
+      {'input' : 'dancing_man2.png', 'expectedOutput' : 1},
     ];
 
 
     _inputsToExpected.forEach((elem) {
       test('input: ${elem['input']}', () async {
-        var _actual = await replaceSymbols(_getFileData(elem['input']), 50, 80);
-        expect(_actual.symbols.length, elem['expectedOutputSymbolsCount']);
+        var _actual = await replaceSymbols(_getFileData(elem['input'] as String), 50, 80);
+        expect(_actual?.symbols.length, elem['expectedOutput']);
       });
     });
   });
