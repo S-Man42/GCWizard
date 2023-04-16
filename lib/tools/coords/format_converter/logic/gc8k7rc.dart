1cbb5e34c98b566c287bd8d4bae0a1f766b78299
tzimmermann <thomas@familiezimmermann.de>
2023-04-16 22:43:17 +0200

    correct logic

--- lib/tools/coords/format_converter/logic/gc8k7rc.dart
+++ lib/tools/coords/format_converter/logic/gc8k7rc.dart
@@ -4,22 +4,27 @@ import 'package:gc_wizard/tools/coords/format_converter/logic/dec.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/coordinates.dart';
 import 'package:latlong2/latlong.dart';
 
-const rEarth = 6378.90594503519;
+const rEarth = 6378905.94503519;
 const vEquator = 464;
+const secondsPerDay = 24 * 60 * 60;
 
 LatLng GC8K7RCToLatLon(GC8K7RC coordsGC8K7RC) {
-  print('tolatlon');
-  print(coordsGC8K7RC.velocity);
-  print(coordsGC8K7RC.distance);
-  double u = coordsGC8K7RC.velocity * 24 * 60 * 60;
+  double u = coordsGC8K7RC.velocity * secondsPerDay;
   double r = u.abs() / 2 / pi;
+
   double lat = acos(r / rEarth);
+  lat = lat * 180 / pi;
 
   if (u < 0) {
     lat = -lat;
   }
 
-  double long = 360 * coordsGC8K7RC.distance / u.abs();
+  double long = 0.0;
+  if (u != 0) {
+    long = 360 * coordsGC8K7RC.distance / u.abs();
+  } else {
+    long = 0;
+  }
   return decToLatLon(DEC(lat, long));
 }
 
@@ -27,7 +32,7 @@ GC8K7RC latLonToGC8K7RC(LatLng coord) {
   double lat = coord.latitude;
   double lon = coord.longitude;
 
-  double u = cos(lat) * rEarth * 2 * pi;
+  double u = cos(lat * pi / 180) * rEarth * 2 * pi;
   double velocity = u / 24 / 60 / 60;
 
   double distance = lon * u / 360;
