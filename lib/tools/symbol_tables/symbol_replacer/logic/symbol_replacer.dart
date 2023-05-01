776f573d0b9980cd60cfe6874dbd40621e012842
Mike-3 <Mike.Binnemann@web.de>
2023-05-01 21:13:01 +0200

    bugfix replacer manual setter

--- lib/tools/symbol_tables/symbol_replacer/logic/symbol_replacer.dart
+++ lib/tools/symbol_tables/symbol_replacer/logic/symbol_replacer.dart
@@ -424,7 +424,7 @@ class SymbolReplacerImage {
         symbol.bmp.height,
       );
 
-      var color = Image.ColorRgb8(Colors.blue.red, Colors.blue.green, Colors.blue.blue);
+      var color = Image.ColorRgb8(Colors.orangeAccent.red, Colors.orangeAccent.green, Colors.orangeAccent.blue);
       Image.drawRect(bmp, x1: rect.left, y1: rect.top, x2: rect.right, y2: rect.bottom, color: color);
       Image.drawRect(bmp, x1: rect.left - 1, y1: rect.top - 1, x2: rect.right + 1,y2:  rect.bottom + 1, color: color);
     }
