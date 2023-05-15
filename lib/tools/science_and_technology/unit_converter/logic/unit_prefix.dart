6e92831adc39fcf26299d368b8ce22dd74a65067
Mike-3 <Mike.Binnemann@web.de>
2023-05-15 19:46:03 +0200

    private units

--- lib/tools/science_and_technology/unit_converter/logic/unit_prefix.dart
+++ lib/tools/science_and_technology/unit_converter/logic/unit_prefix.dart
@@ -1,51 +1,51 @@
-final UNITPREFIX_QUETTA = UnitPrefix('common_unit_prefix_quetta', 'Q', 1.0e30);
-final UNITPREFIX_RONNA = UnitPrefix('common_unit_prefix_ronna', 'R', 1.0e27);
-final UNITPREFIX_YOTTA = UnitPrefix('common_unit_prefix_yotta', 'Y', 1.0e24);
-final UNITPREFIX_ZETTA = UnitPrefix('common_unit_prefix_zetta', 'Z', 1.0e21);
-final UNITPREFIX_EXA = UnitPrefix('common_unit_prefix_exa', 'E', 1.0e18);
-final UNITPREFIX_PETA = UnitPrefix('common_unit_prefix_peta', 'P', 1.0e15);
-final UNITPREFIX_TERA = UnitPrefix('common_unit_prefix_tera', 'T', 1.0e12);
-final UNITPREFIX_GIGA = UnitPrefix('common_unit_prefix_giga', 'G', 1.0e9);
-final UNITPREFIX_MEGA = UnitPrefix('common_unit_prefix_mega', 'M', 1.0e6);
+final _UNITPREFIX_QUETTA = UnitPrefix('common_unit_prefix_quetta', 'Q', 1.0e30);
+final _UNITPREFIX_RONNA = UnitPrefix('common_unit_prefix_ronna', 'R', 1.0e27);
+final _UNITPREFIX_YOTTA = UnitPrefix('common_unit_prefix_yotta', 'Y', 1.0e24);
+final _UNITPREFIX_ZETTA = UnitPrefix('common_unit_prefix_zetta', 'Z', 1.0e21);
+final _UNITPREFIX_EXA = UnitPrefix('common_unit_prefix_exa', 'E', 1.0e18);
+final _UNITPREFIX_PETA = UnitPrefix('common_unit_prefix_peta', 'P', 1.0e15);
+final _UNITPREFIX_TERA = UnitPrefix('common_unit_prefix_tera', 'T', 1.0e12);
+final _UNITPREFIX_GIGA = UnitPrefix('common_unit_prefix_giga', 'G', 1.0e9);
+final _UNITPREFIX_MEGA = UnitPrefix('common_unit_prefix_mega', 'M', 1.0e6);
 final UNITPREFIX_KILO = UnitPrefix('common_unit_prefix_kilo', 'k', 1.0e3);
-final UNITPREFIX_HECTO = UnitPrefix('common_unit_prefix_hecto', 'h', 1.0e2);
-final UNITPREFIX_DECA = UnitPrefix('common_unit_prefix_deca', 'da', 1.0e1);
+final _UNITPREFIX_HECTO = UnitPrefix('common_unit_prefix_hecto', 'h', 1.0e2);
+final _UNITPREFIX_DECA = UnitPrefix('common_unit_prefix_deca', 'da', 1.0e1);
 final UNITPREFIX_NONE = UnitPrefix('', '', 1.0);
-final UNITPREFIX_DECI = UnitPrefix('common_unit_prefix_deci', 'd', 1.0e-1);
-final UNITPREFIX_CENTI = UnitPrefix('common_unit_prefix_centi', 'c', 1.0e-2);
-final UNITPREFIX_MILLI = UnitPrefix('common_unit_prefix_milli', 'm', 1.0e-3);
-final UNITPREFIX_MICRO = UnitPrefix('common_unit_prefix_micro', '\u00B5', 1.0e-6);
-final UNITPREFIX_NANO = UnitPrefix('common_unit_prefix_nano', 'n', 1.0e-9);
-final UNITPREFIX_PICO = UnitPrefix('common_unit_prefix_pico', 'p', 1.0e-12);
-final UNITPREFIX_FEMTO = UnitPrefix('common_unit_prefix_femto', 'f', 1.0e-15);
-final UNITPREFIX_ATTO = UnitPrefix('common_unit_prefix_atto', 'a', 1.0e-18);
-final UNITPREFIX_ZEPTO = UnitPrefix('common_unit_prefix_zepto', 'z', 1.0e-21);
-final UNITPREFIX_YOCTO = UnitPrefix('common_unit_prefix_yocto', 'y', 1.0e-24);
-final UNITPREFIX_RONTO = UnitPrefix('common_unit_prefix_ronto', 'r', 1.0e-27);
-final UNITPREFIX_QUECTO = UnitPrefix('common_unit_prefix_quecto', 'q', 1.0e-30);
+final _UNITPREFIX_DECI = UnitPrefix('common_unit_prefix_deci', 'd', 1.0e-1);
+final _UNITPREFIX_CENTI = UnitPrefix('common_unit_prefix_centi', 'c', 1.0e-2);
+final _UNITPREFIX_MILLI = UnitPrefix('common_unit_prefix_milli', 'm', 1.0e-3);
+final _UNITPREFIX_MICRO = UnitPrefix('common_unit_prefix_micro', '\u00B5', 1.0e-6);
+final _UNITPREFIX_NANO = UnitPrefix('common_unit_prefix_nano', 'n', 1.0e-9);
+final _UNITPREFIX_PICO = UnitPrefix('common_unit_prefix_pico', 'p', 1.0e-12);
+final _UNITPREFIX_FEMTO = UnitPrefix('common_unit_prefix_femto', 'f', 1.0e-15);
+final _UNITPREFIX_ATTO = UnitPrefix('common_unit_prefix_atto', 'a', 1.0e-18);
+final _UNITPREFIX_ZEPTO = UnitPrefix('common_unit_prefix_zepto', 'z', 1.0e-21);
+final _UNITPREFIX_YOCTO = UnitPrefix('common_unit_prefix_yocto', 'y', 1.0e-24);
+final _UNITPREFIX_RONTO = UnitPrefix('common_unit_prefix_ronto', 'r', 1.0e-27);
+final _UNITPREFIX_QUECTO = UnitPrefix('common_unit_prefix_quecto', 'q', 1.0e-30);
 
 final unitPrefixes = [
   // UNITPREFIX_QUETTA,
   // UNITPREFIX_RONNA,
   // UNITPREFIX_YOTTA,
   // UNITPREFIX_ZETTA,
-  UNITPREFIX_EXA,
-  UNITPREFIX_PETA,
-  UNITPREFIX_TERA,
-  UNITPREFIX_GIGA,
-  UNITPREFIX_MEGA,
+  _UNITPREFIX_EXA,
+  _UNITPREFIX_PETA,
+  _UNITPREFIX_TERA,
+  _UNITPREFIX_GIGA,
+  _UNITPREFIX_MEGA,
   UNITPREFIX_KILO,
-  UNITPREFIX_HECTO,
-  UNITPREFIX_DECA,
+  _UNITPREFIX_HECTO,
+  _UNITPREFIX_DECA,
   UNITPREFIX_NONE,
-  UNITPREFIX_DECI,
-  UNITPREFIX_CENTI,
-  UNITPREFIX_MILLI,
-  UNITPREFIX_MICRO,
-  UNITPREFIX_NANO,
-  UNITPREFIX_PICO,
-  UNITPREFIX_FEMTO,
-  UNITPREFIX_ATTO,
+  _UNITPREFIX_DECI,
+  _UNITPREFIX_CENTI,
+  _UNITPREFIX_MILLI,
+  _UNITPREFIX_MICRO,
+  _UNITPREFIX_NANO,
+  _UNITPREFIX_PICO,
+  _UNITPREFIX_FEMTO,
+  _UNITPREFIX_ATTO,
   // UNITPREFIX_ZEPTO,
   // UNITPREFIX_YOCTO,
   // UNITPREFIX_RONTO,
