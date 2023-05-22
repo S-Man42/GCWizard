93f632a62deddb668dd0f935620b2d4d0fc981ea
Mike-3 <Mike.Binnemann@web.de>
2023-05-22 16:37:30 +0200

    bugfix GCWAlphabetDropDown

--- lib/common_widgets/dropdowns/gcw_alphabetdropdown.dart
+++ lib/common_widgets/dropdowns/gcw_alphabetdropdown.dart
@@ -3,12 +3,12 @@ import 'package:gc_wizard/application/i18n/app_localizations.dart';
 import 'package:gc_wizard/common_widgets/dropdowns/gcw_dropdown.dart';
 import 'package:gc_wizard/common_widgets/textfields/gcw_textfield.dart';
 
-class GCWAlphabetDropDown<T extends Object?> extends StatefulWidget {
+class GCWAlphabetDropDown<T> extends StatefulWidget {
   final void Function(T) onChanged;
   final void Function(String)? onCustomAlphabetChanged;
-  final Map<Object, String> items;
+  final Map<T, String> items;
   final Object customModeKey;
-  final Object value;
+  final T value;
   final TextEditingController? textFieldController;
   final String? textFieldHintText;
 
@@ -24,22 +24,22 @@ class GCWAlphabetDropDown<T extends Object?> extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _GCWAlphabetDropDownState createState() => _GCWAlphabetDropDownState();
+  _GCWAlphabetDropDownState<T> createState() => _GCWAlphabetDropDownState<T>();
 }
 
-class _GCWAlphabetDropDownState extends State<GCWAlphabetDropDown> {
-  Object? _currentMode;
+class _GCWAlphabetDropDownState<T> extends State<GCWAlphabetDropDown<T>> {
+  T? _currentMode;
 
   @override
   Widget build(BuildContext context) {
 
     return Column(children: <Widget>[
-      GCWDropDown<Object>(
+      GCWDropDown<T>(
         value: widget.value,
         onChanged: (value) {
           setState(() {
             _currentMode = value;
-            widget.onChanged(_currentMode);
+            widget.onChanged(value);
           });
         },
         items: widget.items.entries.map((mode) {
