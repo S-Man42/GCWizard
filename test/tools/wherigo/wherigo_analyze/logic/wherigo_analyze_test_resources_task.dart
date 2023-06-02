f71f44a659cc658f31cb576e124903ca279a67c2
tzimmermann <thomas@familiezimmermann.de>
2023-06-02 22:30:37 +0200

    add wherigo-test for analyzing TASK

--- test/tools/wherigo/wherigo_analyze/logic/wherigo_analyze_test_resources_task.dart
+++ test/tools/wherigo/wherigo_analyze/logic/wherigo_analyze_test_resources_task.dart
@@ -1,16 +1,33 @@
 import 'package:gc_wizard/tools/wherigo/wherigo_analyze/logic/wherigo_analyze.dart';
 
 WherigoTaskData testOutputTASK = const WherigoTaskData(
-  TaskLUAName: '',
-  TaskID: '',
-  TaskName: '',
-  TaskDescription: '',
-  TaskVisible: '',
-  TaskMedia: '',
-  TaskIcon: '',
-  TaskActive: '',
-  TaskComplete: '',
-  TaskCorrectstate: '',
+  TaskLUAName: 'objwerdeMastermind',
+  TaskID: '376fa2d8-dc30-4bb3-bc29-5998a281c1a8',
+  TaskName: 'werde Mastermind',
+  TaskDescription: 'Donald E. Knuth hat 1976 gezeigt, dass es moeglich ist, jeden moeglichen Farbcode des Spiels (4 Stellen, 6 Farben) in maximal fuenf Zuegen zu ermitteln.\n'+
+      'Kenji Koyama und Tony W. Lai fanden 1994 eine Mastermind-Strategie, die im Durchschnitt nur 4,34 Versuche benoetigt.\n'+
+      'Diese Strategie - und jede andere, die im Durchschnitt genauso schnell ist - benoetigt im schlechtesten Fall sechs Versuche.\n'+
+      'Sie bewiesen ausserdem, dass es keine schnellere Strategie geben kann.',
+  TaskVisible: 'false',
+  TaskMedia: 'objFinal',
+  TaskIcon: 'objicoFinal',
+  TaskActive: 'true',
+  TaskComplete: 'false',
+  TaskCorrectstate: 'None',
 );
 
-String testInputTASK = '';
+String testInputTASK =
+'objwerdeMastermind = Wherigo.ZTask(objKlausMastermindKlabuster)\n'+
+'objwerdeMastermind.Id = "376fa2d8-dc30-4bb3-bc29-5998a281c1a8"\n'+
+'objwerdeMastermind.Name = "werde Mastermind"\n'+
+'objwerdeMastermind.Description = [[\n'+
+'Donald E. Knuth hat 1976 gezeigt, dass es moeglich ist, jeden moeglichen Farbcode des Spiels (4 Stellen, 6 Farben) in maximal fuenf Zuegen zu ermitteln.<BR>\n'+
+'Kenji Koyama und Tony W. Lai fanden 1994 eine Mastermind-Strategie, die im Durchschnitt nur 4,34 Versuche benoetigt.<BR>\n'+
+'Diese Strategie - und jede andere, die im Durchschnitt genauso schnell ist - benoetigt im schlechtesten Fall sechs Versuche.<BR>\n'+
+'Sie bewiesen ausserdem, dass es keine schnellere Strategie geben kann.]]\n'+
+'objwerdeMastermind.Visible = false\n'+
+'objwerdeMastermind.Media = objFinal\n'+
+'objwerdeMastermind.Icon = objicoFinal\n'+
+'objwerdeMastermind.Active = true\n'+
+'objwerdeMastermind.Complete = false\n'+
+'objwerdeMastermind.CorrectState = "None"';
