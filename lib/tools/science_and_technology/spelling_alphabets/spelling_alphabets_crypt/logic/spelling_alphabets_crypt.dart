58799a031ca52b2ae81b24dea66c483adef2f3ae
tzimmermann <thomas@familiezimmermann.de>
2023-06-02 19:57:23 +0200

    add tests, correct logic

--- lib/tools/science_and_technology/spelling_alphabets/spelling_alphabets_crypt/logic/spelling_alphabets_crypt.dart
+++ lib/tools/science_and_technology/spelling_alphabets/spelling_alphabets_crypt/logic/spelling_alphabets_crypt.dart
@@ -1,16 +1,23 @@
 // https://www.geocaching.com/geocache/GC9R4YE_barossa-121
 
 import 'package:gc_wizard/tools/science_and_technology/spelling_alphabets/_common/spelling_alphabets_data.dart';
+import 'package:gc_wizard/utils/constants.dart';
 
 String encodeSpellingAlphabets(String plain, SPELLING language) {
   List<String> result = [];
   Map<String, String> alphabet = SPELLING_ALPHABETS[language]!;
 
+  if (plain.isEmpty) return '';
+
   plain.toUpperCase().split('').forEach((letter){
-    if (alphabet[letter] != null) {
+    if (letter == ' ') {
+      result.add(' ');
+    } else {
+      if (alphabet[letter] != null) {
       result.add(alphabet[letter]!.toUpperCase());
     } else {
-      result.add(' ');
+      result.add(UNKNOWN_ELEMENT);
+    }
     }
   });
   return result.join(' ');
@@ -20,13 +27,18 @@ String decodeSpellingAlphabets(String chiffre, SPELLING language) {
   List<String> result = [];
   Map<String, String> alphabet = {};
 
+  if (chiffre.isEmpty) return '';
+
   SPELLING_ALPHABETS[language]!.forEach((key, value) {
     alphabet[value.toUpperCase()] = key;
   });
 
   chiffre.toUpperCase().split(' ').forEach((word){
-    if (alphabet[word] == null) {
+    if (word == ' ' || word == '') {
       result.add(' ');
+    }
+    else if (alphabet[word] == null) {
+      result.add(UNKNOWN_ELEMENT);
     } else {
       result.add(alphabet[word]!);
     }
