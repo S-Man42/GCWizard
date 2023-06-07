7e96f84f7b9770083a447c1f7603256f5dccd58e
tzimmermann <thomas@familiezimmermann.de>
2023-06-07 21:56:56 +0200

    add test

--- test/tools/scripting/logic/gcwizard_script_codes.dart
+++ test/tools/scripting/logic/gcwizard_script_codes.dart
@@ -2,6 +2,7 @@ part of 'gcwizard_scipt_test.dart';
 
 List<Map<String, Object?>> _inputsCodesToExpected = [
   {'code' : code1c, 'expectedOutput' : '1       2.0     9.0     6.0'},
+  {'code' : codeGC6GE, 'expectedOutput' : 'Punkte im radius 197 sind 571'},
 ];
 
 var code1c = ''' 
@@ -33,4 +34,25 @@ for z = 1 to 9
  next
 next
 end
+''';
+
+var codeGC6GE = '''
+REM https://coord.info/GC6GE4R
+r = 197
+a = 50.9621667
+o = 11.03585
+c = 0
+for m = 0 to 999
+  for s = 1 to 2
+    for n = 0 to 999
+      x = 50 + (57 + m/1000)/60
+      y = 11 + (s + n/1000)/60
+      d = distance(a,o,x,y)
+      if d<=r then c = c + 1
+      endif
+    next
+  next
+next
+print "Punkte im radius ",r, " sind ",c
+end 
 ''';
