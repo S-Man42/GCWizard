440fe634e1a6eee12a7387ac4fea744491c75ced
Mike-3 <Mike.Binnemann@web.de>
2023-06-29 16:47:51 +0200

    Update avemaria.dart

--- lib/tools/crypto_and_encodings/avemaria/logic/avemaria.dart
+++ lib/tools/crypto_and_encodings/avemaria/logic/avemaria.dart
@@ -21,14 +21,16 @@ String decodeAveMaria(String chiffre) {
 
 String encodeAveMaria(String plain) {
   List<String> result = [];
-  List<String> code = plain.toUpperCase().split('  ');
+  List<String> code = plain.toUpperCase().split(' ');
 
-  for (String letter in code) {
-    if (letter == ' ') {
-      result.add(' ');
-    } else {
-      result.add(_AVE_MARIA_ENCODE[letter]![Random().nextInt(2)]);
+  for (String word in code) {
+    for (String letter in word.split('')) {
+      var results = _AVE_MARIA_ENCODE[letter];
+      if (results != null && results.length == 2) {
+        result.add(results[Random().nextInt(2)]);
+      }
     }
+    result.add(' ');
   }
-  return result.join(' ');
+  return result.join(' ').trim();
 }
