18865d009c6afd9bd489a953b7449ae72f925e61
tzimmermann <thomas@familiezimmermann.de>
2023-07-02 21:48:16 +0200

    add rot5, rot13, rot18, rot47

--- lib/tools/scripting/logic/gcwizard_script_functions_geocaching.dart
+++ lib/tools/scripting/logic/gcwizard_script_functions_geocaching.dart
@@ -1,11 +1,62 @@
 part of 'package:gc_wizard/tools/scripting/logic/gcwizard_script.dart';
 
+const _defaultAlphabetAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const _defaultAlphabetDigits = '0123456789';
+
 String _rotx(Object text, Object rot) {
   if (_isNotString(text) || _isString(rot)) {
     _handleError(_INVALIDTYPECAST);
     return '';
   }
   return Rotator().rotate((text as String), (rot as int));
+}
+
+String _rot5(Object text, ) {
+  if (_isNotString(text)) {
+    _handleError(_INVALIDTYPECAST);
+    return '';
+  }
+  Rotator rot = Rotator();
+  rot.alphabet = _defaultAlphabetDigits;
+  return rot.rotate((text as String), 5);
+}
+
+String _rot13(Object text, ) {
+  if (_isNotString(text)) {
+    _handleError(_INVALIDTYPECAST);
+    return '';
+  }
+  Rotator rot = Rotator();
+  rot.alphabet = _defaultAlphabetAlpha;
+  return rot.rotate((text as String), 5);
+}
+
+String _rot18(Object text, ) {
+  if (_isNotString(text)) {
+    _handleError(_INVALIDTYPECAST);
+    return '';
+  }
+
+  return (text as String).split('').map((char) {
+    if (_defaultAlphabetAlpha.contains(char.toUpperCase())) {
+      return _rot13(char);
+    } else if (_defaultAlphabetDigits.contains(char)) {
+      return _rot5(char);
+    } else {
+      return char;
+    }
+  }).join();
+}
+
+String _rot47(Object text, ) {
+  if (_isNotString(text)) {
+    _handleError(_INVALIDTYPECAST);
+    return '';
+  }
+
+  Rotator rot = Rotator();
+  rot.alphabet = '!"#\$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+  return rot.rotate((text as String), 47, removeUnknownCharacters: false, ignoreCase: false);
 }
 
 int _bww(Object text, Object opt_alph, Object opt_itqs) { //ToDo opt_alph ???
