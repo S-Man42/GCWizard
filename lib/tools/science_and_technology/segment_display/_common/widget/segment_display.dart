53ee82bb480a8a8cb5f572f49809dda0f5afa4da
Mike-3 <Mike.Binnemann@web.de>
2023-07-15 14:57:46 +0200

    ...

--- lib/tools/science_and_technology/segment_display/_common/widget/segment_display.dart
+++ lib/tools/science_and_technology/segment_display/_common/widget/segment_display.dart
@@ -320,22 +320,22 @@ class _SegmentDisplayState extends State<SegmentDisplay> {
         if (_currentMode == GCWSwitchPosition.right) {
           _addDropDownEntry('14segment_default.png', 'AUTO', null, SegmentDisplayType.FOURTEENAUTO);
         }
-        _addDropDownEntry('14segment_default.png', 'STANDARD', null, SegmentDisplayType.FOURTEEN);
-        _addDropDownEntry('14segment_hij_g1g2_mlk.png', 'HIJ_G1G2_MLK', null, SegmentDisplayType.FOURTEEN_HIJ_G1G2_MLK);
-        _addDropDownEntry('14segment_pgh_nj_mlk.png', 'FGH_NJ_MLK', null, SegmentDisplayType.FOURTEEN_FGH_NJ_MLK);
-        _addDropDownEntry('14segment_kmn_g1g2_rst.png', 'KMN_G1G2_RST', null, SegmentDisplayType.FOURTEEN_KMN_G1G2_RST);
-        _addDropDownEntry('14segment_ghj_pk_nmi.png', 'KMN_G1G2_RST', null, SegmentDisplayType.FOURTEEN_GHJ_PK_NMI);
-        _addDropDownEntry('14segment_hjk_g1g2_nml.png', 'HJK_G1G2_NML', null, SegmentDisplayType.FOURTEEN_HJK_G1G2_NML);
+        _addDropDownEntry('14segment_default.png', 'STANDARD', '2 → abdeg1g2', SegmentDisplayType.FOURTEEN);
+        _addDropDownEntry('14segment_hij_g1g2_mlk.png', 'HIJ G1G2 MLK', '2 → abdeg1g2', SegmentDisplayType.FOURTEEN_HIJ_G1G2_MLK);
+        _addDropDownEntry('14segment_pgh_nj_mlk.png', 'FGH NJ MLK', '2 → abdenj', SegmentDisplayType.FOURTEEN_FGH_NJ_MLK);
+        _addDropDownEntry('14segment_kmn_g1g2_rst.png', 'KMN G1G2 RST', '2 → abdeg1g2', SegmentDisplayType.FOURTEEN_KMN_G1G2_RST);
+        _addDropDownEntry('14segment_ghj_pk_nmi.png', 'GHJ PK NMI', '2 → abdepk', SegmentDisplayType.FOURTEEN_GHJ_PK_NMI);
+        _addDropDownEntry('14segment_hjk_g1g2_nml.png', 'HJK G1G2 NML', '2 → abdeg1g2', SegmentDisplayType.FOURTEEN_HJK_G1G2_NML);
         break;
       case SegmentDisplayType.SIXTEEN:
-        _addDropDownEntry('16segment_default.png', 'STANDARD', null, SegmentDisplayType.SIXTEEN);
+        _addDropDownEntry('16segment_default.png', 'STANDARD', '2 → a1a2bd1d1eg1g2', SegmentDisplayType.SIXTEEN);
         break;
       default:
         if (_currentMode == GCWSwitchPosition.right) {
           _addDropDownEntry('7segment_default.png', 'AUTO', null, SegmentDisplayType.SEVENAUTO);
         }
-        _addDropDownEntry('7segment_default.png', 'STANDARD', null, SegmentDisplayType.SEVEN);
-        _addDropDownEntry('7segment_12345678.png', '12345678', null, SegmentDisplayType.SEVEN12345678);
+        _addDropDownEntry('7segment_default.png', 'STANDARD', '2 → abdeg', SegmentDisplayType.SEVEN);
+        _addDropDownEntry('7segment_12345678.png', '12345678', '2 → 12457', SegmentDisplayType.SEVEN12345678);
     }
   }
 
@@ -351,10 +351,13 @@ class _SegmentDisplayState extends State<SegmentDisplay> {
     var icon = GCWSymbolContainer(
       symbol: Image.asset('assets/icons/science_and_technology/' + iconName, width: DEFAULT_LISTITEM_SIZE),
     );
+
     return Row(children: [
       Container(
         margin: const EdgeInsets.only(left: 2, top: 2, bottom: 2, right: 10),
-        child: icon, //(icon != null) ? icon : Container(width: 50),
+        height: 150,
+        width: 130,
+        child: icon,
       ),
       Expanded(
           child: Column(
