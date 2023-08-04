ebbb4f4d4abe0623781ea5426d0d4e70a6cb1706
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-08-04 17:06:32 +0200

    reduce warnings, hints

--- test/tools/coords/centroid/centroid_center_of_gravity/logic/centroid_center_of_gravity_test.dart
+++ test/tools/coords/centroid/centroid_center_of_gravity/logic/centroid_center_of_gravity_test.dart
@@ -7,9 +7,9 @@ void main() {
 
   group("Centroid.centroidCenterOfGravity:", () {
     List<Map<String, Object?>> _inputsToExpected = [
-      {'coords' : [LatLng(34.088869, 33.3238855), LatLng(68.3433821667, 5.3754655667), LatLng(54.3917007667, -11.53589395)], 'expectedOutput': LatLng(54.017923, 12.893674)},
-      {'coords' : [LatLng(34.088869, -157.6761145), LatLng(68.3433821667, 174.3754655667), LatLng(54.3917007667, 157.4641061)], 'expectedOutput': LatLng(54.017923, -178.106326)},
-      {'coords' : [LatLng(34.088869, 157.6761145), LatLng(68.3433821667, -174.3754655667), LatLng(54.3917007667, -157.4641061)], 'expectedOutput': LatLng(54.017923, 178.106326)}
+      {'coords' : [const LatLng(34.088869, 33.3238855), const LatLng(68.3433821667, 5.3754655667), const LatLng(54.3917007667, -11.53589395)], 'expectedOutput': const LatLng(54.017923, 12.893674)},
+      {'coords' : [const LatLng(34.088869, -157.6761145), const LatLng(68.3433821667, 174.3754655667), const LatLng(54.3917007667, 157.4641061)], 'expectedOutput': const LatLng(54.017923, -178.106326)},
+      {'coords' : [const LatLng(34.088869, 157.6761145), const LatLng(68.3433821667, -174.3754655667), const LatLng(54.3917007667, -157.4641061)], 'expectedOutput': const LatLng(54.017923, 178.106326)}
     ];
 
     for (var elem in _inputsToExpected) {
