c1d05630ef646426ce42d5d8f9eb384c6b033693
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-08-16 21:16:30 +0200

    add symbol table sith

--- lib/application/registry.dart
+++ lib/application/registry.dart
@@ -3940,6 +3940,7 @@ void initializeRegistry(BuildContext context) {
     ]),
     GCWSymbolTableTool(symbolKey: 'siemens', symbolSearchStrings: const ['symbol_siemens', 'teletypewriter']),
     GCWSymbolTableTool(symbolKey: 'sign', symbolSearchStrings: const ['symbol_signlanguage']),
+    GCWSymbolTableTool(symbolKey: 'sith', symbolSearchStrings: const ['symbol_sith']),
     GCWSymbolTableTool(symbolKey: 'skullz', symbolSearchStrings: const [
       'symbol_skullz',
     ]),
