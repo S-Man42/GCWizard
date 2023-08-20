a93b34d2d31c0ca6ac6e82e30cfff66abac6d829
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-08-20 17:05:53 +0200

    correct sorting lists

--- lib/tools/miscellaneous/gcwizardscript/logic/gcwizard_script_functions_list.dart
+++ lib/tools/miscellaneous/gcwizardscript/logic/gcwizard_script_functions_list.dart
@@ -11,7 +11,7 @@ class _GCWList {
     return _contents.length;
   }
 
-  void add(Object? value ) {
+  void add(Object? value) {
     if (value != null) _contents.add(value);
   }
 
@@ -27,45 +27,104 @@ class _GCWList {
     _contents.shuffle();
   }
 
-  void sort(){
-    _contents.sort();
+  @override
+  void sort() {
+    //https://stackoverflow.com/questions/67847956/can-i-sort-listdynamic-in-dart
+    _contents.sort(
+      (a, b) {
+        if ((a is num && b is num)) {
+          return a.compareTo(b);
+        }
+        if (a is String && b is String) {
+          return a.compareTo(b);
+        }
+        if (a is _GCWList && b is _GCWList) {
+          return a.length().compareTo(b.length());
+        }
+
+        if (a is num && b is String) {
+          return -1;
+        } else if (a is num && b is _GCWList) {
+          return -1;
+        } else if (a is String && b is _GCWList) {
+          return -1;
+        } else if (a is _GCWList && b is num) {
+          return 1;
+        } else if (a is _GCWList && b is String) {
+          return 1;
+        } else if (a is String && b is num) {
+          return 1;
+        }
+        return 0;
+      },
+    );
   }
 
-  void insert(int index, Object? value){
+  void insert(int index, Object? value) {
     if (value != null) _contents.insert(index, value);
   }
 
-  void remove(int index,){
+  void remove(
+    int index,
+  ) {
     _contents.remove(index);
   }
 
-  void reverse(){
-    _contents.reversed.toList();
+  void reverse() {
+    //https://stackoverflow.com/questions/67847956/can-i-sort-listdynamic-in-dart
+    _contents.sort(
+      (a, b) {
+        if ((a is num && b is num)) {
+          return a.compareTo(b);
+        }
+        if (a is String && b is String) {
+          return a.compareTo(b);
+        }
+        if (a is _GCWList && b is _GCWList) {
+          return a.length().compareTo(b.length());
+        }
+
+        if (a is num && b is String) {
+          return 1;
+        } else if (a is num && b is _GCWList) {
+          return 1;
+        } else if (a is String && b is _GCWList) {
+          return 1;
+        } else if (a is _GCWList && b is num) {
+          return -1;
+        } else if (a is _GCWList && b is String) {
+          return -1;
+        } else if (a is String && b is num) {
+          return -1;
+        }
+        return 0;
+      },
+    );
   }
 
   @override
-  String toString(){
+  String toString() {
     return _contents.toString();
   }
 
-  Object getIndex(int index){
+  Object getIndex(int index) {
     if (_contents.isEmpty) return 'NIL';
-    if (index > _contents.length -1) return 'NIL';
+    if (index > _contents.length - 1) return 'NIL';
     return _contents[index];
   }
 
-  List<Object> getContent(){
+  List<Object> getContent() {
     return _contents;
   }
 }
 
 void _listClear(_GCWList? list) {
-   if (list != null) {
-     list.clear();
-   }
+  if (list != null) {
+    list.clear();
+  }
 }
 
-void _listAdd(_GCWList? list, Object? value){
+void _listAdd(_GCWList? list, Object? value) {
   if (list != null) {
     if (value != null) {
       list.add(value);
@@ -73,22 +132,30 @@ void _listAdd(_GCWList? list, Object? value){
   }
 }
 
-void _listSort(_GCWList? list, int mode){
+void _listSort(_GCWList? list, Object? mode) {
   if (list != null) {
-    list.sort();
-    if (mode == 0) {
-      list.reverse();
+    if (_isNotAInt(mode)) {
+      _handleError(_INVALIDTYPECAST);
+    } else {
+      switch (mode as int) {
+        case 0:
+          list.reverse();
+          break;
+        case 1:
+          list.sort();
+          break;
+      }
     }
   }
 }
 
-void _listShuffle(_GCWList? list){
+void _listShuffle(_GCWList? list) {
   if (list != null) {
     list.shuffle();
   }
 }
 
-String _listToString(_GCWList? list){
+String _listToString(_GCWList? list) {
   if (list == null) {
     return '';
   } else {
@@ -96,27 +163,41 @@ String _listToString(_GCWList? list){
   }
 }
 
-void _listAddAll(_GCWList? listto, _GCWList? listfrom){
+void _listAddAll(_GCWList? listto, _GCWList? listfrom) {
   if (listto != null) {
     listto.addall(listfrom);
   }
 }
 
-void _listInsert(_GCWList? list, int index, Object? value){
+void _listInsert(_GCWList? list, Object? index, Object? value) {
   if (list != null) {
-    //TODO range check => ERROR
-    list.insert(index, value);
+    if (_isNotAInt(index)) {
+      _handleError(_INVALIDTYPECAST);
+    } else {
+      if (index as int >= list.length()) {
+        _handleError(_RANGEERROR);
+      } else {
+        list.insert(index, value);
+      }
+    }
   }
 }
 
-void _listRemove(_GCWList? list, int index){
+void _listRemove(_GCWList? list, Object? index) {
   if (list != null) {
-    //TODO range check => ERROR
-    list.remove(index);
+    if (_isNotAInt(index)) {
+      _handleError(_INVALIDTYPECAST);
+    } else {
+      if (index as int >= list.length()) {
+        _handleError(_RANGEERROR);
+      } else {
+        list.remove(index);
+      }
+    }
   }
 }
 
-int _listLength(_GCWList? list){
+int _listLength(_GCWList? list) {
   if (list == null) {
     return 0;
   } else {
@@ -124,20 +205,26 @@ int _listLength(_GCWList? list){
   }
 }
 
-int _listIsEmpty(_GCWList? list){
+int _listIsEmpty(_GCWList? list) {
   if (_listLength(list) == 0) return 1;
   return 0;
 }
 
-int _listIsNotEmpty(_GCWList? list){
+int _listIsNotEmpty(_GCWList? list) {
   if (_listLength(list) == 0) return 0;
   return 1;
 }
 
-Object? _listGet(_GCWList? list, int index){
-  if (list == null) {
-    return 'NIL';
-  } else {
-    return list.getIndex(index);
+Object? _listGet(_GCWList? list, Object? index) {
+  if (list != null) {
+    if (_isNotAInt(index)) {
+      _handleError(_INVALIDTYPECAST);
+    } else {
+      if (index as int >= list.length()) {
+        _handleError(_RANGEERROR);
+      } else {
+        return list.getIndex(index);
+      }
+    }
   }
 }
