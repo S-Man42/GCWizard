6fa7ef0ff70b242eef0a025809706fe4082ebba0
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-08-29 16:51:54 +0200

    changes due to flutter upgrade

--- lib/common_widgets/gcw_textselectioncontrols.dart
+++ lib/common_widgets/gcw_textselectioncontrols.dart
@@ -1,4 +1,6 @@
 /* https://ktuusj.medium.com/flutter-custom-selection-toolbar-3acbe7937dd3 ***/
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_paste_button.dart';
 import 'package:gc_wizard/common_widgets/clipboard/gcw_clipboard.dart';
@@ -21,7 +23,7 @@ class GCWTextSelectionControls extends MaterialTextSelectionControls {
     Offset selectionMidpoint,
     List<TextSelectionPoint> endpoints,
     TextSelectionDelegate delegate,
-    ClipboardStatusNotifier? clipboardStatus,
+    ValueListenable<ClipboardStatus>? clipboardStatus,
     Offset? lastSecondaryTapDownPosition,
   ) {
     final TextSelectionPoint startTextSelectionPoint = endpoints[0];
@@ -36,7 +38,7 @@ class GCWTextSelectionControls extends MaterialTextSelectionControls {
     return _GCWTextSelectionToolbar(
       anchorAbove: anchorAbove,
       anchorBelow: anchorBelow,
-      clipboardStatus: clipboardStatus,
+      clipboardStatus: null,
       delegate: delegate,
       handleCopy: canCopy(delegate)
           ? () {
@@ -89,7 +91,7 @@ bool canCopy(TextSelectionDelegate delegate) {
 }
 
 /// copy of deprecated function
-void handleCopy(TextSelectionDelegate delegate, ClipboardStatusNotifier? clipboardStatus) {
+void handleCopy(TextSelectionDelegate delegate, ValueListenable<ClipboardStatus>? clipboardStatus) {
     delegate.copySelection(SelectionChangedCause.toolbar);
 }
 
@@ -99,7 +101,7 @@ bool canCut(TextSelectionDelegate delegate) {
 }
 
 /// copy of deprecated function
-void handleCut(TextSelectionDelegate delegate, ClipboardStatusNotifier? clipboardStatus) {
+void handleCut(TextSelectionDelegate delegate, ValueListenable<ClipboardStatus>? clipboardStatus) {
   delegate.cutSelection(SelectionChangedCause.toolbar);
 }
 
