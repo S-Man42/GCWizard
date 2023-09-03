b47949704ff3ec25d78184ceeab6168ac125ed52
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-09-03 20:04:04 +0200

    refactor widget heatindex

--- lib/tools/science_and_technology/apparent_temperature/heat_index/widget/heat_index.dart
+++ lib/tools/science_and_technology/apparent_temperature/heat_index/widget/heat_index.dart
@@ -4,6 +4,7 @@ import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 import 'package:gc_wizard/application/theme/theme.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_iconbutton.dart';
 import 'package:gc_wizard/common_widgets/dividers/gcw_divider.dart';
+import 'package:gc_wizard/common_widgets/dividers/gcw_text_divider.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_default_output.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_output.dart';
 import 'package:gc_wizard/common_widgets/units/gcw_unit_dropdown.dart';
@@ -88,52 +89,51 @@ class _HeatIndexState extends State<HeatIndex> {
       children: [
         GCWDefaultOutput(
             child: Row(children: <Widget>[
-          Expanded(
-            flex: 4,
-            child: Text(i18n(context, 'heatindex_title')),
-          ),
-          Expanded(
-            flex: 2,
-            //child: Text(_currentOutputUnit.symbol),
-            child: Container(
-                margin: const EdgeInsets.only(left: DEFAULT_MARGIN, right: DEFAULT_MARGIN),
-                child: GCWUnitDropDown(
-                  value: _currentOutputUnit,
-                  onlyShowSymbols: true,
-                  unitList: temperatures,
-                  unitCategory: UNITCATEGORY_TEMPERATURE,
-                  onChanged: (value) {
-                    setState(() {
-                      _currentOutputUnit = value;
-                    });
-                  },
-                )),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-                margin: const EdgeInsets.only(left: DEFAULT_MARGIN),
-                child: GCWOutput(child: NumberFormat('#.###').format(HeatIndex))),
-          ),
+              Container(
+                width: 50,
+                padding: const EdgeInsets.only(right: DOUBLE_DEFAULT_MARGIN),
+                child: GCWIconButton(
+                  icon: Icons.wb_sunny,
+                  iconColor: _colorHeatIndex(HeatIndex_C),
+                  backgroundColor: const Color(0xFF4d4d4d),
+                  onPressed: () {},
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                //child: Text(_currentOutputUnit.symbol),
+                child: Container(
+                    margin: const EdgeInsets.only(left: DEFAULT_MARGIN, right: 2 * DEFAULT_MARGIN),
+                    child: GCWUnitDropDown(
+                      value: _currentOutputUnit,
+                      onlyShowSymbols: false,
+                      unitList: temperatures,
+                      unitCategory: UNITCATEGORY_TEMPERATURE,
+                      onChanged: (value) {
+                        setState(() {
+                          _currentOutputUnit = value;
+                        });
+                      },
+                    )),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                    margin: const EdgeInsets.only(left: 2 * DEFAULT_MARGIN),
+                    child: GCWOutput(child: NumberFormat('#.###').format(HeatIndex))),
+              ),
         ])),
-        const GCWDivider(),
-        Row(
-          children: [
-            Container(
-              width: 50,
-              padding: const EdgeInsets.only(right: DOUBLE_DEFAULT_MARGIN),
-              child: GCWIconButton(
-                icon: Icons.wb_sunny,
-                iconColor: _colorHeatIndex(HeatIndex_C),
-                backgroundColor: const Color(0xFF4d4d4d),
-                onPressed: () {},
-              ),
+        Column(
+          children: <Widget>[
+            GCWTextDivider(text: i18n(context, 'heatindex_hint')),
+            Row(
+              children: <Widget> [
+
+              ]
             ),
-            Expanded(
-              child: GCWOutput(
-                child: hint != '' ? hint : i18n(context, hintHeatIndex),
-              ),
-            )
+            GCWOutput(
+              child: hint != '' ? hint : i18n(context, hintHeatIndex),
+            ),
           ],
         )
       ],
