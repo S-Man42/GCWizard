25dcd12a3a1073b5ed9632515fdb149079166817
Mike-3 <Mike.Binnemann@web.de>
2023-09-13 14:53:08 +0200

    add tests

---
+++ test/tools/games/nonogram/logic/gap_distributor_test.dart
@@ -0,0 +1,98 @@
+import "package:flutter_test/flutter_test.dart";
+import 'package:gc_wizard/tools/games/nonogram/logic/gap_distributor.dart';
+
+void main() {
+  group("Nonogram.gap_distributor:",() {
+    List<Map<String,Object?>> _inputsToExpected = [
+      {// works, one gap, one hint
+        'line': [0, 0, 0, 0, 0],
+        'hints': [2],
+        'expectedOutputGaps': [[0, 5]], 'expectedOutputDistributions': [[[2]]],
+      },
+      {// two hints, one gap, one hint
+        'line': [0, 0, 0, 0, 0],
+        'hints': [2, 2],
+        'expectedOutputGaps': [[0, 5]], 'expectedOutputDistributions': [[[2, 2]]],
+      },
+      {// works with short hint, one gap, one hint
+        'line': [0, 0, 0, 0, -1, 0, 0, 0],
+        'hints': [2],
+        'expectedOutputGaps': [[0, 4], [5, 8]], 'expectedOutputDistributions': [[<int>[], [2]], [[2], <int>[]]],
+      },
+      {// works with long hint, one gap, one hint
+        'line': [0, 0, -1, 0, 0, 0, -1, 0, 0, 0],
+        'hints': [3],
+        'expectedOutputGaps': [[0, 2], [3, 6], [7, 10]], 'expectedOutputDistributions': [[<int>[], <int>[], [3]], [<int>[], [3], <int>[]]],
+      },
+      {// works with 2,1, one gap, one hint
+        'line': [0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0],
+        'hints': [2, 1],
+        'expectedOutputGaps': [[0, 5], [7, 12]], 'expectedOutputDistributions': [[<int>[], [2, 1]], [[2], [1]], [[2, 1], <int>[]]],
+      },
+      {// works with 2,2, one gap, one hint
+        'line': [0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0],
+        'hints': [2, 2],
+        'expectedOutputGaps': [[0, 5], [7, 12]], 'expectedOutputDistributions': [[<int>[], [2, 2]], [[2], [2]], [[2, 2], <int>[]]],
+      },
+      {// works with 2,3, one gap, one hint
+        'line': [0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0, 0],
+        'hints': [2, 3],
+        'expectedOutputGaps': [[0, 5], [7, 12]], 'expectedOutputDistributions': [[[2], [3]]],
+      },
+      {// one hint, one gap, distributing in non-empty gap
+        'line': [0, 0, 1, 0, 0],
+        'hints': [2],
+        'expectedOutputGaps': [[0, 5]], 'expectedOutputDistributions': [[[2]]],
+      },
+      {// two hints, one gap
+        'line': [0, 1, 0, 0, 0],
+        'hints': [2, 2],
+        'expectedOutputGaps': [[0, 5]], 'expectedOutputDistributions': [[[2, 2]]],
+      },
+      {// works with impossible, one gap
+        'line': [0, 0, 1, 0],
+        'hints': [2, 2],
+        'expectedOutputGaps' : null,
+      },
+      {// complicated case, one gap
+      'line': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+      'hints': [2, 2],
+      'expectedOutputGaps' : null,
+      },
+      {// multiple gaps, one gap
+        'line': [0, 0, 0, 0, -1, 0, 1, 0],
+        'hints': [2],
+        'expectedOutputGaps': [[0, 4], [5, 8]], 'expectedOutputDistributions': [[<int>[], [2]]],
+      },
+      {// works with long hint, one gap
+        'line': [0, 0, -1, 0, 1, 0, -1, 0, 0, 0],
+        'hints': [3, 3],
+        'expectedOutputGaps': [[0, 2], [3, 6], [7, 10]], 'expectedOutputDistributions': [[<int>[], [3], [3]]],
+      },
+      {// two hints, one gap
+        'line': [0, 0, 1, 0, 0, -1, -1, 0, 0, 0, 0, 0],
+        'hints': [2, 1],
+        'expectedOutputGaps': [[0, 5], [7, 12]], 'expectedOutputDistributions': [[[2], [1]], [[2, 1], <int>[]]],
+      },
+      {// works with 2,2, one gap
+        'line': [0, 1, 0, 0, 0, -1, -1, 0, 0, 0, 1, 0],
+        'hints': [2, 2],
+        'expectedOutputGaps': [[0, 5], [7, 12]], 'expectedOutputDistributions': [[[2], [2]]],
+      },
+      {// works with 2,3, one gap
+        'line': [0, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 0],
+        'hints': [[1,1,1,1],[0],[1,1,1,1]],
+        'expectedOutputGaps': [[0, 5], [7, 12]], 'expectedOutputDistributions': [[[2], [3]]],
+      },
+    ];
+
+    for (var elem in _inputsToExpected) {
+      test('line: ${elem['line']},hints: ${elem['hints']}',() {
+        var _actual = gapDistributor(elem['line'] as List<int>, elem['hints'] as List<int>);
+
+        expect(_actual?.gaps ,elem['expectedOutputGaps']);
+        expect(_actual?.distributions ,elem['expectedOutputDistributions']);
+      });
+    }
+  });
+}
---
+++ test/tools/games/nonogram/logic/push_solver_test.dart
@@ -0,0 +1,207 @@
+import "package:flutter_test/flutter_test.dart";
+import 'package:gc_wizard/tools/games/nonogram/logic/push_solver.dart';
+
+void main() {
+  group("Nonogram.push_solver:",() {
+    List<Map<String,Object?>> _inputsToExpected = [
+      {// works with a single hint on an empty line
+        'line': [0, 0, 0, 0, 0],
+        'hints': [3],
+        'expectedOutput':  [1, 1, 1, 0, 0],
+      },
+      {
+        'line': [0, 0, 0],
+        'hints': [3],
+        'expectedOutput': [1, 1, 1],
+      },
+      {
+        'line': [0, 0],
+        'hints': [3],
+        'expectedOutput': null,
+      },
+      {// works with multiple hints on an empty line
+        'line': [0, 0, 0, 0, 0],
+        'hints': [1, 2],
+        'expectedOutput': [1, 0, 1, 1, 0],
+      },
+      {
+        'line':  [0, 0, 0, 0, 0, 0, 0, 0],
+        'hints': [3, 1, 1],
+        'expectedOutput': [1, 1, 1, 0, 1, 0, 1, 0],
+      },
+      {
+        'line': [0, 0, 0, 0],
+        'hints': [2, 2],
+        'expectedOutput': null,
+      },
+      {// works with a single hint on a partially filled line
+        'line': [0, 1, 0, 0, 0],
+        'hints': [3],
+        'expectedOutput': [1, 1, 1, 0, 0],
+      },
+      {
+        'line': [0, 1, 0],
+        'hints': [3],
+        'expectedOutput': [1, 1, 1],
+      },
+      {
+        'line': [0, 0, 0, 1, 0, 0],
+        'hints': [3],
+        'expectedOutput':  [0, 1, 1, 1, 0, 0],
+      },
+      {
+        'line': [0, 1, 0, 1, 0, 0],
+        'hints': [2],
+        'expectedOutput' : null,
+      },
+      {// complicated case, one gap
+      'line': [0, 0, 0, 1, 1, 0],
+      'hints': [3],
+      'expectedOutput' : [0, 0, 1, 1, 1, 0],
+      },
+      {
+        'line': [0, 0, 1, 0, 1, 0],
+        'hints': [4],
+        'expectedOutput': [0, 1, 1, 1, 1, 0],
+      },
+      {
+        'line': [0, 1],
+        'hints': [3],
+        'expectedOutput': null,
+      },
+      {
+        'line': [1, 0, 1, 0, 1, 0, 1],
+        'hints': [2],
+        'expectedOutput': null,
+      },
+      {// works with multiple hints on a partially filled line
+        'line': [0, 1, 0, 0, 0],
+        'hints': [3, 1],
+        'expectedOutput': [1, 1, 1, 0, 1],
+      },
+      {
+        'line': [0, 1, 0, 0, 0, 0, 0],
+        'hints': [3, 1],
+        'expectedOutput': [0, 1, 1, 1, 0, 1],
+      },
+      {
+        'line': [0, 0, 0, 1, 1, 0, 0, 0, 0],
+        'hints': [2, 3, 1],
+        'expectedOutput': [1, 1, 0, 1, 1, 1, 0, 1, 0],
+      },
+      {
+        'line': [0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+        'hints': [3, 3, 1],
+        'expectedOutput': [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+      },
+      {
+        'line':  [0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+        'hints': [3, 3, 1],
+        'expectedOutput': null,
+      },
+      {//'with gaps, works with one gap and occupation
+        'line':  [0, 0, -1, 0, 1],
+        'hints': [1],
+        'expectedOutput': [-1, -1, -1, -1, 1],
+      },
+      {//'without gaps, works with empty line and one hint
+        'line':  [0, 0, 0, 0, 0],
+        'hints': [3],
+        'expectedOutput': [0, 0, 1, 0, 0],
+      },
+      {
+        'line':  [0, 0, 0, 0, 0],
+        'hints': [4],
+        'expectedOutput': [0, 1, 1, 1, 0],
+      },
+      {
+        'line':  [0, 0, 0, 0, 0],
+        'hints': [2],
+        'expectedOutput': [0, 0, 0, 0, 0],
+      },
+      {// works with empty line and multiple hints
+        'line':  [0, 0, 0, 0, 0, 0],
+        'hints': [4, 1, 1],
+        'expectedOutput': [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      },
+      {
+        'line': [0, 0, 0, 0, 0, 0],
+        'hints': [2, 1],
+        'expectedOutput': [0, 0, 0, 0, 0, 0],
+      },
+      {// works with partially filled line and one hint
+        'line': [0, 1, 0, 0, 0, 0],
+        'hints': [3],
+        'expectedOutput': [0, 1, 1, 0, -1, -1],
+      },
+      {
+        'line': [1, 0, 0, 0, 0],
+        'hints': [3],
+        'expectedOutput': [1, 1, 1, -1, -1],
+      },
+      {
+        'line': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        'hints': [4],
+        'expectedOutput': [-1, 0, 0, 0, 1, 0, 0, 0, -1, -1],
+      },
+      {
+        'line': [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+        'hints': [4],
+        'expectedOutput': [-1, -1, -1, 0, 1, 1, 1, 0, -1, -1],
+      },
+      {// works with partially filled line and multiple hints
+        'line': [0, 1, 0, 0, 0, 0, 1, 0],
+        'hints': [3, 3],
+        'expectedOutput': [0, 1, 1, 0, 0, 1, 1, 0],
+      },
+      {
+        'line': [0, 1, 0, 0, 0, 1, 0],
+        'hints': [3, 3],
+        'expectedOutput': [1, 1, 1, -1, 1, 1, 1],
+      },
+      {
+        'line': [1, 0, 0, 0, 0, 1],
+        'hints': [3, 2],
+        'expectedOutput': [1, 1, 1, -1, 1, 1],
+      },
+      {
+        'line': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        'hints': [4, 1],
+        'expectedOutput': [-1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      },
+      {
+        'line': [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+        'hints': [4, 2],
+        'expectedOutput': [-1, 0, 0, 1, 1, 0, 1, 0, 0, 0],
+      },
+      {// with gaps, works with one gap and some occupation and one hint
+        'line': [0, 0, -1, 0, 1],
+        'hints': [1],
+        'expectedOutput': [-1, -1, -1, -1, 1],
+      },
+      {// works with multiple gaps and some occupation and multiple hints
+        'line': [0, 0, 0, -1, 1, 0, 0, -1, 0, 0, 0],
+        'hints': [3, 1, 3],
+        'expectedOutput': [1, 1, 1, -1, 1, -1, -1, -1, 1, 1, 1],
+      },
+      {// works with multiple gaps and some occupation and multiple hints 2
+        'line': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, -1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+        'hints': [2, 3, 7],
+        'expectedOutput': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, -1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+      },
+      {// works with multiple gaps and some occupation and multiple hints 3
+        'line': [-1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, -1, -1, 1, 1, -1, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 1, -1],
+        'hints': [14, 1, 2, 2, 1, 1],
+        'expectedOutput': [-1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 0, 0, -1, -1, 1, 1, -1, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 1, -1],
+      },
+    ];
+
+    for (var elem in _inputsToExpected) {
+      test('line: ${elem['line']},hints: ${elem['hints']}',() {
+        var _actual = pushSolver().solve(elem['line'] as List<int>, elem['hints'] as List<int>);
+
+        expect(_actual ,elem['expectedOutput']);
+      });
+    }
+  });
+}
