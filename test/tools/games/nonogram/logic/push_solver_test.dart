c9457243e91ea35ce5f4fcd1e165694642583b7a
Mike-3 <Mike.Binnemann@web.de>
2023-09-14 18:50:01 +0200

    Update push_solver_test.dart

--- test/tools/games/nonogram/logic/push_solver_test.dart
+++ test/tools/games/nonogram/logic/push_solver_test.dart
@@ -2,7 +2,7 @@ import "package:flutter_test/flutter_test.dart";
 import 'package:gc_wizard/tools/games/nonogram/logic/push_solver.dart';
 
 void main() {
-  group("Nonogram.push_solver:",() {
+  group("Nonogram.push_left:",() {
     List<Map<String,Object?>> _inputsToExpected = [
       {// works with a single hint on an empty line
         'line': [0, 0, 0, 0, 0],
@@ -55,9 +55,9 @@ void main() {
         'expectedOutput' : null,
       },
       {// complicated case, one gap
-      'line': [0, 0, 0, 1, 1, 0],
-      'hints': [3],
-      'expectedOutput' : [0, 0, 1, 1, 1, 0],
+        'line': [0, 0, 0, 1, 1, 0],
+        'hints': [3],
+        'expectedOutput' : [0, 0, 1, 1, 1, 0],
       },
       {
         'line': [0, 0, 1, 0, 1, 0],
@@ -80,7 +80,7 @@ void main() {
         'expectedOutput': [1, 1, 1, 0, 1],
       },
       {
-        'line': [0, 1, 0, 0, 0, 0, 0],
+        'line': [0, 1, 0, 1, 0, 1],
         'hints': [3, 1],
         'expectedOutput': [0, 1, 1, 1, 0, 1],
       },
@@ -99,6 +99,20 @@ void main() {
         'hints': [3, 3, 1],
         'expectedOutput': null,
       },
+    ];
+
+    for (var elem in _inputsToExpected) {
+      test('line: ${elem['line']},hints: ${elem['hints']}',() {
+        var _actual = pushSolver.pushLeft(elem['line'] as List<int>, elem['hints'] as List<int>);
+
+        expect(_actual ,elem['expectedOutput']);
+      });
+    }
+  });
+
+
+  group("Nonogram.push_solver:",() {
+    List<Map<String,Object?>> _inputsToExpected = [
       {//'with gaps, works with one gap and occupation
         'line':  [0, 0, -1, 0, 1],
         'hints': [1],
@@ -116,11 +130,21 @@ void main() {
       },
       {
         'line':  [0, 0, 0, 0, 0],
+        'hints': [5],
+        'expectedOutput': [1, 1, 1, 1, 1],
+      },
+      {
+        'line':  [0, 0, 0, 0, 0],
         'hints': [2],
         'expectedOutput': [0, 0, 0, 0, 0],
       },
       {// works with empty line and multiple hints
         'line':  [0, 0, 0, 0, 0, 0],
+        'hints':  [2, 2],
+        'expectedOutput': [0, 1, 0, 0, 1, 0],
+      },
+      {// works with empty line and multiple hints
+        'line':  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         'hints': [4, 1, 1],
         'expectedOutput': [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
       },
