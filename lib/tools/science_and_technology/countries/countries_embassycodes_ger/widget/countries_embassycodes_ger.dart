aa9b11983e43fa267d62ad1ffeabf35353d67014
S-Man42 <info@s-man42.de>
2023-09-15 13:27:38 +0200

    fix embassy

--- lib/tools/science_and_technology/countries/countries_embassycodes_ger/widget/countries_embassycodes_ger.dart
+++ lib/tools/science_and_technology/countries/countries_embassycodes_ger/widget/countries_embassycodes_ger.dart
@@ -3,5 +3,5 @@ import 'package:gc_wizard/tools/science_and_technology/countries/countries/widge
 import 'package:gc_wizard/tools/science_and_technology/countries/logic/countries.dart';
 
 class CountriesEmbassyCodesGER extends Countries {
-  CountriesEmbassyCodesGER({Key? key}) : super(key: key, fields: [CountryProperties.embassy_plate_code_de]);
+  CountriesEmbassyCodesGER({Key? key}) : super(key: key, fields: [CountryProperties.embassycodes_ger]);
 }
