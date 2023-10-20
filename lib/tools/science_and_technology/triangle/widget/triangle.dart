f77d4dfa381b0a975f1732f38ec188fe74b56515
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-10-20 21:47:19 +0200

    correct path

--- lib/tools/science_and_technology/triangle/widget/triangle.dart
+++ lib/tools/science_and_technology/triangle/widget/triangle.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 
-import 'package:gc_wizard/application/i18n/app_localizations.dart';
 import 'package:gc_wizard/application/theme/theme.dart';
 import 'package:gc_wizard/common_widgets/dividers/gcw_text_divider.dart';
 import 'package:gc_wizard/common_widgets/gcw_expandable.dart';
