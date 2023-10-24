362273cc2e22aa3c3e366065ecb273d171825591
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-10-24 19:52:47 +0200

    add danish alphabet

--- lib/utils/alphabets.dart
+++ lib/utils/alphabets.dart
@@ -153,6 +153,14 @@ const Alphabet alphabetGerman3 = Alphabet(key: 'alphabet_name_german3', type: Al
   '\u00DF': '38', // ß
 });
 
+const Alphabet alphabetDanish = Alphabet(key: 'alphabet_name_danish', type: AlphabetType.STANDARD, alphabet: {
+  'A': '1', 'B': '2', 'C': '3', 'D': '4', 'E': '5', 'F': '6', 'G': '7', 'H': '8', 'I': '9', 'J': '10', 'K': '11',
+  'L': '12', 'M': '13',
+  'N': '14', 'O': '15', 'P': '16', 'Q': '17', 'R': '18', 'S': '19', 'T': '20', 'U': '21', 'V': '22', 'W': '23',
+  'X': '24', 'Y': '25', 'Z': '26',
+  'Æ': '27', 'Ø': '28', 'Å': '29'
+});
+
 const Alphabet alphabetEnglish = Alphabet(key: 'alphabet_name_english', type: AlphabetType.STANDARD, alphabet: {
   'A': '1', 'B': '2', 'C': '3', 'D': '4', 'E': '5', 'F': '6', 'G': '7', 'H': '8', 'I': '9', 'J': '10', 'K': '11',
   'L': '12', 'M': '13',
