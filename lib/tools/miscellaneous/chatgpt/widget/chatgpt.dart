3e4b27a84cd8d4fc242fcb19b728f808db91d778
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-11-01 22:25:51 +0100

    enhance widget

--- lib/tools/miscellaneous/chatgpt/widget/chatgpt.dart
+++ lib/tools/miscellaneous/chatgpt/widget/chatgpt.dart
@@ -3,6 +3,7 @@ import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 import 'package:gc_wizard/application/settings/logic/preferences.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_button.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_default_output.dart';
+import 'package:gc_wizard/common_widgets/spinners/gcw_double_spinner.dart';
 import 'package:gc_wizard/common_widgets/switches/gcw_twooptions_switch.dart';
 import 'package:gc_wizard/common_widgets/textfields/gcw_textfield.dart';
 import 'package:dart_openai/dart_openai.dart';
@@ -20,7 +21,7 @@ class _ChatGPTState extends State<ChatGPT> {
   late TextEditingController _temperatureController;
 
   String _currentPrompt = '';
-  String _currentTemperature = '';
+  double _currentTemperature = 0.0;
   String _currentOutput = '';
 
   GCWSwitchPosition _currentMode = GCWSwitchPosition.left;
@@ -30,13 +31,11 @@ class _ChatGPTState extends State<ChatGPT> {
     super.initState();
 
     _promptController = TextEditingController(text: _currentPrompt);
-    _temperatureController = TextEditingController(text: _currentTemperature);
   }
 
   @override
   void dispose() {
     _promptController.dispose();
-    _temperatureController.dispose();
 
     super.dispose();
   }
@@ -54,6 +53,16 @@ class _ChatGPTState extends State<ChatGPT> {
             });
           },
         ),
+        GCWDoubleSpinner(
+          title: i18n(context, 'chatgpt_temperature'),
+            min: 0,
+            max: 1,
+            onChanged: (value) {
+              setState(() {
+                _currentTemperature = value;
+              });
+            },
+            value: _currentTemperature),
         GCWTwoOptionsSwitch(
           leftValue: i18n(context, 'chatgpt_text'),
           rightValue: i18n(context, 'chatgpt_image'),
@@ -83,6 +92,7 @@ class _ChatGPTState extends State<ChatGPT> {
     final completion = await OpenAI.instance.completion.create(
       model: "text-davinci-003",
       prompt: _currentPrompt,
+      temperature: _currentTemperature,
     );
 
     if (_currentMode == GCWSwitchPosition.left) {
@@ -96,7 +106,6 @@ class _ChatGPTState extends State<ChatGPT> {
       // Printing the output to the console.
       print(image.data.first.url);
     }
-
 
     // create a moderation
     //final moderation = await OpenAI.instance.moderation.create(
