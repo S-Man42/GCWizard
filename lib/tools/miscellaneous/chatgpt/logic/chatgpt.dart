d6580c2a9365eba65258dbb3d51cad908c5ac84f
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2023-11-04 11:04:33 +0100

    refactor API usage

---
+++ lib/tools/miscellaneous/chatgpt/logic/chatgpt.dart
@@ -0,0 +1,10 @@
+
+Future<List<OpenAIModelModel>> ChatGPT_getModels() async {
+    String httpCode = '';
+      String httpMessage = '';
+      late Uri uri;
+      http.Response response;
+    try {
+    } catch (e) {
+    }
+}
