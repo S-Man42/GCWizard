5e17c4219928c2ca93d442554c104d85fc635917
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2024-01-04 19:36:28 +0100

    add searchstrings

--- lib/application/registry.dart
+++ lib/application/registry.dart
@@ -4257,6 +4257,7 @@ void initializeRegistry(BuildContext context) {
     ]),
     GCWSymbolTableTool(symbolKey: 'maya_numerals', symbolSearchStrings: const [
       'mayanumbers',
+      'symbol_maya_number_gyphys',
     ]),
     GCWSymbolTableTool(symbolKey: 'maze', symbolSearchStrings: const [
       'symbol_maze',
