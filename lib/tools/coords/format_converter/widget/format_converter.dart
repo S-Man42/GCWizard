060073162d3fc38d0956e7e2b4243e65c594bdb3
Mike-3 <Mike.Binnemann@web.de>
2024-01-24 20:54:51 +0100

    Update format_converter.dart

--- lib/tools/coords/format_converter/widget/format_converter.dart
+++ lib/tools/coords/format_converter/widget/format_converter.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_submit_button.dart';
-import 'package:gc_wizard/common_widgets/outputs/gcw_output.dart';
 import 'package:gc_wizard/tools/coords/_common/widget/gcw_coords.dart';
 import 'package:gc_wizard/tools/coords/_common/widget/gcw_coords_formatselector.dart';
 import 'package:gc_wizard/tools/coords/_common/widget/gcw_coords_output/gcw_coords_output.dart';
