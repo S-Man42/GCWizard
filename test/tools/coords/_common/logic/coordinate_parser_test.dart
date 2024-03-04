20fd51b75aed28710bbee5d91230b1087f2e7adc
Mike-3 <Mike.Binnemann@web.de>
2024-03-04 20:38:15 +0100

    Update coordinate_parser_test.dart

--- test/tools/coords/_common/logic/coordinate_parser_test.dart
+++ test/tools/coords/_common/logic/coordinate_parser_test.dart
@@ -1,7 +1,6 @@
 import "package:flutter_test/flutter_test.dart";
 import 'package:gc_wizard/tools/coords/_common/formats/dec/logic/dec.dart';
 import 'package:gc_wizard/tools/coords/_common/formats/dmm/logic/dmm.dart';
-import 'package:gc_wizard/tools/coords/_common/formats/reversewherigo_waldmeister/logic/reverse_wherigo_waldmeister.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/coordinate_format_constants.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/coordinate_parser.dart';
 import 'package:latlong2/latlong.dart';
@@ -12,7 +11,7 @@ import '../formats/dms/logic/dms_test.dart';
 
 void main() {
 
-  group("Parser.latlon.parseLatLon:", () {
+  group("Coordinates.parseCoordinates:", () {
     List<Map<String, Object?>> _inputsToExpected = inputsToExpectedDEC;
     _inputsToExpected.addAll(inputsToExpectedDMM);
     _inputsToExpected.addAll(inputsToExpectedDMS);
@@ -22,35 +21,14 @@ void main() {
       .forEach((elem) {
         test('text: ${elem['text']}', () {
           var _actual = parseCoordinates(elem['text'] as String);
-          expect(_actual.elementAt(0).format.type, (elem['expectedOutput'] as Map<String, Object>)['format']);
-          expect((_actual.elementAt(0).toLatLng()!.latitude - ((elem['expectedOutput'] as Map<String, Object>)['coordinate'] as LatLng).latitude).abs() < 1e-8, true);
-          expect((_actual.elementAt(0).toLatLng()!.longitude - ((elem['expectedOutput'] as Map<String, Object>)['coordinate'] as LatLng).longitude).abs() < 1e-8, true);
+          expect(_actual.first.format.type, (elem['expectedOutput'] as Map<String, Object>)['format']);
+          expect((_actual.first.toLatLng()!.latitude - ((elem['expectedOutput'] as Map<String, Object>)['coordinate'] as LatLng).latitude).abs() < 1e-8, true);
+          expect((_actual.first.toLatLng()!.longitude - ((elem['expectedOutput'] as Map<String, Object>)['coordinate'] as LatLng).longitude).abs() < 1e-8, true);
+          expect(_actual.where((coords) => standardCoordinateFormatDefinitions.map((e) => e.type).contains(coords.format.type)).length, 1);
         });
       });
   });
 
-  group("Coordinates.parseCoordinates", ()  {
-
-    List<Map<String, Object?>> _inputsToExpected = [
-
-      {'input': "N 50° 59.403' E 011° 02.693", 'expectedOutput': DMMCoordinate},
-      {'input': "500162 191310 948307", 'expectedOutput': ReverseWherigoWaldmeisterCoordinate},
-      {'input': "52.12312 N 20.12312 E", 'expectedOutput': DECCoordinate},
-    ];
-
-    for (var elem in _inputsToExpected) {
-      test('input: ${elem['input']}', () {
-        var _actual = parseCoordinates(elem['input'] as String);
-
-        expect(_actual.first.runtimeType, elem['expectedOutput']);
-        var baseFormatCounter = 0;
-        for (var coords in _actual) {
-          if (standardCoordinateFormatDefinitions.contains( coords)) baseFormatCounter++;
-        }
-        expect(baseFormatCounter <= 1, true);
-      });
-    }
-  });
 
   group("Coordinates.parseStandardFormats", ()  {
 
