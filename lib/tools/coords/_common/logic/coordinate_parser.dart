de7275ea26ab40795849069e7eee2daffb490f07
Mike-3 <Mike.Binnemann@web.de>
2024-03-04 18:41:24 +0100

    bugfix coords parse standard formats

--- lib/tools/coords/_common/logic/coordinate_parser.dart
+++ lib/tools/coords/_common/logic/coordinate_parser.dart
@@ -1,10 +1,5 @@
-import 'package:gc_wizard/tools/coords/_common/formats/dec/logic/dec.dart';
-import 'package:gc_wizard/tools/coords/_common/formats/dmm/logic/dmm.dart';
-import 'package:gc_wizard/tools/coords/_common/formats/dms/logic/dms.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/coordinate_format_constants.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/coordinates.dart';
-import 'package:latlong2/latlong.dart';
-
 
 //wholeString == false: The first match at the text begin is taken - for copy
 //wholeString == true: The whole text must be a valid coord - for var coords
@@ -13,9 +8,13 @@ List<BaseCoordinate> parseCoordinates(String text, {bool wholeString = false}) {
 
   try {
     BaseCoordinate? coord;
+    coord = parseStandardFormats(text, wholeString: wholeString);
+    if (coord != null) coords.add(coord);
     for (var format in allCoordinateFormatDefinitions) {
-      coord = (wholeString ? format.parseCoordinateWholeString(text) : format.parseCoordinate(text));
-      if (coord != null) coords.add(coord);
+      if (!standardCoordinateFormatDefinitions.contains(format)) {
+        coord = (wholeString ? format.parseCoordinateWholeString(text) : format.parseCoordinate(text));
+        if (coord != null) coords.add(coord);
+      }
     }
   } catch (e) {}
 
@@ -25,15 +24,12 @@ List<BaseCoordinate> parseCoordinates(String text, {bool wholeString = false}) {
 //wholeString == false: The first match at the text begin is taken - for copy
 //wholeString == true: The whole text must be a valid coord - for var coords
 BaseCoordinate? parseStandardFormats(String text, {bool wholeString = false}) {
-  LatLng? coord;
-  coord = (wholeString ? DMSCoordinate.parseWholeString(text) : DMSCoordinate.parse(text))?.toLatLng();
-  if (coord != null) return DMSCoordinate.fromLatLon(coord);
 
-  coord = (wholeString ? DMMCoordinate.parseWholeString(text) : DMMCoordinate.parse(text))?.toLatLng();
-  if (coord != null) return DMMCoordinate.fromLatLon(coord);
-
-  coord = (wholeString ? DECCoordinate.parseWholeString(text) : DECCoordinate.parse(text))?.toLatLng();
-  if (coord != null) return DECCoordinate.fromLatLon(coord);
+  for (var format in standardCoordinateFormatDefinitions) {
+    var coord = (wholeString ? format.parseCoordinateWholeString(text) : format.parseCoordinate(text));
+    if (coord != null) return coord;
+  }
 
   return null;
 }
+
--- test/tools/coords/_common/logic/coordinate_parser_test.dart
+++ test/tools/coords/_common/logic/coordinate_parser_test.dart
@@ -1,4 +1,8 @@
 import "package:flutter_test/flutter_test.dart";
+import 'package:gc_wizard/tools/coords/_common/formats/dec/logic/dec.dart';
+import 'package:gc_wizard/tools/coords/_common/formats/dmm/logic/dmm.dart';
+import 'package:gc_wizard/tools/coords/_common/formats/reversewherigo_waldmeister/logic/reverse_wherigo_waldmeister.dart';
+import 'package:gc_wizard/tools/coords/_common/logic/coordinate_format_constants.dart';
 import 'package:gc_wizard/tools/coords/_common/logic/coordinate_parser.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -23,6 +27,47 @@ void main() {
           expect((_actual.elementAt(0).toLatLng()!.longitude - ((elem['expectedOutput'] as Map<String, Object>)['coordinate'] as LatLng).longitude).abs() < 1e-8, true);
         });
       });
+  });
+
+  group("Coordinates.parseCoordinates", ()  {
+
+    List<Map<String, Object?>> _inputsToExpected = [
+
+      {'input': "N 50° 59.403' E 011° 02.693", 'expectedOutput': DMMCoordinate},
+      {'input': "500162 191310 948307", 'expectedOutput': ReverseWherigoWaldmeisterCoordinate},
+      {'input': "52.12312 N 20.12312 E", 'expectedOutput': DECCoordinate},
+    ];
+
+    for (var elem in _inputsToExpected) {
+      test('input: ${elem['input']}', () {
+        var _actual = parseCoordinates(elem['input'] as String);
+
+        expect(_actual.first.runtimeType, elem['expectedOutput']);
+        var baseFormatCounter = 0;
+        for (var coords in _actual) {
+          if (standardCoordinateFormatDefinitions.contains( coords)) baseFormatCounter++;
+        }
+        expect(baseFormatCounter <= 1, true);
+      });
+    }
+  });
+
+  group("Coordinates.parseStandardFormats", ()  {
+
+    List<Map<String, Object?>> _inputsToExpected = [
+
+      {'input': "N 50° 59.403' E 011° 02.693", 'expectedOutput': DMMCoordinate},
+      {'input': "500162 191310 948307", 'expectedOutput': null},
+      {'input': "52.12312 N 20.12312 E", 'expectedOutput': DECCoordinate},
+    ];
+
+    for (var elem in _inputsToExpected) {
+      test('input: ${elem['input']}', () {
+        var _actual = parseStandardFormats(elem['input'] as String);
+
+        expect(_actual?.runtimeType, elem['expectedOutput']);
+      });
+    }
   });
   
 }
