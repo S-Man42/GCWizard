dd15db1ecce32ede4a15fcd5879e7475f31c0591
S-Man42 <info@s-man42.de>
2024-04-14 10:57:35 +0200

    sort details...

--- lib/tools/science_and_technology/sort/widget/sort.dart
+++ lib/tools/science_and_technology/sort/widget/sort.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 import 'package:gc_wizard/application/theme/theme.dart';
 import 'package:gc_wizard/common_widgets/dropdowns/gcw_dropdown.dart';
+import 'package:gc_wizard/common_widgets/gcw_expandable.dart';
+import 'package:gc_wizard/common_widgets/outputs/gcw_columned_multiline_output.dart';
 import 'package:gc_wizard/common_widgets/outputs/gcw_default_output.dart';
 import 'package:gc_wizard/common_widgets/switches/gcw_onoff_switch.dart';
 import 'package:gc_wizard/common_widgets/switches/gcw_twooptions_switch.dart';
@@ -94,16 +96,54 @@ class _SortState extends State<Sort> {
     );
   }
 
-  String _buildOutput() {
+  Widget _buildOutput() {
+    if (_currentInput.isEmpty) {
+      return const GCWDefaultOutput();
+    }
+
     var sortMode = _currentSortingMode == GCWSwitchPosition.left ? SortingMode.ASCENDING : SortingMode.DESCENDING;
     var sortInput = _currentCaseSensitive ? _currentInput : _currentInput.toUpperCase();
 
+    String textOutput = '';
     switch (_currentSortType) {
-      case _SortType.CHARACTERS: return sortTextASCII(sortInput, sortMode);
-      case _SortType.WORDS: return sortList<String>(sortInput.split(RegExp(r'\s+')).toList(), sortMode).join(' ');
-      case _SortType.LINES: return sortList<String>(sortInput.replaceAll('\r+', '').split(RegExp(r'\n+')).toList(), sortMode).join('\n');
-      case _SortType.CHARS_IN_WORDS: return sortTextInBlocks(sortInput, sortMode);
-      default: return _currentInput;
+      case _SortType.CHARACTERS: textOutput = sortTextASCII(sortInput, sortMode); break;
+      case _SortType.WORDS: textOutput = sortList<String>(sortInput.split(RegExp(r'\s+')).toList(), sortMode).join(' '); break;
+      case _SortType.LINES: textOutput = sortList<String>(sortInput.replaceAll('\r+', '').split(RegExp(r'\n+')).toList(), sortMode).join('\n'); break;
+      case _SortType.CHARS_IN_WORDS: textOutput = sortTextInBlocks(sortInput, sortMode); break;
+      default: break;
     }
+
+    List<String> detailData;
+    switch (_currentSortType) {
+      case _SortType.CHARACTERS: detailData = textOutput.split('').toList(); break;
+      case _SortType.WORDS: detailData = textOutput.split(' ').toList(); break;
+      case _SortType.LINES: detailData = textOutput.split('\n').toList(); break;
+      case _SortType.CHARS_IN_WORDS: detailData = textOutput.split(RegExp(r'[\s]')).toList(); break;
+      default: detailData = <String>[]; break;
+    }
+
+    var details = GCWExpandableTextDivider(
+      suppressTopSpace: false,
+      text: i18n(context, 'common_details'),
+      expanded: false,
+      child: GCWColumnedMultilineOutput(
+        flexValues: const [1, 5],
+        data: detailData.asMap().map((index, data) {
+            return MapEntry<int, List<String>>(
+                index,
+                [(index + 1).toString() + '.', data]
+            );
+          }).values.toList()
+      ),
+    );
+
+    return Column(
+      children: [
+        GCWDefaultOutput(
+          child: textOutput,
+        ),
+        details
+      ],
+    );
   }
 }
