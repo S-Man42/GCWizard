32475572783bd6e856102e8dcd82eac5603e5ec1
Mike Binnemann <Mike.Binnemann@web.de>
2024-05-08 14:56:30 +0200

    Update logical_solver.dart

--- lib/tools/games/logical_solver/widget/logical_solver.dart
+++ lib/tools/games/logical_solver/widget/logical_solver.dart
@@ -76,6 +76,7 @@ class LogicalSolverState extends State<LogicalSolver> {
               GCWIntegerSpinner(
                 title: i18n(context, 'logicalsolver_categories'),
                 value: _categoriesCount,
+                flexValues: const [2, 1],
                 overflow: SpinnerOverflowType.SUPPRESS_OVERFLOW,
                 min: minItemCount,
                 max: maxCategoriesCount,
@@ -89,6 +90,7 @@ class LogicalSolverState extends State<LogicalSolver> {
               GCWIntegerSpinner(
                 title: i18n(context, 'logicalsolver_items'),
                 value: _itemsCount,
+                flexValues: const [2, 1],
                 overflow: SpinnerOverflowType.SUPPRESS_OVERFLOW,
                 min: minItemCount,
                 max: 99,
