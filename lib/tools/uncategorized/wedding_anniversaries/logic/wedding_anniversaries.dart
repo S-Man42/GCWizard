67ca30f78f82aec393543d61eef5dbe84082d367
Olli <102247940+olliklee@users.noreply.github.com>
2024-07-14 10:49:41 +0200

    deleted json and added data dart file

--- lib/tools/uncategorized/wedding_anniversaries/logic/wedding_anniversaries.dart
+++ lib/tools/uncategorized/wedding_anniversaries/logic/wedding_anniversaries.dart
@@ -1,17 +1,45 @@
-import 'dart:convert';
 import 'dart:core';
 
-import 'package:flutter/services.dart';
+part 'package:gc_wizard/tools/uncategorized/wedding_anniversaries/logic/wedding_anniversaries_data.dart';
 
-void main() {
-  Future<void> loadJson() async {
-    String jsonString = await rootBundle
-        .loadString(
-        "tools/uncategorized/wedding_anniversaries/assets/wedding_anniversaries.json");
-    print(jsonString);
-    // Map<String, Map<String, List<String>>>? jsonData = jsonDecode(jsonString);
+enum WeddingCountry { DE, UK, US, NL, DK, SW, FR, IT, ES }
+
+Map<String, List<String>> _countryAnniversaries (WeddingCountry country) {
+  switch (country) {
+    case WeddingCountry.DE:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.UK:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.US:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.NL:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.DK:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.SW:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.FR:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.IT:
+      return _anniversariesDE;
+      break;
+    case WeddingCountry.ES:
+      return _anniversariesDE;
+      break;
   }
+}
 
-  loadJson();
+String getItems (WeddingCountry country, String year) {
+  var aniversaryList = _countryAnniversaries(country);
+  var items = aniversaryList[year];
+
+  return (items != null) ? items.join(", ") : '';
 }
 
