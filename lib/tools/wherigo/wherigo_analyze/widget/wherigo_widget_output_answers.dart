17712bef4fb18814b2feff60f214064ca3677088
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2024-08-07 10:19:08 +0200

    catch exception

--- lib/tools/wherigo/wherigo_analyze/widget/wherigo_widget_output_answers.dart
+++ lib/tools/wherigo/wherigo_analyze/widget/wherigo_widget_output_answers.dart
@@ -36,7 +36,7 @@ List<List<String>> _buildOutputListAnswers(BuildContext context, WherigoInputDat
       }
     }
   } else {
-    String _variable = _answerIsVariable(answers[1]);
+    String _variable = answers.length > 1 ? _answerIsVariable(answers[1]) : '';
     if (_variable.isNotEmpty) {
       result = [
         [i18n(context, 'wherigo_output_answer'), _variable, '']
