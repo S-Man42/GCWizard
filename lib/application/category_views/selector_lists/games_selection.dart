3477a1fb166080f24d1e47384f60a85723ad417b
S-Man42 <info@s-man42.de>
2024-08-19 16:51:25 +0200

    ...

--- lib/application/category_views/selector_lists/games_selection.dart
+++ lib/application/category_views/selector_lists/games_selection.dart
@@ -3,6 +3,8 @@ import 'package:gc_wizard/application/registry.dart';
 import 'package:gc_wizard/common_widgets/gcw_selection.dart';
 import 'package:gc_wizard/application/tools/widget/gcw_tool.dart';
 import 'package:gc_wizard/application/tools/widget/gcw_toollist.dart';
+import 'package:gc_wizard/tools/symbol_tables/_common/widget/symbol_table.dart';
+import 'package:gc_wizard/utils/ui_dependent_utils/common_widget_utils.dart';
 
 class GamesSelection extends GCWSelection {
   const GamesSelection({Key? key}) : super(key: key);
@@ -10,7 +12,9 @@ class GamesSelection extends GCWSelection {
   @override
   Widget build(BuildContext context) {
     final List<GCWTool> _toolList =
-        registeredTools.where((element) => element.categories.contains(ToolCategory.GAMES)).toList();
+      registeredTools.where((element) => element.categories.contains(ToolCategory.GAMES)
+          || (className(element.tool) == className(const SymbolTable()) && (element.tool as SymbolTable).symbolKey == 'billiard_balls')
+    ).toList();
     _toolList.sort((a, b) => sortToolList(a, b));
 
     return GCWToolList(toolList: _toolList);
