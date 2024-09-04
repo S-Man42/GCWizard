4323d9b5ea27c9026d1c014d9f6ef5f03f373b94
S-Man42 <info@s-man42.de>
2024-09-04 15:59:16 +0200

    translations

--- lib/application/main_menu/changelog.dart
+++ lib/application/main_menu/changelog.dart
@@ -6,7 +6,7 @@ import 'package:gc_wizard/common_widgets/gcw_text.dart';
 import 'package:intl/intl.dart';
 
 final CHANGELOG = {
-  '3.2.0': DateTime(2024, 9, 14),
+  '3.2.0': DateTime(2024, 9, 12),
   '3.1.1': DateTime(2024, 7, 2),
   '3.1.0': DateTime(2024, 5, 1),
   '3.0.3': DateTime(2023, 12, 8),
