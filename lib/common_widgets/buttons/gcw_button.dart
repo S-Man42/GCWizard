91833f62ce397ea54adf133af30e257488137e97
Olli <102247940+olliklee@users.noreply.github.com>
2024-09-11 00:14:06 +0200

    disabled gcw_button 'activated' param

--- lib/common_widgets/buttons/gcw_button.dart
+++ lib/common_widgets/buttons/gcw_button.dart
@@ -7,10 +7,10 @@ class GCWButton extends StatefulWidget {
   final void Function() onPressed;
   final TextStyle? textStyle;
   final EdgeInsetsGeometry? margin;
-  final bool activated;
+  //final bool activated;
 
   const GCWButton({Key? key, required this.text, required this.onPressed,
-  this.textStyle, this.margin, this.activated = true})
+  this.textStyle, this.margin, /*this.activated = true*/})
       : super(key: key);
 
   @override
@@ -24,7 +24,7 @@ class _GCWButtonState extends State<GCWButton> {
       margin: widget.margin ?? const EdgeInsets.only(top: 10.0, bottom: 10.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-            backgroundColor: !widget.activated ? themeColors().secondary().withOpacity(0.5) : themeColors().secondary()),
+            backgroundColor: /*!widget.activated ? themeColors().secondary().withOpacity(0.5) :*/ themeColors().secondary()),
         onPressed: widget.onPressed,
         child: Text(
           widget.text,
