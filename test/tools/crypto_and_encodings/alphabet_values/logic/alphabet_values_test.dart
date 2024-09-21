30f3f7b7bb98d46b2cfc041e3e7386431ad8d8f6
Mike-3 <Mike.Binnemann@web.de>
2024-09-21 11:44:11 +0200

    Update alphabet_values_test.dart

--- test/tools/crypto_and_encodings/alphabet_values/logic/alphabet_values_test.dart
+++ test/tools/crypto_and_encodings/alphabet_values/logic/alphabet_values_test.dart
@@ -31,7 +31,7 @@ void main() {
   group("AlphabetValues.valuesToText:", () {
     List<Map<String, Object?>> _inputsToExpected = [
       {'values' : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], 'expectedOutput' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'},
-      // {'values' : [42,26,25,24,23,22,21,20,19,18,17,16,15,14,null,13,12,11,10,9,8,7,6,5,4,3,2,1,0], 'expectedOutput' : UNKNOWN_ELEMENT + 'ZYXWVUTSRQPON<?>MLKJIHGFEDCBA<?>'},
+      // {'values' : [42,26,25,24,23,22,21,20,19,18,17,16,15,14,null,13,12,11,10,9,8,7,6,5,4,3,2,1,0], 'expectedOutput' : UNKNOWN_ELEMENT + 'ZYXWVUTSRQPON' + UNKNOWN_ELEMENT + 'MLKJIHGFEDCBA' + UNKNOWN_ELEMENT},
     ];
 
     for (var elem in _inputsToExpected) {
