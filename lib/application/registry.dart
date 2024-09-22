1375b7bd8a6434b1f200851de2f9840d315c33ae
THOMAS-DESKTOP\thoma <thomas@familiezimmermann.de>
2024-09-22 09:37:40 +0200

    new symboltable - issue 1919

--- lib/application/registry.dart
+++ lib/application/registry.dart
@@ -6640,6 +6640,22 @@ void initializeRegistry(BuildContext context) {
           sourceUrl: 'https://fontmeme.com/fonts/pigpen-cipher-font/',
           licenseType: ToolLicenseType.FREE_TO_USE),
     ]),
+    GCWSymbolTableTool(symbolKey: 'leitz_orgacolor', symbolSearchStrings: const [
+      'symbol_leitz_orgacolor',
+    ], licenses: [
+      ToolLicenseOnlineArticle(
+          context: context,
+          author: 'Leitz',
+          title: 'Buchstabensignale',
+          sourceUrl: 'https://www.bueromarkt-ag.de/buchstabensignale.html',
+          licenseType: ToolLicenseType.FREE_TO_USE),
+      ToolLicenseOnlineArticle(
+          context: context,
+          author: 'Leitz',
+          title: 'Ziffernsignale',
+          sourceUrl: 'https://www.bueromarkt-ag.de/buchstabensignale,b-ziffernsignale.html',
+          licenseType: ToolLicenseType.FREE_TO_USE),
+    ]),
     GCWSymbolTableTool(symbolKey: 'linear_b', symbolSearchStrings: const [
       'symbol_linear_b',
     ], licenses: [
