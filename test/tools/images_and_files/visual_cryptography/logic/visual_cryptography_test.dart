9ccba6c4b8387278186937cd86b4abaafeb29d76
Mike-3 <Mike.Binnemann@web.de>
2024-10-08 12:05:14 +0200

    Update visual_cryptography_test.dart

--- test/tools/images_and_files/visual_cryptography/logic/visual_cryptography_test.dart
+++ test/tools/images_and_files/visual_cryptography/logic/visual_cryptography_test.dart
@@ -32,7 +32,7 @@ void main() {
           _getFileData(elem['image2'] as String),
           elem['offsetX'] as int, elem['offsetY'] as int);
         var _actual = await decodeImagesAsync(GCWAsyncExecuterParameters(para));
-        expect(listEquals(_actual!, _getFileData(elem['expectedOutput'] as String)), true);
+        expect(uint8ListEquals(_actual!, _getFileData(elem['expectedOutput'] as String)), true);
       });
     }
   });
@@ -50,7 +50,7 @@ void main() {
             _getFileData(elem['image2'] as String),
             elem['offsetX'] as int, elem['offsetY'] as int);
         var _actual = cleanImage(para.item1, para.item2, para.item3, para.item4);
-        expect(listEquals(_actual!, _getFileData(elem['expectedOutput'] as String)), true);
+        expect(uint8ListEquals(_actual!, _getFileData(elem['expectedOutput'] as String)), true);
       });
     }
   });
@@ -68,7 +68,7 @@ void main() {
             elem['offsetX'] as int, elem['offsetY'] as int,
             elem['scale'] as int, elem['pixelSize'] as int);
         var _actual = await encodeImagesAsync(GCWAsyncExecuterParameters(para));
-        expect(listEquals(_actual!.item1, _getFileData(elem['expectedOutput1'] as String)), true);
+        expect(uint8ListEquals(_actual!.item1, _getFileData(elem['expectedOutput1'] as String)), true);
       });
     }
   });
