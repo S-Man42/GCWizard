3c5f3f1663660ca16522c2a1ea165c079512af75
Mike-3 <Mike.Binnemann@web.de>
2024-10-09 18:27:17 +0200

    navajo fold map

--- lib/tools/crypto_and_encodings/navajo/logic/navajo.dart
+++ lib/tools/crypto_and_encodings/navajo/logic/navajo.dart
@@ -4,80 +4,18 @@
 import 'dart:math';
 
 import 'package:collection/collection.dart';
+import 'package:gc_wizard/tools/crypto_and_encodings/substitution/logic/substitution.dart';
+import 'package:gc_wizard/utils/collection_utils.dart';
 import 'package:gc_wizard/utils/constants.dart';
 
 part 'package:gc_wizard/tools/crypto_and_encodings/navajo/logic/navajo_dictionary.dart';
 
 String _shrinkText(String input) {
-  return input
-      .replaceAll('COMMANDING GEN.', 'COMMANDINGGEN')
-      .replaceAll('MAJOR GEN.', 'MAJORGEN')
-      .replaceAll('BRIGADIER GEN.', 'BRIGADIERGEN')
-      .replaceAll('LT. COLONEL', 'LTCOLONEL')
-      .replaceAll('COMMANDING OFFICER', 'COMMANDINGOFFICER')
-      .replaceAll('EXECUTIVE OFFICER', 'EXECUTIVEOFFICER')
-      .replaceAll('SOUTH AMERICA', 'SOUTHAMERICA')
-      .replaceAll('DIVE BOMBER', 'DIVEBOMBER')
-      .replaceAll('TORPEDO PLANE', 'TORPEDOPLANE')
-      .replaceAll('OBS. PLAN', 'OBSPLAN')
-      .replaceAll('FIGHTER PLANE', 'FIGHTERPLANE')
-      .replaceAll('BOMBER PLANE', 'BOMBERPLANE')
-      .replaceAll('PATROL PLANE', 'PATROLPLANE')
-      .replaceAll('MINE SWEEPER', 'MINESWEEPER')
-      .replaceAll('MOSQUITO BOAT', 'MOSQUITOBOAT')
-      .replaceAll('BOOBY TRAP', 'BOOBYTRAP')
-      .replaceAll('BULL DOZER', 'BULLDOZER')
-      .replaceAll('COAST GUARD', 'COASTGUARD')
-      .replaceAll('COUNTER ATTACK ', 'COUNTERATTACK ')
-      .replaceAll('DIG IN', 'DIGIN')
-      .replaceAll('FLAME THROWER', 'FLAMETHROWER')
-      .replaceAll('HALF TRACK', 'HALFTRACK')
-      .replaceAll('HIGH EXPLOSIVE', 'HIGHEXPLOSIVE')
-      .replaceAll('MACHINE GUN', 'MACHINEGUN')
-      .replaceAll('MERCHANT SHIP', 'MERCHANTSHIP')
-      .replaceAll('PILL BOX', 'PILLBOX')
-      .replaceAll('PINNED DOWN', 'PINNEDDOWN')
-      .replaceAll('ROBOT BOMB', 'ROBOTBOMB')
-      .replaceAll('SEMI COLON', 'SEMICOLON')
-      .replaceAll('SUPPLY SHIP', 'SUPPLYSHIP')
-      .replaceAll('TANK DESTROYER', 'TANKDESTROYER')
-      .replaceAll('TRAFFIC DIAGRAM', 'TRAFFICDIAGRAM');
+  return substitution(input, _NAVAJO_FOLD_MAP);
 }
 
 String _enfoldText(String input) {
-  return input
-      .replaceAll('COMMANDINGGEN', 'COMMANDING GEN.')
-      .replaceAll('MAJORGEN', 'MAJOR GEN.')
-      .replaceAll('BRIGADIERGEN', 'BRIGADIER GEN.')
-      .replaceAll('LTCOLONEL', 'LT. COLONEL')
-      .replaceAll('COMMANDINGOFFICER', 'COMMANDING OFFICER')
-      .replaceAll('EXECUTIVEOFFICER', 'EXECUTIVE OFFICER')
-      .replaceAll('SOUTHAMERICA', 'SOUTH AMERICA')
-      .replaceAll('DIVEBOMBER', 'DIVE BOMBER')
-      .replaceAll('TORPEDOPLANE', 'TORPEDO PLANE')
-      .replaceAll('OBSPLAN', 'OBS. PLAN')
-      .replaceAll('FIGHTERPLANE', 'FIGHTER PLANE')
-      .replaceAll('BOMBERPLANE', 'BOMBER PLANE')
-      .replaceAll('PATROLPLANE', 'PATROL PLANE')
-      .replaceAll('MINESWEEPER', 'MINE SWEEPER')
-      .replaceAll('MOSQUITOBOAT', 'MOSQUITO BOAT')
-      .replaceAll('BOOBYTRAP', 'BOOBY TRAP')
-      .replaceAll('BULLDOZER', 'BULL DOZER')
-      .replaceAll('COASTGUARD', 'COAST GUARD')
-      .replaceAll('COUNTERATTACK ', 'COUNTER ATTACK ')
-      .replaceAll('DIGIN', 'DIG IN')
-      .replaceAll('FLAMETHROWER', 'FLAME THROWER')
-      .replaceAll('HALFTRACK', 'HALF TRACK')
-      .replaceAll('HIGHEXPLOSIVE', 'HIGH EXPLOSIVE')
-      .replaceAll('MACHINEGUN', 'MACHINE GUN')
-      .replaceAll('MERCHANTSHIP', 'MERCHANT SHIP')
-      .replaceAll('PILLBOX', 'PILL BOX')
-      .replaceAll('PINNEDDOWN', 'PINNED DOWN')
-      .replaceAll('ROBOTBOMB', 'ROBOT BOMB')
-      .replaceAll('SEMICOLON', 'SEMI COLON')
-      .replaceAll('SUPPLYSHIP', 'SUPPLY SHIP')
-      .replaceAll('TANKDESTROYER', 'TANK DESTROYER')
-      .replaceAll('TRAFFICDIAGRAM', 'TRAFFIC DIAGRAM');
+  return substitution(input, switchMapKeyValue(_NAVAJO_FOLD_MAP));
 }
 
 String decodeNavajo(String cipherText, bool useOnlyAlphabet) {
