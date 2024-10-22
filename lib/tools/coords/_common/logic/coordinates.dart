c0e1172c6687dd9f7a46953d5c140b60a6a45313
Mike-3 <Mike.Binnemann@web.de>
2024-10-22 12:12:26 +0200

    Update coordinates.dart

--- lib/tools/coords/_common/logic/coordinates.dart
+++ lib/tools/coords/_common/logic/coordinates.dart
@@ -17,6 +17,7 @@ import 'package:gc_wizard/tools/coords/_common/formats/mgrs_utm/logic/mgrs.dart'
 import 'package:gc_wizard/tools/coords/_common/formats/natural_area_code/logic/natural_area_code.dart';
 import 'package:gc_wizard/tools/coords/_common/formats/openlocationcode/logic/open_location_code.dart';
 import 'package:gc_wizard/tools/coords/_common/formats/quadtree/logic/quadtree.dart';
+import 'package:gc_wizard/tools/coords/_common/formats/reversewherigo_10y_waldmeister/logic/reverse_wherigo_10y_waldmeister.dart';
 import 'package:gc_wizard/tools/coords/_common/formats/reversewherigo_day1976/logic/reverse_wherigo_day1976.dart';
 import 'package:gc_wizard/tools/coords/_common/formats/reversewherigo_waldmeister/logic/reverse_wherigo_waldmeister.dart';
 import 'package:gc_wizard/tools/coords/_common/formats/s2cells_hilbert/logic/s2cells_hilbert.dart';
